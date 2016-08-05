53b66089fc7333442feab12995617133a54fff74
simonguo <simonguo.2009@gmail.com>
2016-08-05 18:08:51 +0800

    Bugfix

--- src/NavItem.js
+++ src/NavItem.js
@@ -28,7 +28,6 @@ const NavItem = React.createClass({
     handleClick(e) {
 
         if (this.props.onSelect) {
-            e.preventDefault();
             if (!this.props.disabled) {
                 this.props.onSelect(this.props.eventKey, e);
             }
