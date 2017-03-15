e691695b426d6d4b3ddb5a24a00f7c04e0e7e5c1
simonguo <simonguo.2009@gmail.com>
2017-03-15 21:43:59 +0800

    rm dev

--- dev/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ButtonGroupDemo from './ButtonGroupDemo';
-
-ReactDOM.render(<ButtonGroupDemo/>, document.getElementById('mount'));
