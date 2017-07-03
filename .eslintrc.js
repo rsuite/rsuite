f0420829f4a78792904e1b01f5950935cf876745
simonguo <simonguo.2009@gmail.com>
2017-07-03 23:50:03 +0800

    Update config

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const OFF = 0;
+const WARNING = 1;
+const ERROR = 2;
+
+module.exports = {
+  'extends': 'airbnb',
+  'env': {
+    'browser': true,
+    'es6': true,
+    'mocha': true
+  },
+  'parser': 'babel-eslint',
+  'plugins': [
+    'react',
+    'babel'
+  ],
+  'parserOptions': {
+    'ecmaVersion': 6,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'jsx': true,
+      'experimentalObjectRestSpread': true
+    }
+  },
+  'rules': {
+    'indent': [ERROR, 2, { 'SwitchCase': 1 }],  //规定代码的缩进方式：2个空格
+    'camelcase': ERROR,                         //强制驼峰法命名
+    'curly': ERROR,                             //必须使用 if(){} 中的{}
+    'eqeqeq': ERROR,                            //必须使用全等
+    'brace-style': [ERROR, '1tbs'],             //大括号风格
+    'quotes': [ERROR, 'single'],                //引号类型
+    'semi': [ERROR, 'always'],                  //语句强制分号结尾
+    'space-infix-ops': ERROR,                   //中缀操作符周围要不要有空格
+    'no-param-reassign': OFF,                   //不允许对函数的形参进行赋值
+    'prefer-spread': ERROR,                     //首选展开运算
+    'comma-dangle': OFF,                        //不允许或强制在对象字面量或者数组属性的结尾使用逗号
+    'padded-blocks': OFF,                       //规定代码块前后是否要加空行
+    'prefer-const': OFF,
+    'no-multi-spaces': ERROR,
+    'no-var': OFF,
+    'one-var': OFF,
+    /**
+     * https://github.com/airbnb/javascript/tree/master/react
+     */
+    'react/prefer-es6-class': [WARNING, 'always'],    //使用 class extends React.Component
+    'react/jsx-pascal-case': ERROR,                   //骆驼式命名
+    'react/jsx-closing-bracket-location': ERROR,      //JSX语法缩进/格式
+    'react/jsx-curly-spacing': ERROR,                 //JSX {} 引用括号里两边加空格
+    'react/jsx-boolean-value': [ERROR, 'always'],     //如果属性值为 true, 可以直接省略
+    'jsx-quotes': [ERROR, 'prefer-double'],           //JSX属性值总是使用双引号(")
+    'react/no-string-refs': ERROR,                    //Refs里使用回调函数
+    'react/jsx-wrap-multilines': ERROR,               //多行的JSX标签写在 ()里
+    'react/self-closing-comp': ERROR,                 //没有子元素的标签来说总是自己关闭标签
+    'react/jsx-no-bind': ERROR,                       //当在 render() 里使用事件处理方法时，提前在构造函数里把 this 绑定上去
+    'react/no-is-mounted': ERROR,                      //不要再使用 isMounted
+    'jsx-a11y/href-no-hash': OFF
+  }
+};
--- karma.conf.js
+++ karma.conf.js
@@ -1,46 +1,46 @@
 var webpackConfig = {
-    output: {
-        pathinfo: true
-    },
-    module: {
-        loaders: [{
-            test: /\.js$/,
-            loaders: [
-                'babel?babelrc'
-            ],
-            exclude: /node_modules/
-        }]
-    },
-    devtool: 'eval'
+  output: {
+    pathinfo: true
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      use: [
+        'babel-loader'
+      ],
+      exclude: /node_modules/
+    }]
+  },
+  devtool: 'eval'
 };
 
 module.exports = function (config) {
-    const { env } = process;
-    config.set({
-        basePath: '',
-        files: ['test/index.js'],
-        frameworks: [
-            'mocha',
-            'sinon-chai'
-        ],
-        colors: true,
-        reporters: ['mocha'],
+  const { env } = process;
+  config.set({
+    basePath: '',
+    files: ['test/index.js'],
+    frameworks: [
+      'mocha',
+      'sinon-chai'
+    ],
+    colors: true,
+    reporters: ['mocha'],
 
-        logLevel: config.LOG_INFO,
-        preprocessors: {
-            'test/index.js': ['webpack'],
-        },
-        webpack: webpackConfig,
-        webpackMiddleware: {
-            noInfo: true
-        },
-        browsers: env.BROWSER ? env.BROWSER.split(',') : ['Chrome'],
-        customLaunchers: {
-            ChromeCi: {
-                base: 'Chrome',
-                flags: ['--no-sandbox'],
-            },
-        }
-    });
+    logLevel: config.LOG_INFO,
+    preprocessors: {
+      'test/index.js': ['webpack'],
+    },
+    webpack: webpackConfig,
+    webpackMiddleware: {
+      noInfo: true
+    },
+    browsers: env.BROWSER ? env.BROWSER.split(',') : ['Chrome'],
+    customLaunchers: {
+      ChromeCi: {
+        base: 'Chrome',
+        flags: ['--no-sandbox'],
+      },
+    }
+  });
 };
 
