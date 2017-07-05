0ff1de75346f33bacaaa997b0247f73bc998887f
simonguo <simonguo.2009@gmail.com>
2017-07-05 19:23:45 +0800

    rm fixtures

--- src/prop-types/deprecated.js
+++
@@ -1,9 +0,0 @@
-export default function deprecated(propType, explanation) {
-    return function validate(props, propName, componentName) {
-        if (props[propName] !== null) {
-            new Error(`"${propName}" property of "${componentName}" has been deprecated.\n${explanation}`);
-        }
-
-        return propType(props, propName, componentName);
-    };
-}
