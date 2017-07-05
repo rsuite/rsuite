d380a3b5d686553e8b3d9893252eaa2e00876e8e
simonguo <simonguo.2009@gmail.com>
2017-07-05 19:22:32 +0800

    Update utils

--- src/utils/keycode.js
+++ src/utils/keycode.js
@@ -2,41 +2,41 @@
  * modified version of:
  * https://github.com/timoxley/keycode/blob/master/index.js
  */
-export let search = function(searchInput) {
-    // Keyboard Events
-    if (searchInput && 'object' === typeof searchInput) {
-        var hasKeyCode = searchInput.which || searchInput.keyCode || searchInput.charCode;
-        if (hasKeyCode) {
-            searchInput = hasKeyCode;
-        }
+export const search = function (searchInput) {
+  // Keyboard Events
+  if (searchInput && 'object' === typeof searchInput) {
+    var hasKeyCode = searchInput.which || searchInput.keyCode || searchInput.charCode;
+    if (hasKeyCode) {
+      searchInput = hasKeyCode;
     }
+  }
 
-    // Numbers
-    if ('number' === typeof searchInput) {
-        return names[searchInput];
-    }
+  // Numbers
+  if ('number' === typeof searchInput) {
+    return names[searchInput];
+  }
 
-    // Everything else (cast to string)
-    var search = String(searchInput);
+  // Everything else (cast to string)
+  var search = String(searchInput);
 
-    // check codes
-    var foundNamedKey = codes[search.toLowerCase()];
-    if (foundNamedKey) {
-        return foundNamedKey;
-    }
+  // check codes
+  var foundNamedKey = codes[search.toLowerCase()];
+  if (foundNamedKey) {
+    return foundNamedKey;
+  }
 
-    // check aliases
-    var foundNamedKey = aliases[search.toLowerCase()];
-    if (foundNamedKey) {
-        return foundNamedKey;
-    }
+  // check aliases
+  var foundNamedKey = aliases[search.toLowerCase()];
+  if (foundNamedKey) {
+    return foundNamedKey;
+  }
 
-    // weird character?
-    if (search.length === 1) {
-        return search.charCodeAt(0);
-    }
+  // weird character?
+  if (search.length === 1) {
+    return search.charCodeAt(0);
+  }
 
-    return undefined;
+  return undefined;
 };
 
 /**
@@ -46,71 +46,71 @@ export let search = function(searchInput) {
  */
 
 export let codes = {
-    'backspace': 8,
-    'tab': 9,
-    'enter': 13,
-    'shift': 16,
-    'ctrl': 17,
-    'alt': 18,
-    'pause/break': 19,
-    'caps lock': 20,
-    'esc': 27,
-    'space': 32,
-    'page up': 33,
-    'page down': 34,
-    'end': 35,
-    'home': 36,
-    'left': 37,
-    'up': 38,
-    'right': 39,
-    'down': 40,
-    'insert': 45,
-    'delete': 46,
-    'command': 91,
-    'right click': 93,
-    'numpad *': 106,
-    'numpad +': 107,
-    'numpad -': 109,
-    'numpad .': 110,
-    'numpad /': 111,
-    'num lock': 144,
-    'scroll lock': 145,
-    'my computer': 182,
-    'my calculator': 183,
-    ';': 186,
-    '=': 187,
-    ',': 188,
-    '-': 189,
-    '.': 190,
-    '/': 191,
-    '`': 192,
-    '[': 219,
-    '\\': 220,
-    ']': 221,
-    '\'': 222
+  'backspace': 8,
+  'tab': 9,
+  'enter': 13,
+  'shift': 16,
+  'ctrl': 17,
+  'alt': 18,
+  'pause/break': 19,
+  'caps lock': 20,
+  'esc': 27,
+  'space': 32,
+  'page up': 33,
+  'page down': 34,
+  'end': 35,
+  'home': 36,
+  'left': 37,
+  'up': 38,
+  'right': 39,
+  'down': 40,
+  'insert': 45,
+  'delete': 46,
+  'command': 91,
+  'right click': 93,
+  'numpad *': 106,
+  'numpad +': 107,
+  'numpad -': 109,
+  'numpad .': 110,
+  'numpad /': 111,
+  'num lock': 144,
+  'scroll lock': 145,
+  'my computer': 182,
+  'my calculator': 183,
+  ';': 186,
+  '=': 187,
+  ',': 188,
+  '-': 189,
+  '.': 190,
+  '/': 191,
+  '`': 192,
+  '[': 219,
+  '\\': 220,
+  ']': 221,
+  '\'': 222
 };
 
 // Helper aliases
-let aliases  = {
-    'windows': 91,
-    '⇧': 16,
-    '⌥': 18,
-    '⌃': 17,
-    '⌘': 91,
-    'ctl': 17,
-    'control': 17,
-    'option': 18,
-    'pause': 19,
-    'break': 19,
-    'caps': 20,
-    'return': 13,
-    'escape': 27,
-    'spc': 32,
-    'pgup': 33,
-    'pgdn': 33,
-    'ins': 45,
-    'del': 46,
-    'cmd': 91
+let aliases = {
+  'windows': 91,
+  '⇧': 16,
+  '⌥': 18,
+  '⌃': 17,
+  '⌘': 91,
+  'ctl': 17,
+  'control': 17,
+  'option': 18,
+  'pause': 19,
+  'break': 19,
+  'caps': 20,
+  'return': 13,
+  'escape': 27,
+  'spc': 32,
+  'pgup': 33,
+  'pgdn': 33,
+  'ins': 45,
+  'del': 46,
+  'cmd': 91
 };
 
 
@@ -120,22 +120,22 @@ let aliases  = {
 
 // lower case chars
 for (i = 97; i < 123; i++) {
-    codes[String.fromCharCode(i)] = i - 32;
+  codes[String.fromCharCode(i)] = i - 32;
 }
 
 // numbers
 for (var i = 48; i < 58; i++) {
-    codes[i - 48] = i;
+  codes[i - 48] = i;
 }
 
 // function keys
 for (i = 1; i < 13; i++) {
-    codes['f' + i] = i + 111;
+  codes['f' + i] = i + 111;
 }
 
 // numpad keys
 for (i = 0; i < 10; i++) {
-    codes['numpad ' + i] = i + 96;
+  codes['numpad ' + i] = i + 96;
 }
 
 /**
@@ -148,15 +148,15 @@ let names = exports.names = exports.title = {}; // title for backward compat
 
 // Create reverse mapping
 for (i in codes) {
-    names[codes[i]] = i;
+  names[codes[i]] = i;
 }
 
 // Add aliases
 for (var alias in aliases) {
-    codes[alias] = aliases[alias];
+  codes[alias] = aliases[alias];
 }
 
 
 export default {
-    search, codes
+  search, codes
 };
