53a192f19ea28541c5a51fe407f5a7110a8bf777
simonguo <simonguo.2009@gmail.com>
2017-07-07 23:49:28 +0800

    Add ReactChildren

---
+++ src/utils/ReactChildren.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+function find(children, func, context) {
+  let index = 0;
+  let result;
+
+  React.Children.forEach(children, (child) => {
+    if (result) {
+      return;
+    }
+    index += 1;
+    if (func.call(context, child, index)) {
+      result = child;
+    }
+  });
+
+  return result;
+}
+
+function map(children, func, context) {
+  let index = 0;
+  return React.Children.map(children, (child) => {
+    index += 1;
+    return func.call(context, child, index);
+  });
+}
+
+export default {
+  map,
+  find
+};
