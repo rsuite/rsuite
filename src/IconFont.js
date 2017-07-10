b494331e5d8f4b99bb05483f44c902b188736224
simonguo <simonguo.2009@gmail.com>
2017-07-10 17:08:02 +0800

    Update IconFont

--- src/IconFont.js
+++ src/IconFont.js
@@ -1,62 +1,69 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import ClassNameMixin from './mixins/ClassNameMixin';
-import elementType from './prop-types/elementType';
+import elementType from 'rsuite-utils/lib/propTypes/elementType';
 
-const IconFont = React.createClass({
-    mixins: [ClassNameMixin],
-    propTypes: {
-        classPrefix: PropTypes.string,
-        componentClass: elementType,
-        icon: PropTypes.string.isRequired,
-        size: PropTypes.oneOf(['lg', '2x', '3x', '4x', '5x']),
-        flip: PropTypes.oneOf(['horizontal', 'vertical']),
-        stack: PropTypes.oneOf(['1x', '2x']),
-        rotate: PropTypes.number,
-        fixedWidth: PropTypes.bool,
-        spin: PropTypes.bool,
-        pulse: PropTypes.bool,
-    },
-    getDefaultProps() {
-        return {
-            componentClass: 'i',
-            classPrefix: 'icon'
-        };
-    },
-    render() {
-        const {
-            componentClass: Component,
-            className,
-            classPrefix,
-            icon,
-            size,
-            fixedWidth,
-            spin,
-            pulse,
-            rotate,
-            flip,
-            stack,
-            ...props
-        } = this.props;
+const propTypes = {
+  prefixClass: PropTypes.string,
+  componentClass: elementType,
+  icon: PropTypes.string.isRequired,
+  size: PropTypes.oneOf(['lg', '2x', '3x', '4x', '5x']),
+  flip: PropTypes.oneOf(['horizontal', 'vertical']),
+  stack: PropTypes.oneOf(['1x', '2x']),
+  rotate: PropTypes.number,
+  fixedWidth: PropTypes.bool,
+  spin: PropTypes.bool,
+  pulse: PropTypes.bool,
+};
 
-        const classes = classNames(
-            classPrefix,
-            this.prefix(icon),
-            className, {
-                [`${classPrefix}-${size}`]: size,
-                [`${classPrefix}-fw`]: fixedWidth,
-                [`${classPrefix}-spin`]: spin,
-                [`${classPrefix}-pulse`]: pulse,
-                [`${classPrefix}-flip-${flip}`]: flip,
-                [`${classPrefix}-rotate-${rotate}`]: rotate,
-                [`${classPrefix}-stack-${stack}`]: stack
-            }
-        );
+const defaultProps = {
+  componentClass: 'i',
+  prefixClass: 'icon',
+  size: null,
+  flip: null,
+  rotate: null,
+  stack: null,
+  fixedWidth: false,
+  spin: false,
+  pulse: false
+};
 
-        return (
-            <Component {...props} className={classes} />
-        );
-    }
-});
+class IconFont extends React.Component {
+  render() {
+    const {
+      componentClass: Component,
+      className,
+      prefixClass,
+      icon,
+      size,
+      fixedWidth,
+      spin,
+      pulse,
+      rotate,
+      flip,
+      stack,
+      ...props
+    } = this.props;
+
+    const classes = classNames(
+      `${prefixClass}`,
+      `${prefixClass}-${icon}`, {
+        [`${prefixClass}-${size}`]: size,
+        [`${prefixClass}-fw`]: fixedWidth,
+        [`${prefixClass}-spin`]: spin,
+        [`${prefixClass}-pulse`]: pulse,
+        [`${prefixClass}-flip-${flip}`]: flip,
+        [`${prefixClass}-rotate-${rotate}`]: rotate,
+        [`${prefixClass}-stack-${stack}`]: stack
+      }, className);
+
+    return (
+      <Component {...props} className={classes} />
+    );
+  }
+}
+
+IconFont.propTypes = propTypes;
+IconFont.defaultProps = defaultProps;
 
 export default IconFont;
