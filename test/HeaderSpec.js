998503acf56838d8fad1159706bb4c7a456c4ab0
simonguo <simonguo.2009@gmail.com>
2017-07-10 16:57:07 +0800

    Update Header

---
+++ test/HeaderSpec.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { findDOMNode } from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
+
+
+import Header from '../src/Header';
+
+describe('Header', () => {
+
+  it('Should render a Header', () => {
+    let title = 'Test';
+    let instance = ReactTestUtils.renderIntoDocument(
+      <Header>{title}</Header>
+    );
+    assert.ok(findDOMNode(instance).className.match(/\bheader\b/));
+    assert.equal(findDOMNode(instance).innerText, title);
+  });
+
+
+  it('Should have a custom className', () => {
+    let instance = ReactTestUtils.renderIntoDocument(
+      <Header className="custom" />
+    );
+    assert.ok(findDOMNode(instance).className.match(/\bcustom\b/));
+  });
+
+
+  it('Should have a custom style', () => {
+    const fontSize = '12px';
+    let instance = ReactTestUtils.renderIntoDocument(
+      <Header style={{ fontSize }} />
+    );
+    assert.equal(findDOMNode(instance).style.fontSize, fontSize);
+  });
+
+});
