3a22413a1ce6030ecbe2b21356ea419f561d8a23
simonguo <simonguo.2009@gmail.com>
2017-07-14 17:16:18 +0800

    Update Whisper*

--- src/Whisper.js
+++ src/Whisper.js
@@ -1,273 +1,254 @@
-import React, {cloneElement} from 'react';
-import ReactDOM from 'react-dom';
-import {contains} from 'dom-lib';
-import Overlay from './fixtures/Overlay';
-import {isNull, isUndefined, pick} from 'lodash';
-
+import React, { cloneElement } from 'react';
+import ReactDOM, { findDOMNode } from 'react-dom';
+import PropTypes from 'prop-types';
+import { contains } from 'dom-lib';
+import { Overlay } from 'rsuite-utils/lib/Overlay';
+import _ from 'lodash';
+import isNullOrUndefined from './utils/isNullOrUndefined';
 import createChainedFunction from './utils/createChainedFunction';
+import isOneOf from './utils/isOneOf';
 
-/**
- * Check if value one is inside or equal to the of value
- *
- * @param {string} one
- * @param {string|array} of
- * @returns {boolean}
- */
-function isOneOf(one, of) {
-    if (Array.isArray(of)) {
-        return of.indexOf(one) >= 0;
+const propTypes = {
+  ..._.omit(Overlay.propTypes, ['target', 'onHide', 'show']),
+  trigger: PropTypes.oneOfType([
+    PropTypes.oneOf(['click', 'hover', 'focus']),
+    PropTypes.arrayOf(PropTypes.oneOf(['click', 'hover', 'focus']))
+  ]),
+  delay: PropTypes.number,
+  delayShow: PropTypes.number,
+  delayHide: PropTypes.number,
+  defaultOverlayShown: PropTypes.bool,
+  speaker: PropTypes.node.isRequired,
+  onBlur: PropTypes.func,
+  onClick: PropTypes.func,
+  onFocus: PropTypes.func,
+  onMouseLeave: PropTypes.func
+};
+
+const defaultProps = {
+  defaultOverlayShown: false,
+  trigger: ['hover', 'focus'],
+  rootClose: true
+};
+
+class Whisper extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+
+    this.handleMouseOver = e => (
+      this.handleMouseOverOut(this.handleDelayedShow, e)
+    );
+    this.handleMouseOut = e => (
+      this.handleMouseOverOut(this.handleDelayedHide, e)
+    );
+
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleHide = this.handleHide.bind(this);
+    this.handleDelayedShow = this.handleDelayedShow.bind(this);
+    this.handleDelayedHide = this.handleDelayedHide.bind(this);
+    this.getOverlayTarget = this.getOverlayTarget.bind(this);
+
+    this.state = {
+      isOverlayShown: props.defaultOverlayShown
+    };
+    this.mountNode = null;
+  }
+
+
+  componentDidMount() {
+    this.mountNode = document.createElement('div');
+    this.renderOverlay();
+  }
+
+  componentDidUpdate() {
+    if (this.mountNode) {
+      this.renderOverlay();
     }
-    return one === of;
-}
+  }
 
+  componentWillUnmount() {
+    ReactDOM.unmountComponentAtNode(this.mountNode);
+    this.mountNode = null;
+    clearTimeout(this.hoverShowDelay);
+    clearTimeout(this.hoverHideDelay);
+  }
 
-function isNullOrUndefinded(k) {
-    return isNull(k) || isUndefined(k);
-}
+  getOverlayTarget() {
+    return findDOMNode(this);
+  }
 
-const Whisper = React.createClass({
+  getOverlay() {
 
-    propTypes: {
+    let speakerProps = {
+      ..._.pick(this.props, Object.keys(Overlay.propTypes)),
+      show: this.state.isOverlayShown,
+      onHide: this.handleHide,
+      target: this.getOverlayTarget
+    };
 
-        ...Overlay.propTypes,
+    let speaker = cloneElement(this.props.speaker, {
+      placement: speakerProps.placement,
+    });
+    return (
+      <Overlay
+        {...speakerProps}
+      >
+        {speaker}
+      </Overlay>
+    );
+  }
 
-        /**
-         * Specify which action or actions trigger Overlay visibility
-         */
-        trigger: React
-            .PropTypes
-            .oneOfType([
-                React
-                    .PropTypes
-                    .oneOf(['click', 'hover', 'focus']),
-                React
-                    .PropTypes
-                    .arrayOf(React.PropTypes.oneOf(['click', 'hover', 'focus']))
-            ]),
+  hide() {
+    this.setState({
+      isOverlayShown: false
+    });
+  }
 
-        /**
-         * A millisecond delay amount to show and hide the Overlay once triggered
-         */
-        delay: React.PropTypes.number,
-        /**
-         * A millisecond delay amount before showing the Overlay once triggered.
-         */
-        delayShow: React.PropTypes.number,
-        /**
-         * A millisecond delay amount before hiding the Overlay once triggered.
-         */
-        delayHide: React.PropTypes.number,
+  show() {
+    this.setState({
+      isOverlayShown: true
+    });
+  }
 
-        /**
-         * The initial visibility state of the Overlay, for more nuanced visibility controll consider
-         * using the Overlay component directly.
-         */
-        defaultOverlayShown: React.PropTypes.bool,
+  handleHide() {
+    this.hide();
+  }
 
-        /**
-         * An element or text to speaker next to the target.
-         */
-        speaker: React.PropTypes.node.isRequired,
-        onBlur: React.PropTypes.func,
-        onClick: React.PropTypes.func,
-        onFocus: React.PropTypes.func,
-        onMouseLeave: React.PropTypes.func,
+  handleToggle() {
+    if (this.state.isOverlayShown) {
+      this.hide();
+    } else {
+      this.show();
+    }
+  }
 
-        // override specific speaker props
-        target() {},
-        onHide() {},
-        show() {}
-    },
+  handleDelayedShow() {
 
-    getDefaultProps() {
-        return {
-            defaultOverlayShown: false,
-            trigger: ['hover', 'focus'],
-            rootClose:true
-        };
-    },
-
-    getInitialState() {
-        return {isOverlayShown: this.props.defaultOverlayShown};
-    },
-
-    show() {
-        this.setState({isOverlayShown: true});
-    },
-
-    hide() {
-        this.setState({isOverlayShown: false});
-    },
-
-    toggle() {
-        if (this.state.isOverlayShown) {
-            this.hide();
-        } else {
-            this.show();
-        }
-    },
-
-    componentWillMount() {
-        this.handleMouseOver = this
-            .handleMouseOverOut
-            .bind(null, this.handleDelayedShow);
-        this.handleMouseOut = this
-            .handleMouseOverOut
-            .bind(null, this.handleDelayedHide);
-    },
-
-    componentDidMount() {
-        this._mountNode = document.createElement('div');
-        this.renderOverlay();
-    },
-
-    renderOverlay() {
-        ReactDOM.unstable_renderSubtreeIntoContainer(this, this._speaker, this._mountNode);
-    },
-
-    componentWillUnmount() {
-        ReactDOM.unmountComponentAtNode(this._mountNode);
-        this._mountNode = null;
-        clearTimeout(this._hoverShowDelay);
-        clearTimeout(this._hoverHideDelay);
-    },
-
-    componentDidUpdate() {
-        if (this._mountNode) {
-            this.renderOverlay();
-        }
-    },
-
-    getOverlayTarget() {
-        return ReactDOM.findDOMNode(this);
-    },
-
-    getOverlay() {
-        let speakerProps = {
-            ...pick(this.props, Object.keys(Overlay.propTypes)),
-            show: this.state.isOverlayShown,
-            onHide: this.hide,
-            target: this.getOverlayTarget,
-            onExit: this.props.onExit,
-            onExiting: this.props.onExiting,
-            onExited: this.props.onExited,
-            onEnter: this.props.onEnter,
-            onEntering: this.props.onEntering,
-            onEntered: this.props.onEntered
-        };
-
-        let speaker = cloneElement(this.props.speaker, {
-            placement: speakerProps.placement,
-            container: speakerProps.container
-        });
-
-        return (
-            <Overlay {...speakerProps}>
-                {speaker}
-            </Overlay>
-        );
-    },
-
-    render() {
-        const trigger = React
-            .Children
-            .only(this.props.children);
-        const triggerProps = trigger.props;
-
-        const props = {
-            'aria-describedby': this.props.speaker.props.id
-        };
-
-        // create in render otherwise owner is lost...
-        this._speaker = this.getOverlay();
-
-        props.onClick = createChainedFunction(triggerProps.onClick, this.props.onClick);
-
-        if (isOneOf('click', this.props.trigger)) {
-            props.onClick = createChainedFunction(this.toggle, props.onClick);
-        }
-
-        if (isOneOf('hover', this.props.trigger)) {
-            new Error(!(this.props.trigger === 'hover'), '[suite] Specifying only the `"hover"` trigger limits the visibilty of the speaker to just mouse users. ' + 'Consider also including the `"focus"` trigger so that touch and keyboard only users can see the speaker as well.');
-
-            props.onMouseOver = createChainedFunction(this.handleMouseOver, this.props.onMouseOver, triggerProps.onMouseOver);
-            props.onMouseOut = createChainedFunction(this.handleMouseOut, this.props.onMouseOut, triggerProps.onMouseOut);
-        }
-
-        if (isOneOf('focus', this.props.trigger)) {
-            props.onFocus = createChainedFunction(this.handleDelayedShow, this.props.onFocus, triggerProps.onFocus);
-            props.onBlur = createChainedFunction(this.handleDelayedHide, this.props.onBlur, triggerProps.onBlur);
-        }
-
-        return cloneElement(trigger, props);
-    },
-
-    handleDelayedShow() {
-
-        if (!isNullOrUndefinded(this._hoverHideDelay)) {
-            clearTimeout(this._hoverHideDelay);
-            this._hoverHideDelay = null;
-            return;
-        }
-
-        if (this.state.isOverlayShown || !isNullOrUndefinded(this._hoverShowDelay)) {
-            return;
-        }
-
-        const delay = !isNullOrUndefinded(this.props.delayShow)
-            ? this.props.delayShow
-            : this.props.delay;
-
-        if (!delay) {
-            this.show();
-            return;
-        }
-
-        this._hoverShowDelay = setTimeout(() => {
-            this._hoverShowDelay = null;
-            this.show();
-        }, delay);
-
-    },
-
-    handleDelayedHide() {
-        if (!isNullOrUndefinded(this._hoverShowDelay)) {
-            clearTimeout(this._hoverShowDelay);
-            this._hoverShowDelay = null;
-            return;
-        }
-
-        if (!this.state.isOverlayShown || !isNullOrUndefinded(this._hoverHideDelay)) {
-            return;
-        }
-
-        const delay = !isNullOrUndefinded(this.props.delayHide)
-            ? this.props.delayHide
-            : this.props.delay;
-
-        if (!delay) {
-            this.hide();
-            return;
-        }
-
-        this._hoverHideDelay = setTimeout(() => {
-            this._hoverHideDelay = null;
-            this.hide();
-        }, delay);
-    },
-
-    // Simple implementation of mouseEnter and mouseLeave.
-    // React's built version is broken: https://github.com/facebook/react/issues/4251
-    // for cases when the trigger is disabled and mouseOut/Over can cause flicker moving
-    // from one child element to another.
-    handleMouseOverOut(handler, e) {
-        let target = e.currentTarget;
-        let related = e.relatedTarget || e.nativeEvent.toElement;
-        if (!related || related !== target && !contains(target, related)) {
-            handler(e);
-        }
+    const { delayShow, delay } = this.props;
+    if (!isNullOrUndefined(this.hoverHideDelay)) {
+      clearTimeout(this.hoverHideDelay);
+      this.hoverHideDelay = null;
+      return;
     }
 
-});
+    if (this.state.isOverlayShown || !isNullOrUndefined(this.hoverShowDelay)) {
+      return;
+    }
+
+    const nextDelay = !isNullOrUndefined(delayShow) ? delayShow : delay;
+
+    if (!nextDelay) {
+      this.show();
+      return;
+    }
+
+    this.hoverShowDelay = setTimeout(() => {
+      this.hoverShowDelay = null;
+      this.show();
+    }, nextDelay);
+
+  }
+
+  handleDelayedHide() {
+    const { delayHide, delay } = this.props;
+    if (!isNullOrUndefined(this.hoverShowDelay)) {
+      clearTimeout(this.hoverShowDelay);
+      this.hoverShowDelay = null;
+      return;
+    }
+
+    if (!this.state.isOverlayShown || !isNullOrUndefined(this.hoverHideDelay)) {
+      return;
+    }
+
+    const nextDelay = !isNullOrUndefined(delayHide) ? delayHide : delay;
+
+    if (!nextDelay) {
+      this.hide();
+      return;
+    }
+
+    this.hoverHideDelay = setTimeout(() => {
+      this.hoverHideDelay = null;
+      this.hide();
+    }, nextDelay);
+  }
+
+
+  handleMouseOverOut(handler, event) {
+    let target = event.currentTarget;
+    let related = event.relatedTarget || event.nativeEvent.toElement;
+
+    if ((!related || (related !== target)) && !contains(target, related)) {
+      handler(event);
+    }
+  }
+
+  renderOverlay() {
+    ReactDOM.unstable_renderSubtreeIntoContainer(this, this.speaker, this.mountNode);
+  }
+
+  render() {
+    const {
+      children,
+      speaker,
+      onClick,
+      trigger,
+      onMouseOver,
+      onMouseOut,
+      onFocus,
+      onBlur
+    } = this.props;
+
+    const triggerComponent = React.Children.only(children);
+    const triggerProps = triggerComponent.props;
+
+    const props = {
+      'aria-describedby': speaker.props.id
+    };
+
+    this.speaker = this.getOverlay();
+
+    props.onClick = createChainedFunction(triggerProps.onClick, onClick);
+
+    if (isOneOf('click', trigger)) {
+      props.onClick = createChainedFunction(this.handleToggle, props.onClick);
+    }
+
+    if (isOneOf('hover', trigger)) {
+      props.onMouseOver = createChainedFunction(
+        this.handleMouseOver,
+        onMouseOver,
+        triggerProps.onMouseOver
+      );
+      props.onMouseOut = createChainedFunction(
+        this.handleMouseOut,
+        onMouseOut,
+        triggerProps.onMouseOut
+      );
+    }
+
+    if (isOneOf('focus', trigger)) {
+
+      props.onFocus = createChainedFunction(
+        this.handleDelayedShow,
+        onFocus,
+        triggerProps.onFocus
+      );
+
+      props.onBlur = createChainedFunction(
+        this.handleDelayedHide,
+        onBlur,
+        triggerProps.onBlur
+      );
+    }
+
+    return cloneElement(triggerComponent, props);
+  }
+}
+
+Whisper.propTypes = propTypes;
+Whisper.defaultProps = defaultProps;
 
 export default Whisper;
---
+++ test/WhisperSpec.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import ReactDOM, { findDOMNode, render } from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
+
+
+import Whisper from '../src/Whisper';
+import Tooltip from '../src/Tooltip';
+
+describe('Whisper', () => {
+
+  let mountPoint;
+  beforeEach(() => {
+    mountPoint = document.createElement('div');
+    document.body.appendChild(mountPoint);
+  });
+  afterEach(() => {
+    ReactDOM.unmountComponentAtNode(mountPoint);
+    document.body.removeChild(mountPoint);
+  });
+
+  const Container = ({ className, children }) => (
+    <div className={className}>
+      {children}
+    </div>
+  );
+
+
+  it('Should create Whisper element', () => {
+    const instance = ReactTestUtils.renderIntoDocument(
+      <Whisper speaker={<Tooltip>dd</Tooltip>}>
+        <button>Click Me</button>
+      </Whisper>
+    );
+    const whisper = findDOMNode(instance);
+    assert.equal(whisper.nodeName, 'BUTTON');
+  });
+
+
+  it('Should maintain overlay classname when trigger click', () => {
+    const instance = ReactTestUtils.renderIntoDocument(
+      <Whisper trigger="click" speaker={<Tooltip className="test-whisper">test</Tooltip>}>
+        <button>button</button>
+      </Whisper>
+    );
+
+    const whisper = findDOMNode(instance);
+    ReactTestUtils.Simulate.click(whisper);
+    assert.equal(document.getElementsByClassName('test-whisper').length, 1);
+  });
+
+  it('Should maintain overlay classname when trigger focus', () => {
+    const instance = ReactTestUtils.renderIntoDocument(
+      <Whisper trigger="focus" speaker={<Tooltip className="test-whisper">test</Tooltip>}>
+        <button>button</button>
+      </Whisper>
+    );
+
+    const whisper = findDOMNode(instance);
+    ReactTestUtils.Simulate.blur(whisper);
+    assert.equal(document.getElementsByClassName('test-whisper').length, 1);
+  });
+
+  it('Should call onClick callback', (done) => {
+    const doneOp = () => {
+      done();
+    };
+    const instance = ReactTestUtils.renderIntoDocument(
+      <Whisper onClick={doneOp} trigger="click" speaker={<Tooltip className="test-whisper">test</Tooltip>}>
+        <button>button</button>
+      </Whisper>
+    );
+
+    const whisper = findDOMNode(instance);
+    ReactTestUtils.Simulate.click(whisper);
+  });
+
+
+  it('Should pass transition callbacks to Transition', (done) => {
+    let count = 0;
+    const increment = () => {
+      count += 1;
+    };
+
+    let whisper;
+
+    const instance = ReactTestUtils.renderIntoDocument(
+      <Whisper
+        trigger="click"
+        speaker={<Tooltip>test</Tooltip>}
+        onExit={increment}
+        onExiting={increment}
+        onExited={() => {
+          increment();
+          if (count === 6) {
+            done();
+          }
+        }}
+        onEnter={increment}
+        onEntering={increment}
+        onEntered={() => {
+          increment();
+          ReactTestUtils.Simulate.click(whisper);
+        }}
+      >
+        <button>button</button>
+      </Whisper>
+    );
+
+    whisper = findDOMNode(instance);
+    ReactTestUtils.Simulate.click(whisper);
+  });
+
+});
