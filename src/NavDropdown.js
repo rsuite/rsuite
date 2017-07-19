50f8bc49b079775f876450919ac01b9baee674b4
simonguo <simonguo.2009@gmail.com>
2017-07-19 13:49:56 +0800

    Added support active and disabled in `NavDropdown`

--- src/NavDropdown.js
+++ src/NavDropdown.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Dropdown from './Dropdown';
+
+const propTypes = {
+  active: PropTypes.bool,
+  disabled: PropTypes.bool
+};
 
 class NavDropdown extends React.Component {
   getMountNode() {
     return this.mountNode;
   }
   render() {
-    const { className, ...props } = this.props;
+    const { className, active, disabled, ...props } = this.props;
+    const classes = classNames('nav-dropdown', {
+      active,
+      disabled
+    }, className);
+
     return (
       <Dropdown
         {...props}
         componentClass="li"
         useAnchor
-        className={classNames('nav-dropdown', className)}
+        className={classes}
+        disabled={disabled}
         ref={(ref) => {
           this.mountNode = ref;
         }}
@@ -21,5 +33,7 @@ class NavDropdown extends React.Component {
     );
   }
 }
+
+NavDropdown.propTypes = propTypes;
 
 export default NavDropdown;
--- test/NavDropdownSpec.js
+++ test/NavDropdownSpec.js
@@ -16,6 +16,20 @@ describe('NavDropdown', () => {
     assert.equal(findDOMNode(instance).innerText, title);
   });
 
+  it('Should be active', () => {
+    let instance = ReactTestUtils.renderIntoDocument(
+      <NavDropdown active />
+    );
+    assert.ok(findDOMNode(instance).className.match(/\bactive\b/));
+  });
+
+  it('Should be disabled', () => {
+    let instance = ReactTestUtils.renderIntoDocument(
+      <NavDropdown disabled />
+    );
+    assert.ok(findDOMNode(instance).className.match(/\bdisabled\b/));
+  });
+
 
   it('Should have a custom className', () => {
     let instance = ReactTestUtils.renderIntoDocument(
