08f1139e5f85e8309404af69f34d9f106da8d8bf
simonguo <simonguo.2009@gmail.com>
2017-07-19 14:38:55 +0800

    Added support custom className in `NavDropdown`

--- src/NavDropdown.js
+++ src/NavDropdown.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import classNames from 'classnames';
-
 import Dropdown from './Dropdown';
 
 const NavDropdown = React.createClass({
 
-    getMountNode() {
-        return this.mountNode;
-    },
-    render: function () {
+  getMountNode() {
+    return this.mountNode;
+  },
+  render: function () {
+    const { className, ...props } = this.props;
+    const classes = classNames({
+      'nav-dropdown': true
+    }, className);
 
-        let clesses = classNames({
-            'nav-dropdown': true
-        });
-
-        return (
-            <Dropdown
-
-                {...this.props}
-                ref={ref => this.mountNode = ref}
-                componentClass="li"
-                useAnchor
-                className={clesses}
-            >
-                {this.props.children}
-            </Dropdown>
-        );
-    }
+    return (
+      <Dropdown
+        {...props}
+        ref={ref => this.mountNode = ref}
+        componentClass="li"
+        useAnchor
+        className={classes}
+      />
+    );
+  }
 });
 
 export default NavDropdown;
