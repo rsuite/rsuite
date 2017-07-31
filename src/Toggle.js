6a84515ab759ae7514d4ec79076c9661744d8aff
simonguo <simonguo.2009@gmail.com>
2017-07-31 14:27:49 +0800

    Add Toggle

---
+++ src/Toggle.js
@@ -0,0 +1,99 @@
+import classNames from 'classnames';
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import decorate, { getClassNames } from './utils/decorate';
+
+
+const propTypes = {
+  title: PropTypes.string,
+  inline: PropTypes.bool,
+  disabled: PropTypes.bool,
+  checked: PropTypes.bool,
+  defaultChecked: PropTypes.bool,
+  onChange: PropTypes.func,
+  inputRef: PropTypes.func,
+  checkedChildren: PropTypes.node,
+  unCheckedChildren: PropTypes.node,
+  prefixClass: PropTypes.string
+};
+
+const defaultProps = {
+  prefixClass: 'toggle'
+};
+
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      checked: props.checked || props.defaultChecked
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.checked !== this.props.checked) {
+      this.setState({
+        checked: nextProps.checked
+      });
+    }
+  }
+  handleChange(event) {
+    const { onChange, disabled } = this.props;
+    const checked = !this.state.checked;
+
+    if (disabled) {
+      return;
+    }
+
+    this.setState({ checked }, () => {
+      onChange && onChange(checked, event);
+    });
+  }
+
+  render() {
+
+    const {
+      inline,
+      disabled,
+      className,
+      onChange,
+      checkedChildren,
+      unCheckedChildren,
+      prefixClass,
+      ...props
+    } = this.props;
+
+    const { checked } = this.state;
+    const classes = classNames({
+      ...getClassNames(this.props),
+      [`${prefixClass}-checked`]: checked,
+      [`${prefixClass}-inline`]: inline,
+      disabled
+    }, className);
+
+
+    const inner = checked ? checkedChildren : unCheckedChildren;
+    const elementProps = _.omit(props, Object.keys(propTypes));
+
+    return (
+      <span
+        {...elementProps}
+        className={classes}
+        role="button"
+        tabIndex={-1}
+        onClick={this.handleChange}
+      >
+        <span className="toggle-inner">{inner}</span>
+      </span>
+    );
+
+  }
+}
+
+Toggle.propTypes = propTypes;
+Toggle.defaultProps = defaultProps;
+
+export default decorate({
+  size: true
+})(Toggle);
