014b90081c9b0484e7dd0fbf68127e1267586a43
simonguo <simonguo.2009@gmail.com>
2017-08-25 13:42:50 +0800

    test travis

--- test/index.js
+++ test/index.js
@@ -1,2 +1,6 @@
+/* eslint-disable */
+require('babel-polyfill');
+require('core-js/fn/object/assign');
+
 const testsContext = require.context('.', true, /Spec.js$/);
 testsContext.keys().forEach(testsContext);
