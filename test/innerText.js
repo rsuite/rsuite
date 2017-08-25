b6a1efa6da0d6dab381e127364eb6a45545c9493
simonguo <simonguo.2009@gmail.com>
2017-08-25 17:09:48 +0800

    test travis

--- test/innerText.js
+++ test/innerText.js
@@ -1,7 +1,9 @@
 export default function innerText(node) {
 
   if (window.navigator.userAgent.toLowerCase().indexOf('firefox') !== -1) {
+    console.log('1', node.innerContent);
     return node.innerContent;
   }
+  console.log('2');
   return node.innerText;
 }
