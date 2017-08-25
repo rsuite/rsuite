ba074f527fd153e225ff4c21512258b0ff6a1e22
simonguo <simonguo.2009@gmail.com>
2017-08-25 14:44:07 +0800

    test travis

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+require('babel-core/register');
 
 const webpackConfig = {
   output: {
--- test/index.js
+++ test/index.js
@@ -1,6 +1,2 @@
-/* eslint-disable */
-require('babel-polyfill');
-require('core-js');
-
 const testsContext = require.context('.', true, /Spec.js$/);
 testsContext.keys().forEach(testsContext);
