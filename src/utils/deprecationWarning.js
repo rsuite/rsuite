5963a9838881f18629b3a55b768e7771841a1f94
simonguo <simon.guo@hypers.com>
2017-12-27 16:16:44 +0800

    Add lowPriorityWarning function

---
+++ src/utils/deprecationWarning.js
@@ -0,0 +1,47 @@
+
+/**
+ * Forked from react-bootstrap/react-bootstrap:
+ * https://github.com/react-bootstrap/react-bootstrap/blob/master/src/utils/deprecationWarning.js
+ */
+
+import lowPriorityWarning from './lowPriorityWarning';
+
+let warned = {};
+
+function deprecationWarning(oldname, newname, link) {
+  let message;
+
+  if (typeof oldname === 'object') {
+    message = oldname.message;
+  } else {
+    message = `${oldname} is deprecated. Use ${newname} instead.`;
+
+    if (link) {
+      message += `\nYou can read more about it at ${link}`;
+    }
+  }
+
+  if (warned[message]) {
+    return;
+  }
+
+  lowPriorityWarning(false, message);
+  warned[message] = true;
+}
+
+
+deprecationWarning.wrapper = (Component, ...args) => class DeprecatedComponent extends Component {
+  componentWillMount(...methodArgs) {
+    deprecationWarning(...args);
+
+    if (super.componentWillMount) {
+      super.componentWillMount(...methodArgs);
+    }
+  }
+};
+
+export default deprecationWarning;
+
+export function resetWarned() {
+  warned = {};
+}
