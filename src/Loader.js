0c486033c177b8337710fde2f36bc84660f40aad
simonguo <simon.guo@hypers.com>
2018-01-02 17:49:19 +0800

    Added Loader

---
+++ src/Loader.js
@@ -0,0 +1,64 @@
+// @flow
+
+import * as React from 'react';
+import classNames from 'classnames';
+import withStyleProps from './utils/withStyleProps';
+import prefix, { globalKey } from './utils/prefix';
+
+type Props = {
+  className?: string,
+  classPrefix: string,
+  center?: boolean,
+  backdrop?: boolean,
+  inverse?: boolean,
+  content?: React.Node,
+  speed?: 'normal' | 'fast' | 'slow'
+};
+
+class Loader extends React.Component<Props> {
+
+  static defaultProps = {
+    classPrefix: `${globalKey}loader`,
+    speed: 'normal'
+  };
+
+  addPrefix(name: string) {
+    return prefix(this.props.classPrefix)(name);
+  }
+
+  render() {
+
+    const {
+      classPrefix,
+      className,
+      inverse,
+      backdrop,
+      speed,
+      center,
+      content,
+      ...props
+    } = this.props;
+
+    const addPrefix = prefix(classPrefix);
+    const classes = classNames(classPrefix, addPrefix(speed), {
+      [`${addPrefix(backdrop)}-wrapper`]: backdrop,
+      [addPrefix(inverse)]: inverse,
+      [addPrefix(center)]: center
+    }, className);
+
+    return (
+      <div
+        {...props}
+        className={classes}
+      >
+        {backdrop && <div className={addPrefix(backdrop)} />}
+        <span className={addPrefix('spin')} />
+        <span className={addPrefix('content')}>{content}</span>
+      </div>
+    );
+  }
+}
+
+export default withStyleProps({
+  hasSize: true
+})(Loader);
