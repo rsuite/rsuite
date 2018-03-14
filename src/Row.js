257cc058d372b1475b1ee878f3909f7206463a3c
simonguo <simon.guo@hypers.com>
2018-03-14 23:03:47 +0800

    Update Row

--- src/Row.js
+++ src/Row.js
@@ -2,20 +2,60 @@
 
 import * as React from 'react';
 import classNames from 'classnames';
-import { defaultProps } from './utils';
+import { defaultProps, ReactChildren } from './utils';
+import Col from './Col';
 
 type Props = {
   className?: string,
   classPrefix?: string,
-  componentClass: React.ElementType
+  gutter?: number,
+  style?: Object,
+  componentClass: React.ElementType,
+  children: React.ChildrenArray<React.Element<typeof Col>>
 };
 
 class Row extends React.Component<Props> {
   render() {
-    const { className, componentClass: Component, classPrefix, ...props } = this.props;
+    const {
+      className,
+      gutter,
+      children,
+      componentClass: Component,
+      classPrefix,
+      style,
+      ...props
+    } = this.props;
+
     const classes = classNames(classPrefix, className);
 
-    return <Component {...props} className={classes} />;
+    if (typeof gutter !== 'undefined') {
+      const padding = gutter / 2;
+      const cols = ReactChildren.mapCloneElement(children, child => ({
+        ...child.props,
+        style: {
+          ...child.props.style,
+          paddingLeft: padding,
+          paddingRight: padding
+        }
+      }));
+      const styles = {
+        ...style,
+        marginLeft: -padding,
+        marginRight: -padding
+      };
+
+      return (
+        <Component {...props} className={classes} style={styles}>
+          {cols}
+        </Component>
+      );
+    }
+
+    return (
+      <Component {...props} className={classes}>
+        {children}
+      </Component>
+    );
   }
 }
 
