c9fb7824ca881ad3b292a19cd481a8351755914f
simonguo <simon.guo@hypers.com>
2018-04-13 14:32:38 +0800

    Update webpack to 4.5.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,22 @@
 const path = require('path');
-const webpack = require('webpack');
-
-const isPublish = process.env.NODE_ENV === 'publish';
-const plugins = [];
-
-if (isPublish) {
-  plugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false
-    }
-  }));
-  plugins.push(new webpack.BannerPlugin(`Last update: ${new Date().toString()}`));
-}
 
 module.exports = {
   entry: {
-    suite: path.join(__dirname, 'src')
+    rsuite: path.join(__dirname, 'src')
   },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
-    library: 'RSuite',
+    library: 'rsuite',
     libraryTarget: 'umd'
   },
-  plugins,
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      use: [
-        'babel-loader?babelrc'
-      ],
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: ['babel-loader?babelrc'],
+        exclude: /node_modules/
+      }
+    ]
   }
 };
