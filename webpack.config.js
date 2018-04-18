e3a627c79813e93b82e906f1b6d650ce457b701f
simonguo <simon.guo@hypers.com>
2018-04-18 14:43:01 +0800

    Update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,35 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: {
     rsuite: path.join(__dirname, 'src')
   },
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].min.js',
+    path: path.join(__dirname, 'dist/js'),
+    filename: '[name].production.min.js',
     library: 'rsuite',
     libraryTarget: 'umd'
   },
   externals: {
-    react: 'react',
-    'react-dom': 'react-dom'
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom'
+    },
+    moment: {
+      root: 'moment',
+      commonjs2: 'moment',
+      commonjs: 'moment',
+      amd: 'moment'
+    }
   },
   module: {
     rules: [
@@ -23,4 +40,5 @@ module.exports = {
       }
     ]
   }
+  //plugins: [new BundleAnalyzerPlugin()]
 };
