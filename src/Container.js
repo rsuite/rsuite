bd9d2f9d4024a4612d5eb0a365f42a703d54d9b6
simonguo <simon.guo@hypers.com>
2018-05-03 22:17:54 +0800

    Bugfix: The error is thrown when children is false in `Container`

--- src/Container.js
+++ src/Container.js
@@ -2,22 +2,24 @@
 
 import * as React from 'react';
 import classNames from 'classnames';
+import _ from 'lodash';
+
 import { defaultProps, prefix } from './utils';
 
 type Props = {
   className?: string,
-  children: React.ChildrenArray<React.Element<any>>,
+  children?: React.ChildrenArray<React.Element<any>>,
   classPrefix?: string
 };
 
 class Container extends React.Component<Props> {
   render() {
-    const { className, children, classPrefix, ...props } = this.props;
+    const { className, children = [], classPrefix, ...props } = this.props;
     const addPrefix = prefix(classPrefix);
     let hasSidebar = false;
 
     React.Children.forEach(children, item => {
-      if (item.type.displayName === 'Sidebar') {
+      if (_.get(item, 'type.displayName') === 'Sidebar') {
         hasSidebar = true;
       }
     });
