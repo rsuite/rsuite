ecd5731653797951e54df1065550c93256b31fd5
simonguo <simon.guo@hypers.com>
2018-05-24 17:59:14 +0800

    Update tests

--- test/RadioSpec.js
+++ test/RadioSpec.js
@@ -3,55 +3,53 @@ import { findDOMNode } from 'react-dom';
 import ReactTestUtils from 'react-dom/test-utils';
 
 import Radio from '../src/Radio';
+import { getDOMNode } from './TestWrapper';
 
 describe('Radio', () => {
   it('Should render a radio', () => {
-    const instance = ReactTestUtils.renderIntoDocument(<Radio>Test</Radio>);
-    const instanceDom = findDOMNode(instance);
-    assert.equal(instanceDom.querySelectorAll('input[type="radio"]').length, 1);
+    const instance = getDOMNode(<Radio>Test</Radio>);
+    assert.equal(instance.querySelectorAll('input[type="radio"]').length, 1);
   });
 
   it('Should add title', () => {
     const title = 'Text';
-    const instance = ReactTestUtils.renderIntoDocument(<Radio title={title}>Test</Radio>);
-    const instanceDom = findDOMNode(instance);
-    assert.equal(instanceDom.querySelector('label').title, title);
+    const instance = getDOMNode(<Radio title={title}>Test</Radio>);
+    assert.equal(instance.querySelector('label').title, title);
   });
 
   it('Should have radio-inline class', () => {
-    const instance = ReactTestUtils.renderIntoDocument(<Radio inline>Test</Radio>);
-    assert.ok(findDOMNode(instance).className.match(/\bradio-inline\b/));
+    const instance = getDOMNode(<Radio inline>Test</Radio>);
+    assert.ok(instance.className.match(/\bradio-inline\b/));
   });
 
   it('Should be disabled', () => {
-    const instance = ReactTestUtils.renderIntoDocument(<Radio disabled>Test</Radio>);
-    const instanceDom = findDOMNode(instance);
-    assert.ok(instanceDom.querySelector('input').disabled);
-    assert.ok(instanceDom.className.match(/\bradio-disabled\b/));
+    const instance = getDOMNode(<Radio disabled>Test</Radio>);
+    assert.ok(instance.querySelector('input').disabled);
+    assert.ok(instance.className.match(/\bradio-disabled\b/));
   });
 
   it('Should be checked', () => {
-    const instance = ReactTestUtils.renderIntoDocument(<Radio checked>Test</Radio>);
-    const instanceDom = findDOMNode(instance);
-    assert.ok(instanceDom.className.match(/\bradio-checked\b/));
+    const instance = getDOMNode(<Radio checked>Test</Radio>);
+
+    assert.ok(instance.className.match(/\bradio-checked\b/));
   });
 
   it('Should be defaultChecked', () => {
-    const instance = ReactTestUtils.renderIntoDocument(<Radio defaultChecked>Test</Radio>);
-    const instanceDom = findDOMNode(instance);
-    assert.ok(instanceDom.className.match(/\bradio-checked\b/));
+    const instance = getDOMNode(<Radio defaultChecked>Test</Radio>);
+
+    assert.ok(instance.className.match(/\bradio-checked\b/));
   });
 
   it('Should have a `Test` value', () => {
     const value = 'Test';
-    const instance = ReactTestUtils.renderIntoDocument(<Radio defaultValue={value}>Test</Radio>);
-    const instanceDom = findDOMNode(instance);
-    assert.equal(instanceDom.querySelector('input').value, value);
+    const instance = getDOMNode(<Radio defaultValue={value}>Test</Radio>);
+
+    assert.equal(instance.querySelector('input').value, value);
   });
 
   it('Should support inputRef', () => {
     let input;
-    ReactTestUtils.renderIntoDocument(<Radio inputRef={ref => (input = ref)}>Test</Radio>);
+    getDOMNode(<Radio inputRef={ref => (input = ref)}>Test</Radio>);
     assert.ok(input);
   });
 
@@ -59,8 +57,8 @@ describe('Radio', () => {
     const doneOp = data => {
       done();
     };
-    const instance = ReactTestUtils.renderIntoDocument(<Radio onClick={doneOp}>Title</Radio>);
-    ReactTestUtils.Simulate.click(findDOMNode(instance).querySelector('label'));
+    const instance = getDOMNode(<Radio onClick={doneOp}>Title</Radio>);
+    ReactTestUtils.Simulate.click(instance.querySelector('label'));
   });
 
   it('Should call onChange callback', done => {
@@ -71,12 +69,12 @@ describe('Radio', () => {
       }
     };
 
-    const instance = ReactTestUtils.renderIntoDocument(
+    const instance = getDOMNode(
       <Radio onChange={doneOp} value={value}>
         Title
       </Radio>
     );
-    ReactTestUtils.Simulate.change(findDOMNode(instance).querySelector('input'));
+    ReactTestUtils.Simulate.change(instance.querySelector('input'));
   });
 
   it('Should be checked with change', done => {
@@ -86,23 +84,23 @@ describe('Radio', () => {
       }
     };
 
-    const instance = ReactTestUtils.renderIntoDocument(
+    const instance = getDOMNode(
       <Radio onChange={doneOp} value="100">
         Title
       </Radio>
     );
-    const instanceDom = findDOMNode(instance);
-    ReactTestUtils.Simulate.change(instanceDom.querySelector('input'));
+
+    ReactTestUtils.Simulate.change(instance.querySelector('input'));
   });
 
   it('Should have a custom className', () => {
-    const instance = ReactTestUtils.renderIntoDocument(<Radio className="custom" />);
-    assert.ok(findDOMNode(instance).className.match(/\bcustom\b/));
+    const instance = getDOMNode(<Radio className="custom" />);
+    assert.ok(instance.className.match(/\bcustom\b/));
   });
 
   it('Should have a custom style', () => {
     const fontSize = '12px';
-    const instance = ReactTestUtils.renderIntoDocument(<Radio style={{ fontSize }} />);
-    assert.equal(findDOMNode(instance).style.fontSize, fontSize);
+    const instance = getDOMNode(<Radio style={{ fontSize }} />);
+    assert.equal(instance.style.fontSize, fontSize);
   });
 });
