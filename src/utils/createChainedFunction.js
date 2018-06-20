b23b32e346bbbcd56422d7bd57ba1bb3f7b2014d
simonguo <simon.guo@hypers.com>
2018-06-20 23:42:08 +0800

    format code

--- src/utils/createChainedFunction.js
+++ src/utils/createChainedFunction.js
@@ -16,7 +16,7 @@ function createChainedFunction(...funcs) {
     if (typeof f !== 'function') {
       throw new Error('Invalid Argument Type, must only provide functions, undefined, or null.');
     }
-    
+
     if (acc === undefined) {
       return f;
     }
