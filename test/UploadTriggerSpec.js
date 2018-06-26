1578a919da72a00e51ec4f75be65bb888c169ab0
Simon Guo <simonguo.2009@gmail.com>
2018-06-26 18:27:26 +0800

    Feature: Support `toggleComponentClass` on `<Uploader>` (#104)
    * Support `toggleComponentClass` on `<Uploader>`
    * Update CHANGELOG.md

--- test/UploadTriggerSpec.js
+++ test/UploadTriggerSpec.js
@@ -1,51 +1,57 @@
 import React from 'react';
 import { findDOMNode } from 'react-dom';
 import ReactTestUtils from 'react-dom/test-utils';
+import { getDOMNode, getInstance } from './TestWrapper';
 
 import UploadTrigger from '../src/UploadTrigger';
 
 describe('UploadTrigger', () => {
   it('Should output a UploadTrigger', () => {
-    const instance = ReactTestUtils.renderIntoDocument(<UploadTrigger />);
-    assert.include(findDOMNode(instance).className, 'rs-uploader-trigger');
+    const instance = getDOMNode(<UploadTrigger />);
+    assert.include(instance.className, 'rs-uploader-trigger');
   });
 
   it('Should be disabled', () => {
-    const instance = ReactTestUtils.renderIntoDocument(<UploadTrigger disabled />);
-    assert.include(findDOMNode(instance).className, 'rs-uploader-trigger-disabled');
+    const instance = getDOMNode(<UploadTrigger disabled />);
+    assert.include(instance.className, 'rs-uploader-trigger-disabled');
   });
 
   it('Should be multipled', () => {
-    const instance = ReactTestUtils.renderIntoDocument(<UploadTrigger multiple />);
-    assert.ok(findDOMNode(instance).querySelector('input[multiple]'));
+    const instance = getDOMNode(<UploadTrigger multiple />);
+    assert.ok(instance.querySelector('input[multiple]'));
   });
 
   it('Should have a accept', () => {
-    const instance = ReactTestUtils.renderIntoDocument(<UploadTrigger accept=".jpg" />);
-    assert.ok(findDOMNode(instance).querySelector('input[accept=".jpg"]'));
+    const instance = getDOMNode(<UploadTrigger accept=".jpg" />);
+    assert.ok(instance.querySelector('input[accept=".jpg"]'));
+  });
+
+  it('Should render custom component', () => {
+    const instance = getDOMNode(<UploadTrigger componentClass={'a'} />);
+    assert.equal(instance.querySelector('.rs-uploader-trigger-btn').tagName, 'A');
   });
 
   it('Should call onChange callback', done => {
     const doneOp = () => {
       done();
     };
-    const instance = ReactTestUtils.renderIntoDocument(<UploadTrigger onChange={doneOp} />);
-    ReactTestUtils.Simulate.change(findDOMNode(instance).querySelector('input'));
+    const instance = getDOMNode(<UploadTrigger onChange={doneOp} />);
+    ReactTestUtils.Simulate.change(instance.querySelector('input'));
   });
 
   it('Should have a name', () => {
-    const instance = ReactTestUtils.renderIntoDocument(<UploadTrigger name="file" />);
-    assert.ok(findDOMNode(instance).querySelector('input[name="file"]'));
+    const instance = getDOMNode(<UploadTrigger name="file" />);
+    assert.ok(instance.querySelector('input[name="file"]'));
   });
 
   it('Should have a custom className', () => {
-    const instance = ReactTestUtils.renderIntoDocument(<UploadTrigger className="custom" />);
-    assert.include(findDOMNode(instance).className, 'custom');
+    const instance = getDOMNode(<UploadTrigger className="custom" />);
+    assert.include(instance.className, 'custom');
   });
 
   it('Should have a custom style', () => {
     const fontSize = '12px';
-    const instance = ReactTestUtils.renderIntoDocument(<UploadTrigger style={{ fontSize }} />);
-    assert.equal(findDOMNode(instance).querySelector('button').style.fontSize, fontSize);
+    const instance = getDOMNode(<UploadTrigger style={{ fontSize }} />);
+    assert.equal(instance.querySelector('button').style.fontSize, fontSize);
   });
 });
