312eb5424ff7212a240f7a04d51f349fa4a41089
Simon Guo <simonguo.2009@gmail.com>
2018-06-26 17:06:33 +0800

    Update table to ~3.0.4 (#103)

--- src/Table.js
+++ src/Table.js
@@ -1,10 +1,14 @@
 import setStatic from 'recompose/setStatic';
+import defaultProps from 'recompose/defaultProps';
+import compose from 'recompose/compose';
 import { Table, Column, Cell, HeaderCell } from 'rsuite-table';
 
 import withLocale from './IntlProvider/withLocale';
 import TablePagination from './TablePagination';
 
-const EnhancedLocaleTable = withLocale(['Table'])(Table);
+const EnhancedLocaleTable = compose(withLocale(['Table']), defaultProps({ loadAnimation: true }))(
+  Table
+);
 
 setStatic('Column', Column)(EnhancedLocaleTable);
 setStatic('Cell', Cell)(EnhancedLocaleTable);
