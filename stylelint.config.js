0456d33009ee52d936b422c6a6b56d0e7afc3e4d
Godfery <hiyangguo@qq.com>
2018-08-29 14:53:30 +0800

    update:Reformat code style with prettier.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,50 +1,49 @@
 module.exports = {
-  'extends': 'stylelint-config-standard',
-  'rules': {
-    // 覆盖 stylelint-config-standard 规则
-    // `stylelint-config-standard`中的一些规则并不适用，这里进行了覆盖
+  extends: 'stylelint-config-standard',
+  rules: {
+    // Overwrite stylelint-config-standard rules
     // ----------------------------------
 
-    // 不检查函数之后是否有空白
-    // style-lint 无法区分 less 的 mixin 还是 css function 这里直接禁用掉该检查项
+    // Don't check if there is a whitespace after the function.
+    // Style-lint can't distinguish less mixin or css function. Disable this check item directly here.
     'function-whitespace-after': null,
-    // 不允许小于1数字的前导零
-    'number-leading-zero': 'never',
+    // Require a leading zero for fractional numbers less than 1.
+    'number-leading-zero': 'always',
 
-    // 扩充规则
-    // - 启用了最严格的 vendor-prefix 限制，该限制会适用 {@link https://caniuse.com} 的数据进行判断
+    // Extended rules
+    // - The most restrictive vendor-prefix limit is enabled, which is determined by the data of {@link https://caniuse.com}
     // ----------------------------------
 
-    // 禁止 at 规则（ @keyframes ）使用浏览器引擎前缀
+    // Forbidden the at rule (eg: @keyframes ) to use the browser prefix.
     'at-rule-no-vendor-prefix': true,
-    // 指定一个在声明中允许使用的属性和单位的白名单
+    // Specify a whitelist of attributes and units allowed in the declaration.
     'declaration-property-unit-whitelist': {
       'font-size': ['px', 'em', '%'],
-      '/^animation/': ['s'],
+      '/^animation/': ['s']
     },
-    // 禁止 media 特性名称（ @media ）带有浏览器引擎前缀
+    // Forbidden @media  use the browser prefix.
     'media-feature-name-no-vendor-prefix': true,
-    // 限制允许嵌套的深度
+    // Limit the depth of nesting allowed.
     'max-nesting-depth': 5,
-    // 不禁止低优先级的选择器出现在高优先级的选择器之后
-    // [优先级的描述]{@link https://www.w3.org/TR/css3-selectors/#specificity}
+    // Don't forbidden low specificity selectors from appearing after high specificity selectors.
+    // [Specificity description]{@link https://www.w3.org/TR/css3-selectors/#specificity}
     'no-descending-specificity': null,
-    // 类选择器使用 snake case
+    // Class selector naming rules use snake case. eg: .class-name
     'selector-class-pattern': '^[a-z0-9]+(-?[a-z0-9]+)*$',
-    // ID 选择器使用小驼峰命名
-    'selector-id-pattern': '^([a-z]+([A-Z][a-z]+)?)$',
-    // 禁止属性使用浏览器引擎前缀
+    // ID selector naming rules use camel case , excepted `icon-`. eg: #loginButton,#icon-login
+    'selector-id-pattern': '^([a-z]+([A-Z][a-z]+)?)||(icon-([a-z-]+))$',
+    // Forbidden use the browser prefix.
     'property-no-vendor-prefix': true,
-    // 限制复合选择器的数量
-    'selector-max-compound-selectors': 6,
-    // 限制 ID 选择器的数量
-    'selector-max-id': 1,
-    // 限制通配符（*）在一个选择器的数量
-    'selector-max-universal': 1,
-    // 禁止使用浏览器引擎前缀
     'selector-no-vendor-prefix': true,
-    // 指定一个允许使用的伪类选择器的白名单(仅可使用 IE9+ 浏览器支持的伪类)
-    // [CSS选择器的浏览器支持]{@link https://labs.qianduan.net/css-selector/}
+    'value-no-vendor-prefix': true,
+    // Limit the number of composite selectors in a selector.
+    'selector-max-compound-selectors': 6,
+    // Limit the number of ID selectors in a selector.
+    'selector-max-id': 1,
+    // Limit the number of universal selectors in a selector.
+    'selector-max-universal': 1,
+    // Specify a whitelist of allowed pseudo-class selectors.(Only use pseudo-classes supported by IE10+ browsers)
+    // [CSS selector browser support]{@link https://labs.qianduan.net/css-selector/}
     'selector-pseudo-class-whitelist': [
       // CSS 1
       'active',
@@ -73,10 +72,9 @@ module.exports = {
       'root',
       'target'
     ],
-    // 字符串使用双引号
-    'string-quotes': 'double',
-    // 禁止给值添加浏览器引擎前缀
-    'value-no-vendor-prefix': true,
+    // String using single quotes
+    'string-quotes': 'single',
+    'declaration-colon-newline-after': null,
+    'value-list-comma-newline-after': 'always-multi-line'
   }
 };
-
