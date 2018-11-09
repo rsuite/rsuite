01c180b0697a9f6831d00a4d1507743979e43c0f
simonguo <simon.guo@hypers.com>
2018-11-09 15:50:11 +0800

    Added `countable` for Picker in types

--- types/CheckPicker.d.ts
+++ types/CheckPicker.d.ts
@@ -6,6 +6,9 @@ import { SelectProps } from './SelectPicker';
 export interface CheckPickerProps extends FormControlPickerProps<any[]>, SelectProps<any[]> {
   /** Top the selected option in the options */
   sticky?: boolean;
+
+  /** A picker that can be counted */
+  countable?: boolean;
 }
 
 declare const CheckPicker: React.ComponentType<CheckPickerProps>;
