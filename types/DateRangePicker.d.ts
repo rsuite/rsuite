b83b3dfedf483691dd2817a9d3550fc39a61e842
simonguo <simon.guo@hypers.com>
2019-01-07 09:53:57 +0800

    Update d.ts files for component props

--- types/DateRangePicker.d.ts
+++ types/DateRangePicker.d.ts
@@ -27,9 +27,6 @@ export interface DateRangePickerProps extends PickerBaseProps, FormControlBasePr
   /** ISO 8601 standard, each calendar week begins on Monday and Sunday on the seventh day */
   isoWeek?: boolean;
 
-  /** Set an optional year limit relative to the current selection date */
-  limitStartYear?: number;
-
   /** Set the lower limit of the available year relative to the current selection date */
   limitEndYear?: number;
 
