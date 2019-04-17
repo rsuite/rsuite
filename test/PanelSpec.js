946b9b91ad505a4a717a210bb168a177b156cc93
simonguo <simonguo.2009@gmail.com>
2019-04-17 10:04:30 +0800

    Bugfix: Remove invalid anchors in the Panel title

--- test/PanelSpec.js
+++ test/PanelSpec.js
@@ -33,7 +33,7 @@ describe('Panel', () => {
 
   it('Should have a role in header', () => {
     const instance = getDOMNode(<Panel headerRole="button" collapsible header={'abc'} />);
-    assert.equal(instance.querySelector('h4 a').getAttribute('role'), 'button');
+    assert.equal(instance.querySelector('h4 span').getAttribute('role'), 'button');
   });
 
   it('Should have a role in header', () => {
