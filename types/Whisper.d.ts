2983be68238394378b6ba3f488844d7550a3a0d3
simonguo <simonguo.2009@gmail.com>
2019-05-13 16:19:36 +0800

    Support `open` on `<Whisper>`

--- types/Whisper.d.ts
+++ types/Whisper.d.ts
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { AnimationEventProps, PropTypes } from '.';
+import { AnimationEventProps, StandardProps, PropTypes } from '.';
 import { TooltipProps } from './Tooltip';
 import { PopoverProps } from './Popover';
 
-export interface WhisperProps extends AnimationEventProps {
+export interface WhisperProps extends AnimationEventProps, StandardProps {
   /** Triggering events */
   trigger?: 'click' | 'hover' | 'focus' | 'active';
 
@@ -27,6 +27,9 @@ export interface WhisperProps extends AnimationEventProps {
 
   /** display element */
   speaker?: React.ReactElement<TooltipProps | PopoverProps>;
+
+  /** Show speaker */
+  open?: boolean;
 
   /** Lose Focus callback function */
   onBlur?: () => void;
