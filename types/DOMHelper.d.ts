2c4005fa24d5591d9f4d13202566b0dd9c29d47e
simonguo <simonguo.2009@gmail.com>
2019-05-15 11:01:35 +0800

    Update types/DOMHelper.d.ts

--- types/DOMHelper.d.ts
+++ types/DOMHelper.d.ts
@@ -1,34 +1,60 @@
+type DOMElement = HTMLElement | Window;
+type Offset = {
+  top: number;
+  left: number;
+  height: number;
+  width: number;
+};
+
 export interface DOMHelperAPI {
   /** classes */
-  hasClass: (node: HTMLElement, className: string) => boolean;
-  addClass: (node: HTMLElement, className: string) => HTMLElement;
-  removeClass: (node: HTMLElement, className: string) => HTMLElement;
-  toggleClass: (node: HTMLElement, className: string) => HTMLElement;
+  hasClass(node: HTMLElement, className: string): boolean;
+  addClass(node: HTMLElement, className: string): HTMLElement;
+  removeClass(node: HTMLElement, className: string): HTMLElement;
+  toggleClass(node: HTMLElement, className: string): HTMLElement;
 
   /** styles */
-  getStyle: (node: HTMLElement, property?: string) => object;
-  addStyle: (node: HTMLElement, property: string | object, value?: string | number) => void;
+  getStyle(node: HTMLElement): React.CSSProperties;
+  getStyle(node: HTMLElement, property?: string): string | number;
+
+  addStyle(node: HTMLElement, propertys: React.CSSProperties): void;
+  addStyle(node: HTMLElement, property: string, value: string | number): void;
   removeStyle: (node: HTMLElement, keys: string | string[]) => void;
-  translateDOMPositionXY: (style: object, x: number, y: number) => object;
+  translateDOMPositionXY: (style: React.CSSProperties, x: number, y: number) => React.CSSProperties;
 
   /** events */
-  on: (target: HTMLElement, eventName: string, listener: () => void, capture?: boolean) => object;
-  off: (target: HTMLElement, eventName: string, listener: () => void, capture?: boolean) => void;
+  on(
+    target: DOMElement,
+    eventName: string,
+    listener: (event: EventInit) => void,
+    capture?: boolean
+  ): {
+    off: () => void;
+  };
+
+  off(
+    target: DOMElement,
+    eventName: string,
+    listener: (event: EventInit) => void,
+    capture?: boolean
+  ): void;
 
   /** query  */
-  activeElement: () => HTMLElement;
-  getHeight: (node: HTMLElement, client?: HTMLElement) => number;
-  getWidth: (node: HTMLElement, client?: HTMLElement) => number;
-  getOffset: (node: HTMLElement) => object;
-  getOffsetParent: (node: HTMLElement) => object;
-  getPosition: (node: HTMLElement, offsetParent?: object) => object;
-  getWindow: (node: HTMLElement) => string;
-  nodeName: (node: HTMLElement) => string;
-  ownerDocument: (node: HTMLElement) => object;
-  ownerWindow: (node: HTMLElement) => object;
-  contains: (context: HTMLElement, node: HTMLElement) => boolean;
-  scrollLeft: (node: HTMLElement, val?: number) => number | void;
-  scrollTop: (node: HTMLElement, val?: number) => number | void;
+  activeElement(): HTMLElement;
+  getHeight(node: DOMElement, client?: DOMElement): number;
+  getWidth(node: DOMElement, client?: DOMElement): number;
+  getOffset(node: DOMElement): Offset | DOMRect;
+  getOffsetParent(node: HTMLElement | Document): HTMLElement;
+  getPosition(node: HTMLElement, offsetParent?: HTMLElement): Offset;
+  getWindow(node: HTMLElement): Window;
+  nodeName(node: HTMLElement): string;
+  ownerDocument(node: HTMLElement): Document;
+  ownerWindow(node: HTMLElement): Window;
+  contains(context: HTMLElement, node: HTMLElement): boolean;
+  scrollLeft(node: DOMElement): number;
+  scrollLef(node: DOMElement, val: number): void;
+  scrollTop(node: DOMElement): number;
+  scrollTop(node: DOMElement, val: number): void;
 }
 
 declare const DOMHelper: DOMHelperAPI;
