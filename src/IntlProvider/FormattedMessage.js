dc347a29a45884fc0a65f11e3e82d56dded85c7d
simonguo <simonguo.2009@gmail.com>
2019-06-13 09:51:56 +0800

    Support `componentClass` on FormattedMessage

--- src/IntlProvider/FormattedMessage.js
+++ src/IntlProvider/FormattedMessage.js
@@ -4,19 +4,23 @@ import * as React from 'react';
 import IntlContext from './IntlContext';
 
 type Props = {
-  id: string
+  id: string,
+  componentClass?: React.ElementType
 };
 
-const FormattedMessage = ({ id }: Props) => {
+const FormattedMessage = ({ id, componentClass }: Props) => {
+  const Component = componentClass || 'span';
   return (
-    <IntlContext.Consumer>
-      {context => {
-        if (context && typeof context[id] !== 'undefined') {
-          return context[id];
-        }
-        return id;
-      }}
-    </IntlContext.Consumer>
+    <Component>
+      <IntlContext.Consumer>
+        {context => {
+          if (context && typeof context[id] !== 'undefined') {
+            return context[id];
+          }
+          return id;
+        }}
+      </IntlContext.Consumer>
+    </Component>
   );
 };
 
