7ac480e80ff847de8571e02fd1c9705be56af3ef
Godfery <hiyangguo@qq.com>
2019-08-05 16:56:48 +0800

    Dark theme for <Tag> <Tagpicker> <TimeLine> <Toggle> <TreePicker>

---
+++ src/TagPicker/styles/themes/dark.ts
@@ -0,0 +1,6 @@
+import '../../../styles/themes/dark/core.less';
+import '../../../Picker/styles/themes/dark';
+import '../../../CheckPicker/styles/themes/dark';
+import '../../../Tag/styles/themes/dark';
+import '../../../InputPicker/styles/themes/dark';
+import './dark.less';
