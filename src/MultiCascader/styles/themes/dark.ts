8568ffab329e58b8e4ce3dd80a13f25d60b4061a
Godfery <hiyangguo@qq.com>
2019-08-05 13:55:25 +0800

    Dark theme for <Multicascader>

---
+++ src/MultiCascader/styles/themes/dark.ts
@@ -0,0 +1,6 @@
+import '../../../styles/core.less';
+import '../../../Picker/styles/themes/dark';
+import '../../../CheckPicker/styles/themes/dark';
+import '../../../Cascader/styles/themes/dark';
+import '../../../Checkbox/styles/themes/dark';
+import './dark.less';
