d2c2232d47c2bb2c94c18108769d93c68149b070
Godfery <hiyangguo@qq.com>
2019-08-19 15:55:43 +0800

    Fixed some modular issue.

--- src/DateRangePicker/styles/themes/dark.ts
+++ src/DateRangePicker/styles/themes/dark.ts
@@ -1,3 +1,5 @@
 import '../../../styles/themes/dark/core.less';
+import '../../../Picker/styles/themes/dark';
+import '../../../Calendar/styles/themes/dark';
 import '../../../DatePicker/styles/themes/dark';
 import './default.less';
