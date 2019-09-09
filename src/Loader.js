c302f5cdc640ee24eca30458222bf7d6d61e562b
Rafał Pysz <kuchasz1@o2.pl>
2019-09-09 04:16:56 +0200

    Fixed alignment of Loader within its container (#599)
    * removed unnecessary inline styles that brakes center on IE, Edge and Chrome
    
    * removed unused loader reference
    
    * removed unused loader reference pt2

--- src/Loader.js
+++ src/Loader.js
@@ -2,11 +2,9 @@
 
 import * as React from 'react';
 import classNames from 'classnames';
-import { getWidth, addStyle } from 'dom-lib';
 import compose from 'recompose/compose';
 
 import { withStyleProps, defaultProps, prefix } from './utils';
-import { isIE11, isEdge } from './utils/BrowserDetection';
 
 type Props = {
   className?: string,
@@ -22,24 +20,6 @@ type Props = {
 class Loader extends React.Component<Props> {
   static defaultProps = {
     speed: 'normal'
-  };
-
-  componentDidMount() {
-    const { center, backdrop } = this.props;
-
-    if (center || backdrop) {
-      const width = getWidth(this.loader);
-      addStyle(this.loader, {
-        display: isIE11() || isEdge() ? 'block' : 'table',
-        width: `${width}px`
-      });
-    }
-  }
-
-  loader = null;
-
-  loaderRef = ref => {
-    this.loader = ref;
   };
 
   addPrefix(name: string) {
@@ -72,7 +52,7 @@ class Loader extends React.Component<Props> {
     return (
       <div {...props} className={classes}>
         {backdrop && <div className={addPrefix('backdrop')} />}
-        <div className={classPrefix} ref={this.loaderRef}>
+        <div className={classPrefix}>
           <span className={addPrefix('spin')} />
           {hasContent && <span className={addPrefix('content')}>{content}</span>}
         </div>
