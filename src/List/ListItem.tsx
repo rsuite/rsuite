5041964e41c02f39b0bb75d9f3a6591891765634
漆工 <simonguo.2009@gmail.com>
2019-09-19 10:45:56 +0800

    Fix assigned a value but never used (#615)
    * Fix assigned a value but never used
    
    * format code

--- src/List/ListItem.tsx
+++ src/List/ListItem.tsx
@@ -64,16 +64,7 @@ class ListItem extends React.Component<ListItemProps> {
   };
 
   render() {
-    const {
-      className,
-      classPrefix,
-      bordered,
-      disabled,
-      children,
-      size,
-      manager,
-      ...rest
-    } = this.props;
+    const { className, classPrefix, bordered, disabled, children, size, ...rest } = this.props;
     const addPrefix = prefix(classPrefix);
     const unhandled = getUnhandledProps(ListItem, rest);
     const classes = classNames(classPrefix, className, addPrefix(size), {
