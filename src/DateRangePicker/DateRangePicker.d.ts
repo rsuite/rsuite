fa061d6d1b4713adbf8a7099cea73a4f8e058384
漆工 <simonguo.2009@gmail.com>
2019-09-19 10:42:56 +0800

    Support `defaultCalendarValue` on `<DateRangePicker>` (#610)

--- src/DateRangePicker/DateRangePicker.d.ts
+++ src/DateRangePicker/DateRangePicker.d.ts
@@ -45,6 +45,9 @@ export interface DateRangePickerProps extends PickerBaseProps, FormControlBasePr
   /** Whether to show week numbers */
   showWeekNumbers?: boolean;
 
+  /** Set default date for calendar */
+  defaultCalendarValue: ValueType;
+
   /** Disabled date */
   disabledDate?: (
     date: Date,
