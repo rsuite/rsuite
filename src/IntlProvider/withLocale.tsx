4be683d875bf684a2427115b9e011b98fccddea5
simonguo <simonguo.2009@gmail.com>
2019-10-28 12:00:01 +0800

    Added default value for rtl

--- src/IntlProvider/withLocale.tsx
+++ src/IntlProvider/withLocale.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import _ from 'lodash';
 import { setDisplayName, wrapDisplayName } from 'recompose';
-import enGB from './locales/en_GB';
+import defaultLocale from './locales/default';
 import IntlContext from './IntlContext';
 
 const mergeObject = (list: any[]) =>
@@ -16,9 +16,18 @@ function withLocale<T>(combineKeys: string[] = []) {
     const WithLocale = React.forwardRef((props: T, ref) => {
       return (
         <IntlContext.Consumer>
-          {messages => {
-            let locale = mergeObject(combineKeys.map(key => _.get(messages || enGB, `${key}`)));
-            locale.rtl = messages.rtl;
+          {value => {
+            const locale = mergeObject(
+              combineKeys.map(key => _.get(value || defaultLocale, `${key}`))
+            );
+            if (typeof value.rtl !== undefined) {
+              locale.rtl = value.rtl;
+            } else if (
+              typeof window !== 'undefined' &&
+              (document.body.getAttribute('dir') || document.dir) === 'rtl'
+            ) {
+              locale.rtl = true;
+            }
 
             return factory({
               ref,
