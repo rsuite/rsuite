a0dfb5bdaaf84767e653f08aa5b266ad7b5b348a
Godfery <hiyangguo@qq.com>
2019-11-04 18:44:34 +0800

    Added styles tests for Carousel

---
+++ src/Carousel/test/CarouselStylesSpec.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Carousel from '../index';
+import { createTestContainer, getDOMNode, getStyle, toRGB, itChrome } from '@test/testUtils';
+
+import '../styles/index';
+
+describe('Carousel styles', () => {
+  it('Should render correct style ', () => {
+    const instanceRef = React.createRef();
+    ReactDOM.render(<Carousel ref={instanceRef} />, createTestContainer());
+    const dom = getDOMNode(instanceRef.current);
+    assert.equal(getStyle(dom, 'backgroundColor'), toRGB('#8e8e93'), 'Carousel background-color');
+  });
+});
