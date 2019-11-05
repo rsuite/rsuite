c564105c1b60fd62c7d0ec85489aa7a1039fe4ba
simonguo <simonguo.2009@gmail.com>
2019-11-05 15:28:01 +0800

    Added ts type definition for Context

--- src/CheckboxGroup/CheckboxGroup.d.ts
+++ src/CheckboxGroup/CheckboxGroup.d.ts
@@ -3,6 +3,14 @@ import * as React from 'react';
 import { StandardProps, FormControlBaseProps } from '../@types/common';
 import { CheckboxProps } from '../Checkbox/Checkbox.d';
 
+export interface CheckboxContextProps {
+  inline?: boolean;
+  name?: string;
+  value?: any[];
+  controlled?: boolean;
+  onChange?: (value: any, checked: boolean, event: React.SyntheticEvent<HTMLInputElement>) => void;
+}
+
 export interface CheckboxGroupProps<V = any>
   extends StandardProps,
     FormControlBaseProps<CheckboxProps<V>['value'][]> {
@@ -18,4 +26,5 @@ export interface CheckboxGroupProps<V = any>
 
 declare const CheckboxGroup: React.ComponentType<CheckboxGroupProps>;
 
+export const CheckboxContext: React.Context<CheckboxContextProps>;
 export default CheckboxGroup;
