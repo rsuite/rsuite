cb40bf0ff5e46b222182c927c876e81dd8178998
Godfery <hiyangguo@qq.com>
2019-12-19 15:44:28 +0800

    Added component prefix to Popover arrow.

--- src/Popover/test/PopoverStylesSpec.js
+++ src/Popover/test/PopoverStylesSpec.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Popover from '../index';
-import {
-  createTestContainer,
-  getDOMNode,
-  getStyle,
-  toRGB,
-  getDefaultPalette
-} from '@test/testUtils';
+import { createTestContainer, getDOMNode, getStyle, toRGB } from '@test/testUtils';
 
 import '../styles/index';
+import Whisper from '../../Whisper/index';
+import Button from '../../Button/index';
 
 describe('Popover styles', () => {
   it('Should render the correct styles', () => {
@@ -23,5 +19,28 @@ describe('Popover styles', () => {
     const dom = getDOMNode(instanceRef.current);
 
     assert.equal(getStyle(dom, 'backgroundColor'), toRGB('#fff'), 'Popover background-color');
+  });
+
+  it('Should render top start', () => {
+    ReactDOM.render(
+      <Whisper
+        trigger="click"
+        open
+        placement="topStart"
+        speaker={
+          <Popover className="popover-top-start">
+            This is a ToolTip for simple text hints. It can replace the title property
+          </Popover>
+        }
+      >
+        <Button appearance="subtle">Test</Button>
+      </Whisper>,
+      createTestContainer()
+    );
+    const dom = document.querySelector('.popover-top-start');
+    assert.equal(getStyle(dom, 'marginTop'), '-6px', 'Popover margin value');
+    const arrowDom = dom.querySelector('.rs-popover-arrow');
+    assert.equal(getStyle(arrowDom, 'bottom'), '-6px', 'Popover arrow bottom value');
+    assert.equal(getStyle(arrowDom, 'marginLeft'), '-6px', 'Popover arrow bottom value');
   });
 });
