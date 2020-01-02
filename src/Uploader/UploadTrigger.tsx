339ce801ac88f8af976dbda8fb3734f129723498
Godfery <hiyangguo@qq.com>
2020-01-02 09:58:14 +0800

    Supported drag and drop for <Uploader> (#752)

--- src/Uploader/UploadTrigger.tsx
+++ src/Uploader/UploadTrigger.tsx
@@ -9,17 +9,26 @@ export interface UploadTriggerProps {
   name?: string;
   multiple?: boolean;
   disabled?: boolean;
+  dragable?: boolean;
   accept?: string;
   classPrefix?: string;
   className?: string;
   children?: React.FunctionComponentElement<any>;
   componentClass: React.ElementType;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onDragEnter?: (event: React.DragEvent<HTMLInputElement>) => void;
+  onDragLeave?: (event: React.DragEvent<HTMLInputElement>) => void;
+  onDragOver?: (event: React.DragEvent<HTMLInputElement>) => void;
+  onDrop?: (event: React.DragEvent<HTMLInputElement>) => void;
+}
+
+interface UploaderTriggerState {
+  dragOver: boolean;
 }
 
 const Button = props => <button {...props} type="button" />;
 
-class UploadTrigger extends React.Component<UploadTriggerProps> {
+class UploadTrigger extends React.Component<UploadTriggerProps, UploaderTriggerState> {
   static propTypes = {
     name: PropTypes.string,
     multiple: PropTypes.bool,
@@ -29,12 +38,21 @@ class UploadTrigger extends React.Component<UploadTriggerProps> {
     classPrefix: PropTypes.string,
     className: PropTypes.string,
     children: PropTypes.node,
-    componentClass: PropTypes.elementType
+    componentClass: PropTypes.elementType,
+    dragable: PropTypes.bool,
+    onDragEnter: PropTypes.func,
+    onDragLeave: PropTypes.func,
+    onDragOver: PropTypes.func,
+    onDrop: PropTypes.func
   };
   inputRef: React.RefObject<HTMLInputElement>;
+
   constructor(props) {
     super(props);
     this.inputRef = React.createRef();
+    this.state = {
+      dragOver: false
+    };
   }
 
   getInputInstance() {
@@ -43,6 +61,42 @@ class UploadTrigger extends React.Component<UploadTriggerProps> {
 
   handleClick = () => {
     !this.props.disabled && this.inputRef.current.click();
+  };
+
+  handleDragEnter = event => {
+    if (this.props.dragable) {
+      event.preventDefault();
+      this.setState({
+        dragOver: true
+      });
+    }
+    this.props.onDragEnter?.(event);
+  };
+
+  handleDragLeave = event => {
+    if (this.props.dragable) {
+      event.preventDefault();
+      this.setState({
+        dragOver: false
+      });
+    }
+    this.props.onDragLeave?.(event);
+  };
+
+  handleDragOver = event => {
+    this.props.dragable && event.preventDefault();
+    this.props.onDragOver?.(event);
+  };
+
+  handleDrop = event => {
+    if (this.props.dragable) {
+      event.preventDefault();
+      this.setState({
+        dragOver: false
+      });
+      this.props.onChange?.(event);
+    }
+    this.props.onDrop?.(event);
   };
 
   render() {
@@ -56,19 +110,30 @@ class UploadTrigger extends React.Component<UploadTriggerProps> {
       classPrefix,
       className,
       componentClass: Component,
+      dragable,
+      onDragEnter,
+      onDragLeave,
+      onDragOver,
+      onDrop,
       ...rest
     } = this.props;
 
     const unhandled = getUnhandledProps(UploadTrigger, rest);
     const addPrefix = prefix(classPrefix);
     const classes = classNames(classPrefix, className, {
-      [addPrefix('disabled')]: disabled
+      [addPrefix('disabled')]: disabled,
+      [addPrefix('customize')]: children,
+      [addPrefix('drag-over')]: this.state.dragOver
     });
 
     const buttonProps = {
       ...unhandled,
       className: addPrefix('btn'),
-      onClick: this.handleClick
+      onClick: this.handleClick,
+      onDragEnter: this.handleDragEnter,
+      onDragLeave: this.handleDragLeave,
+      onDragOver: this.handleDragOver,
+      onDrop: this.handleDrop
     };
 
     const trigger = children ? (
