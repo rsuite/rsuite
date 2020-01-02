ab2be46929af75ca076b1159142c5eec8036090a
Simon Guo <simonguo.2009@gmail.com>
2020-01-02 10:52:00 +0800

    Enhance Tooltip and Popover to support HTML attributes (#806)

--- src/Whisper/Whisper.tsx
+++ src/Whisper/Whisper.tsx
@@ -5,6 +5,15 @@ import { createChainedFunction, placementPolyfill } from '../utils';
 import IntlContext from '../IntlProvider/IntlContext';
 import { WhisperProps } from './Whisper.d';
 
+export const overlayProps = [
+  'placement',
+  'shouldUpdatePosition',
+  'arrowOffsetLeft',
+  'arrowOffsetTop',
+  'positionLeft',
+  'positionTop'
+];
+
 class Whisper extends React.Component<WhisperProps> {
   static propTypes = {
     triggerRef: PropTypes.func,
