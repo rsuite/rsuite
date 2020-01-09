85e7c4457be1a2cf7a59823d2e0f7fa52469ca36
Godfery <hiyangguo@qq.com>
2020-01-09 12:38:31 +0800

    Added change theme feature

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,32 +1,25 @@
-const withLess = require('@zeit/next-less');
 const withImages = require('next-images');
 const markdown = require('react-markdown-reader');
 
 const languages = ['javascript', 'bash', 'xml', 'css', 'less', 'json', 'diff', 'typescript'];
 
-module.exports = withImages(
-  withLess({
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.md$/,
-        use: [
-          {
-            loader: 'html-loader'
-          },
-          {
-            loader: 'markdown-loader',
-            options: {
-              pedantic: true,
-              renderer: markdown.renderer(languages)
-            }
+module.exports = withImages({
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.md$/,
+      use: [
+        {
+          loader: 'html-loader'
+        },
+        {
+          loader: 'markdown-loader',
+          options: {
+            pedantic: true,
+            renderer: markdown.renderer(languages)
           }
-        ]
-      });
-
-      return config;
-    },
-    lessLoaderOptions: {
-      javascriptEnabled: true
-    }
-  })
-);
+        }
+      ]
+    });
+    return config;
+  }
+});
--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useMemo, useState, useCallback } from 'react';
 import { Grid } from 'rsuite';
 import { ThemeContext } from '../components/Context';
 import { IntlProvider as RSIntlProvider } from 'rsuite';
@@ -6,35 +7,64 @@ import zhCN from 'rsuite/lib/IntlProvider/locales/zh_CN';
 //import enUS from 'rsuite/lib/IntlProvider/locales/en_US';
 
 import { getMessages } from '../locales';
-import { readTheme } from '../utils/themeHelpers';
-
-import '../less/index.less';
+import {
+  DirectionType,
+  getThemeCssPath,
+  getThemeId,
+  readTheme,
+  ThemeType,
+  writeTheme
+} from '../utils/themeHelpers';
+import loadCssFile from '../utils/loadCssFile';
+import StyleHead from '../components/StyleHead';
 
 interface AppProps {
   Component: React.ElementType;
   pageProps: any;
 }
 
-function handleToggleDirection() {
-  console.log('handleToggleDirection');
-}
-
-function handleToggleTheme() {
-  console.log('handleToggleTheme');
-}
-
 function App({ Component, pageProps }: AppProps) {
+  const [defaultThemeName, defaultDirection] = useMemo<[ThemeType, DirectionType]>(readTheme, []);
+  const [themeName, setThemeName] = useState(defaultThemeName);
+  const [direction, setDirection] = useState(defaultDirection);
+
+  const loadTheme = useCallback((themeName: ThemeType, direction: DirectionType) => {
+    const themeId = getThemeId(themeName, direction);
+    loadCssFile(getThemeCssPath(themeName, direction), themeId).then(() => {
+      const html = document.querySelector('html');
+      html.dir = direction;
+      writeTheme(themeName, direction);
+      Array.from(document.querySelectorAll('[id^=theme]')).forEach(css => {
+        if (css.id !== themeId) {
+          css.remove();
+        }
+      });
+    });
+  }, []);
+
+  const handleToggleTheme = useCallback(() => {
+    const newThemeName = themeName === 'default' ? 'dark' : 'default';
+    setThemeName(newThemeName);
+    loadTheme(newThemeName, direction);
+  }, [themeName, direction]);
+
+  const handleToggleDirection = useCallback(() => {
+    const newDirection = direction === 'ltr' ? 'rtl' : 'ltr';
+    setDirection(newDirection);
+    loadTheme(themeName, newDirection);
+  }, [themeName, direction]);
   return (
     <Grid fluid className="app-container">
       <RSIntlProvider locale={zhCN} rtl={false}>
         <ThemeContext.Provider
           value={{
-            theme: readTheme(),
+            theme: [themeName, direction],
             messages: getMessages(),
             handleToggleDirection,
             handleToggleTheme
           }}
         >
+          <StyleHead />
           <Component {...pageProps} />
         </ThemeContext.Provider>
       </RSIntlProvider>
