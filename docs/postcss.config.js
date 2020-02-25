2dbc9b038278c63eef95e2a8a626e25381f8be00
Godfery <hiyangguo@qq.com>
2020-02-25 19:05:38 +0800

    Fix build errors

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,25 +1,4 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { NODE_ENV, STYLE_DEBUG } = process.env;
-const __PRO__ = NODE_ENV === 'production';
-
-let plugins = [];
-
-if (STYLE_DEBUG === 'STYLE' || __PRO__) {
-  plugins = [
-    require('autoprefixer'),
-    require('cssnano')({
-      preset: [
-        'default',
-        {
-          discardComnments: {
-            removeAll: false
-          }
-        }
-      ]
-    })
-  ];
-}
-
+// This config provide to nextjs ,and useful config write in RTLCSSPlugin.js
 module.exports = {
-  plugins
+  plugins: []
 };
