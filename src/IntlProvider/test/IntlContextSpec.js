1d4fac12010388c61efdc3490222ec46386a5be5
simonguo <simonguo.2009@gmail.com>
2020-03-13 13:17:39 +0800

    Fix rendering error in local language

---
+++ src/IntlProvider/test/IntlContextSpec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import IntlProvider from '../IntlProvider';
+import Pagination from '../../Pagination';
+import { getDOMNode } from '@test/testUtils';
+import zhCN from '../locales/zh_CN';
+
+describe('IntlProvider', () => {
+  it('Should render the correct local language', () => {
+    const domNode = getDOMNode(
+      <div>
+        <IntlProvider locale={zhCN}>
+          <Pagination pages={2} activePage={1} first last next prev />
+        </IntlProvider>
+      </div>
+    );
+    console.log(domNode.querySelectorAll('li span')[0].title);
+    assert.equal(domNode.querySelectorAll('li span')[0].title, '第一页');
+  });
+});
