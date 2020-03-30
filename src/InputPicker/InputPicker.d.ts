b50f7be3ad1b0fd5c9bc1976eb1d0e6255ca3b06
simonguo <simonguo.2009@gmail.com>
2020-03-30 11:14:13 +0800

    feat(TagPicker): Support `tagProps` on TagPicker

--- src/InputPicker/InputPicker.d.ts
+++ src/InputPicker/InputPicker.d.ts
@@ -2,10 +2,17 @@ import * as React from 'react';
 
 import { FormControlPickerProps } from '../@types/common';
 import { SelectProps } from '../SelectPicker/SelectPicker.d';
+import { TagProps } from '../Tag/Tag.d';
 
 export interface InputPickerProps extends FormControlPickerProps<any>, SelectProps<any> {
   /** Settings can create new options */
   creatable?: boolean;
+
+  /**
+   * Tag related props.
+   * https://github.com/rsuite/rsuite/blob/master/src/Tag/Tag.d.ts
+   */
+  tagProps?: TagProps;
 }
 
 declare const InputPicker: React.ComponentType<InputPickerProps>;
