ac656f2fe1eb558fe68c8c40fc7dc15b1f598fe9
simonguo <simonguo.2009@gmail.com>
2020-05-05 10:46:56 +0800

    Fixed Carousel unable to render invalid children

--- src/Carousel/test/CarouselSpec.js
+++ src/Carousel/test/CarouselSpec.js
@@ -8,6 +8,18 @@ describe('Carousel', () => {
     assert.ok(instance.className.match(/\bcarousel-placement-right\b/));
   });
 
+  it('Should render 2 subitems', () => {
+    const instance = getDOMNode(
+      <Carousel>
+        <img />
+        {undefined}
+        <img />
+      </Carousel>
+    );
+    assert.equal(instance.querySelector('.rs-carousel-slider').childNodes.length, 2);
+    assert.equal(instance.querySelectorAll('.rs-carousel-label-wrapper').length, 2);
+  });
+
   it('Should button be displayed as a bar', () => {
     const instance = getDOMNode(<Carousel shape="bar" />);
     assert.ok(instance.className.match(/\bcarousel-shape-bar\b/));
--- src/Carousel/test/CarouselStylesSpec.js
+++ src/Carousel/test/CarouselStylesSpec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Carousel from '../index';
-import { createTestContainer, getDOMNode, getStyle, toRGB, itChrome } from '@test/testUtils';
+import { createTestContainer, getDOMNode, getStyle, toRGB } from '@test/testUtils';
 
 import '../styles/index';
 
