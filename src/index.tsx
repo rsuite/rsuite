f5934d5ef927bd6dca4bbb1c708d3fe3d7507f23
Doma <leishenghao@126.com>
2020-05-20 15:25:07 +0800

    style(dts): Re-arrange index.ts according to documentation order

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,30 @@
-// Base
+// General
+export { default as SafeAnchor } from './SafeAnchor';
 export { default as Button, ButtonProps } from './Button';
 export { default as IconButton, IconButtonProps } from './IconButton';
 export { default as ButtonToolbar, ButtonToolbarProps } from './ButtonToolbar';
 export { default as ButtonGroup, ButtonGroupProps } from './ButtonGroup';
-export { default as Whisper, WhisperProps } from './Whisper';
+export { default as Icon, IconProps } from './Icon';
+export { default as IconStack, IconStackProps } from './IconStack';
 export { default as Tooltip, TooltipProps } from './Tooltip';
+export { default as Whisper, WhisperProps } from './Whisper';
 export { default as Popover, PopoverProps } from './Popover';
-export { default as Modal, ModalProps } from './Modal';
 export { default as Alert } from './Alert';
 export { default as Notification } from './Notification';
 export { default as Message, MessageProps } from './Message';
-export { default as Drawer, DrawerProps } from './Drawer';
 export { default as Loader, LoaderProps } from './Loader';
-export { default as Icon, IconProps } from './Icon';
-export { default as IconStack, IconStackProps } from './IconStack';
-export { default as SafeAnchor } from './SafeAnchor';
+export { default as Modal, ModalProps } from './Modal';
+export { default as Drawer, DrawerProps } from './Drawer';
 export { default as Divider, DividerProps } from './Divider';
+export { default as Progress, ProgressCircleProps, ProgressLineProps } from './Progress';
 export {
   default as Placeholder,
   PlaceholderGraphProps,
   PlaceholderGridProps,
   PlaceholderParagraphProps
 } from './Placeholder';
-export { default as Badge, BadgeProps } from './Badge';
-export { default as Avatar, AvatarProps } from './Avatar';
 
-// Nav
+// Navigation
 export {
   default as Dropdown,
   DropdownProps,
@@ -35,47 +34,44 @@ export {
 export { default as Nav, NavProps, NavItemProps } from './Nav';
 export { default as Navbar, NavbarProps } from './Navbar';
 export { default as Sidenav, SidenavProps } from './Sidenav';
-export { default as Breadcrumb, BreadcrumbProps, BreadcrumbItemProps } from './Breadcrumb';
-export { default as Pagination, PaginationProps } from './Pagination';
 export { default as Steps, StepsProps } from './Steps';
+export { default as Pagination, PaginationProps } from './Pagination';
+export { default as Breadcrumb, BreadcrumbProps, BreadcrumbItemProps } from './Breadcrumb';
 
-// Data enter
-export { default as Toggle, ToggleProps } from './Toggle';
+// Data entry
 export { default as Form, FormProps } from './Form';
+export { default as FormControl, FormControlProps } from './FormControl';
 export { default as FormGroup, FormGroupProps } from './FormGroup';
-export { default as ErrorMessage } from './ErrorMessage';
 export { default as ControlLabel, ControlLabelProps } from './ControlLabel';
 export { default as HelpBlock, HelpBlockProps } from './HelpBlock';
-export { default as FormControl, FormControlProps } from './FormControl';
-export { default as Input, InputProps } from './Input';
-export { default as InputNumber, InputNumberProps } from './InputNumber';
-export { default as InputGroup, InputGroupProps } from './InputGroup';
+export { default as ErrorMessage } from './ErrorMessage';
 export { default as Checkbox, CheckboxProps } from './Checkbox';
 export { default as CheckboxGroup, CheckboxGroupProps } from './CheckboxGroup';
 export { default as Radio, RadioProps } from './Radio';
 export { default as RadioGroup, RadioGroupProps } from './RadioGroup';
-export { default as SelectPicker, SelectPickerProps } from './SelectPicker';
-export { default as CheckPicker, CheckPickerProps } from './CheckPicker';
+export { default as Input, InputProps } from './Input';
+export { default as InputGroup, InputGroupProps } from './InputGroup';
+export { default as InputNumber, InputNumberProps } from './InputNumber';
+export { default as AutoComplete, AutoCompleteProps } from './AutoComplete';
+export { default as Toggle, ToggleProps } from './Toggle';
 export { default as InputPicker, InputPickerProps } from './InputPicker';
 export { default as TagPicker, TagPickerProps } from './TagPicker';
+export { default as SelectPicker, SelectPickerProps } from './SelectPicker';
+export { default as CheckPicker, CheckPickerProps } from './CheckPicker';
 export { default as TreePicker, TreePickerProps } from './TreePicker';
 export { default as CheckTreePicker, CheckTreePickerProps } from './CheckTreePicker';
 export { default as Cascader, CascaderProps } from './Cascader';
 export { default as MultiCascader, MultiCascaderProps } from './MultiCascader';
 export { default as DatePicker, DatePickerProps } from './DatePicker';
 export { default as DateRangePicker, DateRangePickerProps } from './DateRangePicker';
-export { default as AutoComplete, AutoCompleteProps } from './AutoComplete';
-export { default as Uploader, UploaderProps } from './Uploader';
 export { default as Slider, SliderProps } from './Slider';
 export { default as RangeSlider, RangeSliderProps } from './RangeSlider';
+export { default as Uploader, UploaderProps } from './Uploader';
 export { default as Rate, RateProps } from './Rate';
 
 // Data display
-
-export { default as Tree, TreeProps } from './Tree';
-export { default as CheckTree, CheckTreeProps } from './CheckTree';
-export { default as Panel, PanelProps } from './Panel';
-export { default as PanelGroup, PanelGroupProps } from './PanelGroup';
+export { default as Avatar, AvatarProps } from './Avatar';
+export { default as Badge, BadgeProps } from './Badge';
 export {
   default as Table,
   TableProps,
@@ -83,7 +79,10 @@ export {
   TableCellProps,
   TablePaginationProps
 } from './Table';
-export { default as Progress, ProgressCircleProps, ProgressLineProps } from './Progress';
+export { default as Tree, TreeProps } from './Tree';
+export { default as CheckTree, CheckTreeProps } from './CheckTree';
+export { default as Panel, PanelProps } from './Panel';
+export { default as PanelGroup, PanelGroupProps } from './PanelGroup';
 export { default as Timeline, TimelineProps, TimelineItemProps } from './Timeline';
 export { default as Tag, TagProps } from './Tag';
 export { default as TagGroup } from './TagGroup';
@@ -106,8 +105,8 @@ export { default as Footer } from './Footer';
 // Utils
 
 export { default as Schema } from './Schema';
+export { default as Affix, AffixProps } from './Affix';
 export { default as Animation, TransitionProps, CollapseProps, SlideProps } from './Animation';
-export { default as IntlProvider } from './IntlProvider';
 export { default as Portal, PortalProps } from './Portal';
 export { default as DOMHelper } from './DOMHelper';
-export { default as Affix, AffixProps } from './Affix';
+export { default as IntlProvider } from './IntlProvider';
