0888be48c5b283046c711e06d66fbe887a5536e2
Sleaf <sleaf1996@qq.com>
2020-06-11 16:22:41 +0800

    fix: autoplay can now be interrupted in Carousel (#1120)
    * fix: autoplay can now be interrupted in Carousel
    
    * fix: better support for `rtl`
    
    * fix: remove unused import
    
    Co-authored-by: Sleaf <shu.huang@mail.hypers.com>

--- .babelrc.js
+++ .babelrc.js
@@ -12,6 +12,7 @@ module.exports = (api, options) => {
     'date-fns',
     ['@babel/plugin-proposal-class-properties', { loose: true }],
     '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
     '@babel/plugin-proposal-export-namespace-from',
     '@babel/plugin-proposal-export-default-from',
     ['@babel/plugin-transform-runtime', { useESModules: !modules }]
