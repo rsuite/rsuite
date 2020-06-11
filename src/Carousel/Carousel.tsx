245e48ba934a076d6677c79c06bf563f7190b7af
Sleaf <shu.huang@mail.hypers.com>
2020-06-11 11:12:59 +0800

    fix: remove unused import

--- src/Carousel/Carousel.tsx
+++ src/Carousel/Carousel.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { prefix, defaultProps, getUnhandledProps } from '../utils';
-import IntlContext from '../IntlProvider/IntlContext';
 import ReactChildren from '../utils/ReactChildren';
 import { CarouselProps } from './Carousel.d';
 
