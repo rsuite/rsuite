9c3f76e85a550437347849cbd282f16e6b7b3f5e
simonguo <simonguo.2009@gmail.com>
2020-08-14 18:58:17 +0800

    perf: make `as` compatible with html attributes

--- src/@types/common.ts
+++ src/@types/common.ts
@@ -1,11 +1,9 @@
 import React from 'react';
+import { ReplaceProps } from './utils';
 
 export interface StandardProps {
   /** The prefix of the component CSS class */
   classPrefix?: string;
-
-  /** You can use a custom element for this component */
-  as?: React.ElementType | string;
 
   /** Additional classes */
   className?: string;
@@ -15,6 +13,23 @@ export interface StandardProps {
 
   /** Additional style */
   style?: React.CSSProperties;
+}
+
+export interface WithAsProps<As extends React.ElementType | string = React.ElementType>
+  extends StandardProps {
+  /** You can use a custom element for this component */
+  as?: As;
+}
+
+export interface RsRefForwardingComponent<T extends React.ElementType, P = unknown> {
+  <As extends React.ElementType = T>(
+    props: React.PropsWithChildren<ReplaceProps<As, WithAsProps<As> & P>>,
+    context?: any
+  ): React.ReactElement | null;
+  propTypes?: any;
+  contextTypes?: any;
+  defaultProps?: Partial<P>;
+  displayName?: string;
 }
 
 export interface AnimationEventProps {
