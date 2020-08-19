19a5cb3462bc620c7754e7c6b5f42e8e5311f3c8
simonguo <simonguo.2009@gmail.com>
2020-08-19 13:22:43 +0800

    refactor(divider): refactor divider

--- src/Divider/test/DividerPickerStylesSpec.js
+++ src/Divider/test/DividerPickerStylesSpec.js
@@ -9,9 +9,10 @@ describe('Divider styles', () => {
   it('Should render the correct styles', () => {
     const instanceRef = React.createRef();
     ReactDOM.render(<Divider ref={instanceRef} />, createTestContainer());
-    const dom = getDOMNode(instanceRef.current);
-    assert.equal(getStyle(dom, 'backgroundColor'), toRGB('#e5e5ea'), 'Divider background-color');
-    assert.equal(getStyle(dom, 'height'), '1px', 'Divider height');
-    inChrome && assert.equal(getStyle(dom, 'margin'), '24px 0px', 'Divider margin');
+
+    const element = instanceRef.current;
+    assert.equal(getStyle(element, 'backgroundColor'), toRGB('#e5e5ea'), 'Divider background');
+    assert.equal(getStyle(element, 'height'), '1px', 'Divider height');
+    inChrome && assert.equal(getStyle(element, 'margin'), '24px 0px', 'Divider margin');
   });
 });
