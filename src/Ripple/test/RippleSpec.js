85cc9cc9427c0e30b65910fa81148878f471cb7a
simonguo <simonguo.2009@gmail.com>
2020-10-15 11:55:31 +0800

    test(ripple): update tests

--- src/Ripple/test/RippleSpec.js
+++ src/Ripple/test/RippleSpec.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-dom/test-utils';
 import { getDOMNode } from '@test/testUtils';
 import Ripple from '../Ripple';
+
+let container;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
 
 describe('Ripple', () => {
   it('Should render a Ripple', () => {
@@ -13,15 +26,20 @@ describe('Ripple', () => {
     const doneOp = () => {
       done();
     };
-    const instance = getDOMNode(
-      <div style={{ width: 100, height: 100 }}>
-        <Ripple onMouseDown={doneOp} />
-      </div>
-    );
+    const ref=React.createRef();
+    ReactTestUtils.act(() => {
+      ReactDOM.render(
+        <div ref={ref} style={{ width: 100, height: 100 }}>
+          <Ripple onMouseDown={doneOp} />
+        </div>,
+        container
+      );
+    });
 
-    ReactTestUtils.SimulateNative.mouseDown(instance);
-    const event = new Event('mousedown');
-    instance.dispatchEvent(event);
+    ReactTestUtils.act(() => {
+      const event = new Event('mousedown');
+      ref.current.dispatchEvent(event);
+    });
   });
 
   it('Should have a custom className prefix', () => {
