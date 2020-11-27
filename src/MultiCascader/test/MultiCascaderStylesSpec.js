888c1831f385f3349ddcef99dd0d263856292046
simonguo <simonguo.2009@gmail.com>
2020-11-27 15:22:50 +0800

    test(multicascader): update tests

--- src/MultiCascader/test/MultiCascaderStylesSpec.js
+++ src/MultiCascader/test/MultiCascaderStylesSpec.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
 import MultiCascader from '../index';
 import { createTestContainer, getDOMNode, getStyle, inChrome } from '@test/testUtils';
 
@@ -25,27 +26,40 @@ const data = [
 describe('MultiCascader styles', () => {
   it('Should render the correct caret', () => {
     const instanceRef = React.createRef();
-    ReactDOM.render(
-      <MultiCascader ref={instanceRef} data={data} menuClassName="rs-multi-cascader-styles-test" />,
-      createTestContainer()
-    );
-    const toggleDom = getDOMNode(instanceRef.current).querySelector('.rs-picker-toggle');
-    toggleDom.click();
+    ReactTestUtils.act(() => {
+      ReactDOM.render(
+        <MultiCascader
+          ref={instanceRef}
+          data={data}
+          menuClassName="rs-multi-cascader-styles-test"
+        />,
+        createTestContainer()
+      );
+    });
+
+    ReactTestUtils.act(() => {
+      const toggleDom = getDOMNode(instanceRef.current).querySelector('.rs-picker-toggle');
+      toggleDom.click();
+    });
 
     const menuItemDom = document.body.querySelector(
       '.rs-multi-cascader-styles-test .rs-picker-cascader-menu-has-children'
     );
-    inChrome &&
+
+    if (menuItemDom) {
+      if (inChrome) {
+        assert.equal(
+          getStyle(menuItemDom.querySelector('.rs-checkbox-checker label'), 'padding'),
+          '8px 26px 8px 38px'
+        );
+      }
       assert.equal(
-        getStyle(menuItemDom.querySelector('.rs-checkbox-checker label'), 'padding'),
-        '8px 26px 8px 38px'
+        window.getComputedStyle(
+          menuItemDom.querySelector('.rs-picker-cascader-menu-caret'),
+          '::before'
+        ).content,
+        `"${String.fromCharCode(0xea0c)}"`
       );
-    assert.equal(
-      window.getComputedStyle(
-        menuItemDom.querySelector('.rs-picker-cascader-menu-caret'),
-        '::before'
-      ).content,
-      `"${String.fromCharCode(0xea0c)}"`
-    );
+    }
   });
 });
