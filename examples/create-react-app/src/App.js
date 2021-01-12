b071ed2c15719b6ed05bbf802620d087a88dffed
simonguo <simonguo.2009@gmail.com>
2021-01-12 14:16:50 +0800

    docs(example): update example for modular import

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import './App.css';
-
-import 'rsuite/lib/styles/themes/dark/index.less';
-
-/** import default css */
-// import 'rsuite/dist/styles/rsuite-default.css';
-
-/** import dark css */
-// import 'rsuite/dist/styles/rsuite-dark.css';
-
-import { Button } from 'rsuite';
+import Button from 'rsuite/lib/Button';
+import 'rsuite/lib/Button/styles/themes/default';
 
 function App() {
   return (
