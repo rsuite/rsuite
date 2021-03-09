dab1c7d94050b66c6b2161e87ad0cb526d964716
simonguo <simonguo.2009@gmail.com>
2021-03-09 18:51:00 +0800

    fix(uploader): fixed the status icon not showing during upload

--- docs/pages/components/uploader/index.tsx
+++ docs/pages/components/uploader/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { Uploader, Button, Loader, Message } from 'rsuite';
+import { Uploader, Button, Loader, Message, toaster } from 'rsuite';
 import DefaultPage from '@/components/Page';
 import Avatar from '@rsuite/icons/legacy/Avatar';
 import CameraRetro from '@rsuite/icons/legacy/CameraRetro';
 
 export default function Page() {
-  return <DefaultPage dependencies={{ Uploader, Button, Loader, Message, Avatar, CameraRetro }} />;
+  return (
+    <DefaultPage
+      dependencies={{ Uploader, Button, Loader, Message, Avatar, CameraRetro, toaster }}
+    />
+  );
 }
