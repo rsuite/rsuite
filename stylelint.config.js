4b755834e6e997152340535369e74c58d485fc54
Doma <leishenghao@126.com>
2021-04-21 18:42:23 +0800

    style: upgrade stylelint and resolve violations

--- stylelint.config.js
+++ stylelint.config.js
@@ -18,8 +18,8 @@ module.exports = {
     'at-rule-no-vendor-prefix': true,
     // fix less detached ruleset
     'at-rule-no-unknown': null,
-    // Specify a whitelist of attributes and units allowed in the declaration.
-    'declaration-property-unit-whitelist': {
+    // Specify an allowed-list of attributes and units allowed in the declaration.
+    'declaration-property-unit-allowed-list': {
       'font-size': ['px', 'em', '%'],
       '/^animation/': ['s']
     },
@@ -30,8 +30,11 @@ module.exports = {
     // Don't forbidden low specificity selectors from appearing after high specificity selectors.
     // [Specificity description]{@link https://www.w3.org/TR/css3-selectors/#specificity}
     'no-descending-specificity': null,
-    // Class selector naming rules use snake case. eg: .class-name
-    'selector-class-pattern': '^[a-z0-9]+(-?[a-z0-9]+)*$',
+    // Class selector naming rules use kebab-case. eg: .class-name
+    // FIXME: this regex covers LESS parametric mixin definitions
+    //        which was supposed to be ignored from this rule by stylelint
+    //        @see https://github.com/stylelint/stylelint/issues/5258
+    'selector-class-pattern': '^[a-z0-9]+(-?[a-z0-9]+)*(\\(.*\\))?$',
     // ID selector naming rules use camel case , excepted `icon-`. eg: #loginButton,#icon-login
     'selector-id-pattern': '^([a-z]+([A-Z][a-z]+)?)||(icon-([a-z-]+))$',
     // Forbidden use the browser prefix.
@@ -39,14 +42,15 @@ module.exports = {
     'selector-no-vendor-prefix': true,
     'value-no-vendor-prefix': true,
     // Limit the number of composite selectors in a selector.
+    // TODO consider narrowing this limit
     'selector-max-compound-selectors': 6,
     // Limit the number of ID selectors in a selector.
     'selector-max-id': 1,
     // Limit the number of universal selectors in a selector.
     'selector-max-universal': 1,
-    // Specify a whitelist of allowed pseudo-class selectors.(Only use pseudo-classes supported by IE10+ browsers)
+    // Specify an allowed-list of allowed pseudo-class selectors.(Only use pseudo-classes supported by IE10+ browsers)
     // [CSS selector browser support]{@link https://labs.qianduan.net/css-selector/}
-    'selector-pseudo-class-whitelist': [
+    'selector-pseudo-class-allowed-list': [
       // CSS 1
       'active',
       'link',
@@ -79,6 +83,14 @@ module.exports = {
     // String using single quotes
     'string-quotes': 'single',
     'declaration-colon-newline-after': null,
-    'value-list-comma-newline-after': 'always-multi-line'
+    'value-list-comma-newline-after': 'always-multi-line',
+    // font-family can contain uppercase letters
+    // @see https://github.com/stylelint/stylelint/issues/4622#issuecomment-594641799
+    'value-keyword-case': [
+      'lower',
+      {
+        ignoreKeywords: ['BlinkMacSystemFont']
+      }
+    ]
   }
 };
