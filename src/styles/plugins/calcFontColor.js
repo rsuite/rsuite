3291a37ca9e7181ff0ce80799a9db22f7b3b9c65
Doma <leishenghao@126.com>
2021-05-13 18:04:57 +0800

    refactor(custom-properties): rename rs-highlight colors to rs-primary

--- src/styles/plugins/calcFontColor.js
+++ src/styles/plugins/calcFontColor.js
@@ -2,12 +2,12 @@ const tinycolor = require('tinycolor2');
 
 module.exports = {
   install: function (less, pluginManager, functions) {
-    functions.add('calcFontColor', function (color) {
-      const black = '#575757';
-      const white = '#ffffff';
-      const a = tinycolor.readability(color, black);
-      const b = tinycolor.readability(color, white);
-      return less.color(tinycolor(a > b ? black : white).toHex());
+    functions.add('calcFontColor', function (bg, blackish, whitish) {
+      blackish = blackish || '#575757';
+      whitish = whitish || '#ffffff';
+      const a = tinycolor.readability(bg, blackish);
+      const b = tinycolor.readability(bg, whitish);
+      return less.color(tinycolor(a > b ? blackish : whitish).toHex());
     });
   }
 };
