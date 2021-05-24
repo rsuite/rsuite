f03f93c908147337385deb0b37821a67fd2a5d15
dekuofa1995 <dekuofa1995@gmail.com>
2021-05-24 11:12:27 +0800

    fix: multiCascader data cann't be controlled
    update columns when data prop change

--- src/MultiCascader/utils.ts
+++ src/MultiCascader/utils.ts
@@ -5,6 +5,7 @@ import slice from 'lodash/slice';
 import { MultiCascaderProps, ValueType } from './MultiCascader';
 import { ItemDataType } from '../@types/common';
 import { flattenTree } from '../utils/treeUtils';
+import { useUpdateEffect } from '../utils';
 
 export interface ItemType extends ItemDataType {
   parent?: ItemType;
@@ -195,6 +196,10 @@ export function useColumnData(flattenData: ItemType[]) {
   function addColumn(column: ItemDataType[], index: number) {
     setColumnData([...slice(columnData, 0, index), column]);
   }
+
+  useUpdateEffect(() => {
+    setColumnData([flattenData.filter(item => !item.parent)]);
+  }, [flattenData]);
 
   return {
     columnData,
