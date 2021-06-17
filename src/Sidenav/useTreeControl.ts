254849f93e22baa651c9073ab539527c214f2e22
Doma <leishenghao@126.com>
2021-06-18 00:39:08 +0800

    chore: fix eslint problem

--- src/Sidenav/useTreeControl.ts
+++ src/Sidenav/useTreeControl.ts
@@ -1,10 +1,14 @@
 import { TreeControlContextProps } from './TreeControlContext';
 import { useState } from 'react';
 
+/**
+ * TODO Focus management for ARIA `tree` widget
+ */
 export default function useTreeControl(): TreeControlContextProps {
-  const [activeItemIndex, setActiveItemIndex] = useState<number | null>(null);
+  const [activeItemIndex] = useState<number | null>(null);
 
   return {
-    activeItemIndex
+    activeItemIndex,
+    activeDescendantId: null
   };
 }
