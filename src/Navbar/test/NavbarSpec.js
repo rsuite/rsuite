5bbd3b72b2a039ec73f8eddeacacb7ff264e7ff1
Doma <leishenghao@126.com>
2021-06-30 17:26:38 +0800

    test(navbar): test aria roles for Nav inside Navbar

--- src/Navbar/test/NavbarSpec.js
+++ src/Navbar/test/NavbarSpec.js
@@ -3,6 +3,8 @@ import { getDOMNode } from '@test/testUtils';
 
 import Navbar from '../Navbar';
 import Nav from '../../Nav';
+import Dropdown from '../../Dropdown';
+import { render } from '@testing-library/react';
 
 describe('Navbar', () => {
   it('Should render a navbar', () => {
@@ -33,5 +35,29 @@ describe('Navbar', () => {
   it('Should have a custom className prefix', () => {
     const instance = getDOMNode(<Navbar classPrefix="custom-prefix" />);
     assert.ok(instance.className.match(/\bcustom-prefix\b/));
+  });
+
+  it('Should render an ARIA menubar with menuitems', () => {
+    const { getByRole } = render(
+      <Navbar>
+        <Navbar.Brand href="#">RSUITE</Navbar.Brand>
+        <Nav>
+          <Nav.Item>Home</Nav.Item>
+          <Nav.Item>News</Nav.Item>
+          <Nav.Item>Products</Nav.Item>
+          <Dropdown title="About">
+            <Dropdown.Item>Company</Dropdown.Item>
+            <Dropdown.Item>Team</Dropdown.Item>
+            <Dropdown.Item>Contact</Dropdown.Item>
+          </Dropdown>
+        </Nav>
+      </Navbar>
+    );
+
+    expect(getByRole('menubar'), 'Menubar').not.to.be.null;
+
+    for (const label of ['Home', 'News', 'Products', 'About']) {
+      expect(getByRole('menuitem', { name: label })).not.to.be.null;
+    }
   });
 });
