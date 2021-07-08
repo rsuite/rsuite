d46cfd238389bf131eeeb35f07f80b793e60a283
simonguo <simonguo.2009@gmail.com>
2021-07-08 17:19:16 +0800

    build(deps): bump rsuite-table to 5.0.0-alpha.4

--- src/Table/index.tsx
+++ src/Table/index.tsx
@@ -1,12 +1,6 @@
 import Table from './Table';
-
-export type {
-  TableProps,
-  TableInstance,
-  TableLocale,
-  ColumnGroupProps,
-  CellProps,
-  ColumnProps
-} from './Table';
+import type { TableProps, ColumnProps, ColumnGroupProps, TableLocaleType } from 'rsuite-table';
+export type { TableProps, ColumnGroupProps, ColumnProps, TableLocaleType };
+export type { TableInstance, CellProps } from './Table';
 
 export default Table;
