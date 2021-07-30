a4edeba0161bd9c5ee47b2c3e1dee8055b0111d9
Simon Guo <simonguo.2009@gmail.com>
2021-07-30 19:10:20 +0800

    fix(typescript): export props of all components (#1803)

--- src/CustomProvider/index.tsx
+++ src/CustomProvider/index.tsx
@@ -1,4 +1,6 @@
 import CustomProvider, { CustomContext, CustomConsumer } from './CustomProvider';
 import FormattedDate from './FormattedDate';
 export { CustomContext, CustomConsumer, FormattedDate };
+export type { CustomProviderProps } from './CustomProvider';
+
 export default CustomProvider;
