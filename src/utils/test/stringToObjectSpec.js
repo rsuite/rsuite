18650a53d0ca92431c217e50c970727e870e8b27
simonguo <simonguo.2009@gmail.com>
2021-09-01 19:03:55 +0800

    test(utils): add tests for utils

---
+++ src/utils/test/stringToObjectSpec.js
@@ -0,0 +1,17 @@
+import stringToObject from '../stringToObject';
+
+describe('[utils] stringToObject', () => {
+  it('Should create an object', () => {
+    const obj = stringToObject('foo', 'value', 'label');
+
+    assert.equal(obj.value, 'foo');
+    assert.equal(obj.label, 'foo');
+  });
+
+  it('Should keep the default object', () => {
+    const obj = { foo: 1 };
+    const newObj = stringToObject(obj, 'value', 'label');
+
+    assert.equal(obj, newObj);
+  });
+});
