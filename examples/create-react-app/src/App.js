0904fa719eb13d60f00ebee662776257138b3753
曹立泽 <lize.cao@mail.hypers.com>
2021-11-03 11:25:03 +0800

    build: upgrade to rsuite5

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
-import 'rsuite/lib/styles/themes/dark/index.less';
+import 'rsuite/dist/rsuite.min.css';
 
-/** import default css */
-// import 'rsuite/dist/styles/rsuite-default.css';
+/**
+ * 支持本地化、Right to Left、主题等个性化配置。
+ * @CustomProvider
+ */
 
-/** import dark css */
-// import 'rsuite/dist/styles/rsuite-dark.css';
-
-import { Button } from 'rsuite';
+import { Button, CustomProvider } from 'rsuite';
 
 function App() {
+  const [theme, setTheme] = useState('light');
   return (
-    <div className="App">
-      <Button appearance="primary"> Hello world </Button>
-    </div>
+    <CustomProvider theme={theme}>
+      <div className="App">
+        <Button appearance="default" onClick={() => setTheme('light')}>
+          {' '}
+          浅色主题 (默认){' '}
+        </Button>
+        <Button appearance="primary" onClick={() => setTheme('dark')}>
+          {' '}
+          深色主题{' '}
+        </Button>
+        <Button appearance="ghost" onClick={() => setTheme('high-contrast')}>
+          {' '}
+          高对比度主题{' '}
+        </Button>
+      </div>
+    </CustomProvider>
   );
 }
 
