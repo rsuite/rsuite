523381892799851c789e4d0bcfd9009a0d84dcf6
曹立泽 <lize.cao@mail.hypers.com>
2021-11-03 14:52:19 +0800

    docs: update example for create-react-app

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,28 +1,34 @@
 import React, { useState } from 'react';
 import './App.css';
 
-import 'rsuite/dist/rsuite.min.css';
+// import default style
+import 'rsuite/styles/index.less'; // or 'rsuite/dist/rsuite.min.css'
 
 /**
- * 支持本地化、Right to Left、主题等个性化配置
+ * Support personalized configurations such as localization, Right to Left, and themes
  */
 
-import { Button, CustomProvider } from 'rsuite';
+import { Button, CustomProvider, ButtonToolbar } from 'rsuite';
 
 function App() {
   const [theme, setTheme] = useState('light');
+
+  const switchTheme = e => setTheme(e.target.value);
+
   return (
     <CustomProvider theme={theme}>
       <div className="App">
-        <Button appearance="default" onClick={() => setTheme('light')} style={{ margin: 10 }}>
-          浅色主题(默认)
-        </Button>
-        <Button appearance="primary" onClick={() => setTheme('dark')} style={{ margin: 10 }}>
-          深色主题
-        </Button>
-        <Button appearance="ghost" onClick={() => setTheme('high-contrast')} style={{ margin: 10 }}>
-          高对比度主题
-        </Button>
+        <ButtonToolbar>
+          <Button appearance="default" onClick={switchTheme} value="light">
+            Light theme(default)
+          </Button>
+          <Button appearance="primary" onClick={switchTheme} value="dark">
+            Dark theme
+          </Button>
+          <Button appearance="ghost" onClick={switchTheme} value="high-contrast">
+            High contrast theme
+          </Button>
+        </ButtonToolbar>
       </div>
     </CustomProvider>
   );
