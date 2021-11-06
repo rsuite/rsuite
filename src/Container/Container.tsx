3c40d013b477a1da0867d9c3eb858003865a21ac
MarvelSQ <sun1993hlj@hotmail.com>
2021-11-06 11:22:08 +0800

    feat(Container): add `hasSidebar` to props

--- src/Container/Container.tsx
+++ src/Container/Container.tsx
@@ -3,39 +3,48 @@ import PropTypes from 'prop-types';
 import { useClassNames } from '../utils';
 import { WithAsProps } from '../@types/common';
 
-export type ContainerProps = WithAsProps & React.HTMLAttributes<HTMLDivElement>;
+export type ContainerProps = WithAsProps &
+  React.HTMLAttributes<HTMLDivElement> & {
+    hasSidebar?: boolean;
+  };
 export const ContainerContext = React.createContext<ContainerContextValue>({});
 
 interface ContainerContextValue {
   setHasSidebar?: (value: boolean) => void;
 }
 
-const Container = React.forwardRef((props: ContainerProps, ref: React.Ref<HTMLDivElement>) => {
-  const {
-    as: Component = 'section',
-    classPrefix = 'container',
-    className,
-    children,
-    ...rest
-  } = props;
-  const [hasSidebar, setHasSidebar] = useState(false);
-  const { withClassPrefix, merge } = useClassNames(classPrefix);
-  const classes = merge(className, withClassPrefix({ 'has-sidebar': hasSidebar }));
+const Container = React.forwardRef(
+  (
+    { hasSidebar: PropsHasSidebar = false, ...props }: ContainerProps,
+    ref: React.Ref<HTMLDivElement>
+  ) => {
+    const {
+      as: Component = 'section',
+      classPrefix = 'container',
+      className,
+      children,
+      ...rest
+    } = props;
+    const [hasSidebar, setHasSidebar] = useState(PropsHasSidebar);
+    const { withClassPrefix, merge } = useClassNames(classPrefix);
+    const classes = merge(className, withClassPrefix({ 'has-sidebar': hasSidebar }));
 
-  return (
-    <ContainerContext.Provider value={{ setHasSidebar }}>
-      <Component {...rest} ref={ref} className={classes}>
-        {children}
-      </Component>
-    </ContainerContext.Provider>
-  );
-});
+    return (
+      <ContainerContext.Provider value={{ setHasSidebar }}>
+        <Component {...rest} ref={ref} className={classes}>
+          {children}
+        </Component>
+      </ContainerContext.Provider>
+    );
+  }
+);
 
 Container.displayName = 'Container';
 Container.propTypes = {
   className: PropTypes.string,
   children: PropTypes.node,
-  classPrefix: PropTypes.string
+  classPrefix: PropTypes.string,
+  hasSidebar: PropTypes.bool
 };
 
 export default Container;
