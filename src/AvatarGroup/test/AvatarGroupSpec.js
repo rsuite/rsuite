f12e5a9e24a5d1c45580bb550970b6b395b7d4d6
simonguo <simonguo.2009@gmail.com>
2021-12-10 13:43:11 +0800

    test(AvatarGroup): use testing-library

--- src/AvatarGroup/test/AvatarGroupSpec.js
+++ src/AvatarGroup/test/AvatarGroupSpec.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import AvatarGroup from '../AvatarGroup';
 import Avatar from '../../Avatar';
-import { getDOMNode } from '@test/testUtils';
+import { render } from '@testing-library/react';
 
 describe('AvatarGroup', () => {
   it('Should change the size of all avatars', () => {
-    const instance = getDOMNode(
-      <AvatarGroup size="xs">
+    const { getByTestId } = render(
+      <AvatarGroup size="xs" data-testid="group">
         <Avatar>A</Avatar>
         <Avatar>B</Avatar>
       </AvatarGroup>
     );
-    assert.equal(instance.querySelectorAll('.rs-avatar-xs').length, 2);
+
+    assert.equal(getByTestId('group').querySelectorAll('.rs-avatar-xs').length, 2);
   });
 
   it('Should set the spacing between the avatars', () => {
-    const instance = getDOMNode(
-      <AvatarGroup spacing={10}>
+    const { getByTestId } = render(
+      <AvatarGroup spacing={10} data-testid="group">
+        <Avatar>A</Avatar>
         <Avatar>A</Avatar>
         <Avatar>B</Avatar>
         {[<Avatar key={1}>C</Avatar>, <Avatar key={2}>D</Avatar>]}
       </AvatarGroup>
     );
 
-    const avatars = instance.querySelectorAll('.rs-avatar');
+    const avatars = getByTestId('group').querySelectorAll('.rs-avatar');
 
     assert.equal(avatars[0].style.marginRight, '10px');
     assert.equal(avatars[1].style.marginRight, '10px');
@@ -32,18 +34,18 @@ describe('AvatarGroup', () => {
   });
 
   it('Should be stack', () => {
-    const instance = getDOMNode(<AvatarGroup stack />);
-    assert.include(instance.className, 'rs-avatar-group-stack');
+    const { getByTestId } = render(<AvatarGroup stack data-testid="group" />);
+    assert.include(getByTestId('group').className, 'rs-avatar-group-stack');
   });
 
   it('Should have a custom style', () => {
     const fontSize = '12px';
-    const instance = getDOMNode(<AvatarGroup style={{ fontSize }} />);
-    assert.equal(instance.style.fontSize, fontSize);
+    const { getByTestId } = render(<AvatarGroup style={{ fontSize }} data-testid="group" />);
+    assert.equal(getByTestId('group').style.fontSize, fontSize);
   });
 
   it('Should have a custom className prefix', () => {
-    const instance = getDOMNode(<AvatarGroup classPrefix="custom-prefix" />);
-    assert.include(instance.className, 'custom-prefix');
+    const { getByTestId } = render(<AvatarGroup classPrefix="custom-prefix" data-testid="group" />);
+    assert.include(getByTestId('group').className, 'custom-prefix');
   });
 });
