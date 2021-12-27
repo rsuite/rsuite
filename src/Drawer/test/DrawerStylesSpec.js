34aaad92038cb09f07b867fa66038be2410b7c25
simonguo <simonguo.2009@gmail.com>
2021-12-27 09:43:31 +0800

    fix(Drawer): fix Drawer cannot be closed when backdrop is clicked

--- src/Drawer/test/DrawerStylesSpec.js
+++ src/Drawer/test/DrawerStylesSpec.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import Drawer from '../index';
-import { render, getStyle } from '@test/testUtils';
+import { render } from '@testing-library/react';
+import { getStyle } from '@test/testUtils';
 
 import '../styles/index.less';
 
 describe('Drawer styles', () => {
   it('Should render the correct styles', () => {
     const instanceRef = React.createRef();
-    render(<Drawer ref={instanceRef} open />);
-    const dom = instanceRef.current;
-    const drawerDom = dom.querySelector('.rs-drawer');
-    assert.equal(getStyle(dom, 'position'), 'fixed');
-    assert.equal(getStyle(dom, 'zIndex'), '1050');
-    assert.equal(getStyle(drawerDom, 'position'), 'fixed');
-    assert.equal(getStyle(drawerDom, 'zIndex'), '1050');
-    assert.equal(getStyle(drawerDom, 'overflow'), 'visible');
 
-    // Skip this assertion
-    // const backdropDom = dom.querySelector('.rs-drawer-backdrop');
-    // assert.equal(getStyle(backdropDom, 'backgroundColor'), toRGB('#272c36'));
+    render(<Drawer ref={instanceRef} open />);
+
+    const drawer = instanceRef.current.querySelector('.rs-drawer');
+
+    assert.equal(getStyle(drawer, 'position'), 'fixed');
+    assert.equal(getStyle(drawer, 'zIndex'), '1050');
+    assert.equal(getStyle(drawer, 'overflow'), 'visible');
+  });
+
+  it('Should have a wrapper that fills the window', () => {
+    const instanceRef = React.createRef();
+    render(<Drawer ref={instanceRef} open />);
+
+    const wrapper = instanceRef.current;
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+
+    assert.equal(getStyle(wrapper, 'position'), 'fixed');
+    assert.equal(getStyle(wrapper, 'zIndex'), '1050');
+    assert.equal(getStyle(wrapper, 'width'), `${windowWidth}px`);
+    assert.equal(getStyle(wrapper, 'height'), `${windowHeight}px`);
+    assert.equal(getStyle(wrapper, 'left'), `0px`);
+    assert.equal(getStyle(wrapper, 'top'), `0px`);
   });
 });
