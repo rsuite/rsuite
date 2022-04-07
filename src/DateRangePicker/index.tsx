76e4bc38e8e939ae6ffbcf5bc154281b4ec9360c
Simon Guo <simonguo.2009@gmail.com>
2022-04-07 19:12:55 +0800

    feat(DateRangePicker): export type definition (#2434)

--- src/DateRangePicker/index.tsx
+++ src/DateRangePicker/index.tsx
@@ -1,4 +1,5 @@
 import DateRangePicker from './DateRangePicker';
 
 export type { DateRangePicker, DateRangePickerProps } from './DateRangePicker';
+export type { ValueType, DateRange, RangeType, DisabledDateFunction } from './types';
 export default DateRangePicker;
