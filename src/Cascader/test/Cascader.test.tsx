2a8ef481e49dec2570868982e0dbbba808efbc03
Doma <leishenghao@126.com>
2022-04-14 20:49:37 +0800

    fix(Cascader): infer value and onChange types from data (#2449)

---
+++ src/Cascader/test/Cascader.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { expectType } from 'ts-expect';
+import { PickerInstance } from '../../Picker';
+import Cascader from '../Cascader';
+
+// Infer value and onChange types from data
+const numberValuedData = [{ label: 'One', value: 1 }];
+
+<Cascader data={numberValuedData} value={1} />;
+// @ts-expect-error should not accept string value
+<Cascader data={numberValuedData} value="1" />;
+<Cascader
+  data={numberValuedData}
+  onChange={newValue => {
+    expectType<number | null>(newValue);
+  }}
+/>;
+
+const stringValuedData = [{ label: 'One', value: 'One' }];
+
+<Cascader data={stringValuedData} value="1" />;
+// @ts-expect-error should not accept number value
+<Cascader data={stringValuedData} value={1} />;
+<Cascader
+  data={stringValuedData}
+  onChange={newValue => {
+    expectType<string | null>(newValue);
+  }}
+/>;
+
+const pickerRef = React.createRef<PickerInstance>();
+
+<Cascader ref={pickerRef} data={[]} />;
