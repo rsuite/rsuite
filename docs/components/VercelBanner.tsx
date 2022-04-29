530139d71c892d8166d7a77a80a1705fa956173b
Doma <leishenghao@126.com>
2022-04-29 10:35:07 +0800

    docs: add vercel banner

---
+++ docs/components/VercelBanner.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const VercelBanner = () => (
+  <a
+    href="https://vercel.com?utm_source=rsuite&utm_campaign=oss"
+    target="_blank"
+    rel="noreferrer noopener"
+  >
+    <img
+      src="https://www.datocms-assets.com/31049/1618983297-powered-by-vercel.svg"
+      alt="Vercel banner"
+    />
+  </a>
+);
