956571b913971cb304eef814d7125b8768bbc86b
simonguo <simonguo.2009@gmail.com>
2022-06-02 11:57:40 +0800

    fix(useToaster): add checks for containers

--- src/toaster/useToaster.ts
+++ src/toaster/useToaster.ts
@@ -3,23 +3,26 @@ import toaster from './toaster';
 import { ToastContainerProps } from './ToastContainer';
 import { useCustom } from '../utils';
 
+/**
+ * Hook to use the toaster
+ * @returns toaster { push, remove, clear }
+ */
 const useToaster = () => {
   const { toasters } = useCustom();
 
   return {
-    push: (message: React.ReactNode, options: ToastContainerProps = {}) => {
-      if (toasters) {
-        return toasters?.current.get(options.placement || 'topCenter')?.push(message);
-      }
-      return toaster.push(message, options);
+    push: (message: React.ReactNode, options?: ToastContainerProps) => {
+      const customToaster = toasters?.current?.get(options?.placement || 'topCenter');
+
+      return customToaster ? customToaster.push(message) : toaster.push(message, options);
     },
     remove: (key: string) => {
       toasters
-        ? Array.from(toasters?.current).forEach(([, c]) => c?.remove(key))
+        ? Array.from(toasters.current).forEach(([, c]) => c?.remove(key))
         : toaster.remove(key);
     },
     clear: () => {
-      toasters ? Array.from(toasters?.current).forEach(([, c]) => c?.clear()) : toaster.clear();
+      toasters ? Array.from(toasters.current).forEach(([, c]) => c?.clear()) : toaster.clear();
     }
   };
 };
