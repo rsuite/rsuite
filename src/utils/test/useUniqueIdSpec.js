710b0eb0c81e34fa3118ec510fd7f71a9acd798b
simonguo <simonguo.2009@gmail.com>
2022-07-17 17:02:08 +0800

    refactor(useUniqueId): use React 18 new API useId in useUniqueId

---
+++ src/utils/test/useUniqueIdSpec.js
@@ -0,0 +1,22 @@
+import { renderHook } from '@test/testUtils';
+import useUniqueId from '../useUniqueId';
+
+describe('[utils] useUniqueId', () => {
+  it('Should generate an Id', () => {
+    const { result } = renderHook(() => useUniqueId());
+
+    expect(result.current).to.be.not.equal('');
+  });
+
+  it('Should generate an Id with prefix', () => {
+    const { result } = renderHook(() => useUniqueId('rs-'));
+
+    expect(result.current).to.be.contain('rs-');
+  });
+
+  it('Should use the provided Id non-generated', () => {
+    const { result } = renderHook(() => useUniqueId('rs-', 'my-id'));
+
+    expect(result.current).to.be.equal('my-id');
+  });
+});
