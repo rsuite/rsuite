492590979c5e14fa3960bbf319999d3878e49852
jxj <33154783+BlindMonkLeeSin@users.noreply.github.com>
2022-08-04 11:25:24 +0800

    docs(examples): add example to umi (#2634)
    * docs(examples): add example to umi
    
    * feat(examples): update example to umi
    
    Co-authored-by: jixianjing <xianjing.ji@mail.hypers.com>

---
+++ examples/with-umi/src/pages/docs.tsx
@@ -0,0 +1,16 @@
+import { Container, Content, Header } from 'rsuite';
+
+import yayJpg from '../assets/yay.jpg';
+
+export default function DocsPage() {
+  return (
+    <Container>
+      <Header>
+        <h2>Docs</h2>
+      </Header>
+      <Content>
+        <img src={yayJpg} />
+      </Content>
+    </Container>
+  );
+}
