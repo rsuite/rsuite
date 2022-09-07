b47a048f358ca187079ec049340288c72c8999bd
Superman <supermanchc@gmail.com>
2022-09-07 11:12:54 +0800

    fix(CheckTreePicker): fix uncheckable style errors (#2695)
    * fix(CheckTreePicker): fix uncheckable style errors
    
    * test(CheckTreePicker): update test for uncheckable state

--- src/CheckTreePicker/test/CheckTreePickerStylesSpec.js
+++ src/CheckTreePicker/test/CheckTreePickerStylesSpec.js
@@ -46,4 +46,21 @@ describe('CheckTreePicker styles', () => {
     const itemLabel = document.body.querySelector('.rs-check-tree .rs-checkbox-checker label');
     assert.equal(getStyle(itemLabel, 'padding'), '8px 12px 8px 32px');
   });
+
+  itChrome('Should render the correct styles when first level data is unchecked', () => {
+    const instanceRef = React.createRef();
+    render(
+      <CheckTreePicker
+        data={[
+          { value: 1, label: '1', children: [{ value: 2, label: '2' }] },
+          { value: 3, label: '3' }
+        ]}
+        uncheckableItemValues={[1, 3]}
+        ref={instanceRef}
+        open
+      />
+    );
+    const itemLabel = document.body.querySelector('.rs-check-tree .rs-checkbox-checker label');
+    assert.equal(getStyle(itemLabel, 'padding'), '8px 12px 8px 22px');
+  });
 });
