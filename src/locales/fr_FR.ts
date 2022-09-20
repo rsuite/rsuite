a766734d2947e68d753ae9bc78aeddd1ef6c19ce
KODSU <17864005+kodsu@users.noreply.github.com>
2022-09-20 08:57:40 +0200

    Create fr_FR.ts

---
+++ src/locales/fr_FR.ts
@@ -0,0 +1,78 @@
+import fr from 'date-fns/locale/fr';
+
+const Calendar = {
+  sunday: 'Di',
+  monday: 'Lu',
+  tuesday: 'Ma',
+  wednesday: 'Me',
+  thursday: 'Je',
+  friday: 'Ve',
+  saturday: 'Sa',
+  ok: 'OK',
+  today: "Aujourd'hui",
+  yesterday: 'Hier',
+  hours: 'Heures',
+  minutes: 'Minutes',
+  seconds: 'Secondes',
+  formattedMonthPattern: 'MMM yyyy',
+  formattedDayPattern: 'dd MMM yyyy',
+  dateLocale: fr as any
+};
+
+export default {
+  common: {
+    loading: 'Chargement...',
+    emptyMessage: 'Aucune donnée trouvée'
+  },
+  Plaintext: {
+    unfilled: 'Non rempli',
+    notSelected: 'Non sélectionné',
+    notUploaded: 'Non uploadé'
+  },
+  Pagination: {
+    more: 'Plus',
+    prev: 'Précédent',
+    next: 'Suivant',
+    first: 'Premier',
+    last: 'Dernier',
+    limit: '{0} / page',
+    total: 'Lignes totales: {0}',
+    skip: 'Aller à{0}'
+  },
+  Calendar,
+  DatePicker: {
+    ...Calendar
+  },
+  DateRangePicker: {
+    ...Calendar,
+    last7Days: '7 Derniers Jours'
+  },
+  Picker: {
+    noResultsText: 'Aucun résultat trouvé',
+    placeholder: 'Sélectionner',
+    searchPlaceholder: 'Rechercher',
+    checkAll: 'Tous'
+  },
+  InputPicker: {
+    newItem: 'Nouvel item',
+    createOption: 'Option de création "{0}"'
+  },
+  Uploader: {
+    inited: 'Initial',
+    progress: 'Chargement',
+    error: 'Erreur',
+    complete: 'Terminé',
+    emptyFile: 'Vide',
+    upload: 'Uploader'
+  },
+  CloseButton: {
+    closeLabel: 'Fermer'
+  },
+  Breadcrumb: {
+    expandText: 'Afficher le chemin'
+  },
+  Toggle: {
+    on: 'Ouvrir',
+    off: 'Fermer'
+  }
+};
