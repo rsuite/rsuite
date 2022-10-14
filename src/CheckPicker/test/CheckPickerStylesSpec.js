cc58f7e7e8859dd26a62db43bdcb7b1663c9cc4d
Simon Guo <simonguo.2009@gmail.com>
2022-10-14 21:53:12 +0800

    fix(AutoSizer): fix virtualized list width not being updated (#2806)

--- src/CheckPicker/test/CheckPickerStylesSpec.js
+++ src/CheckPicker/test/CheckPickerStylesSpec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import CheckPicker from '../index';
 import { getStyle, inChrome } from '@test/testUtils';
 
@@ -31,5 +31,21 @@ describe('CheckPicker styles', () => {
       '.rs-picker-check-menu-items .rs-checkbox-checker label'
     );
     inChrome && assert.equal(getStyle(menuItemLabel, 'padding'), '8px 12px 8px 38px');
+  });
+
+  it('Should change the width of the virtualized list', done => {
+    const { getByRole } = render(
+      <CheckPicker
+        data={data}
+        style={{ width: 400 }}
+        virtualized
+        onOpen={() => {
+          expect(getByRole('listbox').firstChild.firstChild).to.style('width', '400px');
+          done();
+        }}
+      />
+    );
+
+    fireEvent.click(getByRole('combobox'));
   });
 });
