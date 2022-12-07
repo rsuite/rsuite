872bab6c0b9ecd8a98620d006223ad0f64cbf27e
Doma <leishenghao@126.com>
2022-12-07 17:32:01 +0800

    fix(Form.HelpText): change font size to 12px

--- src/FormHelpText/test/FormHelpTextStylesSpec.tsx
+++ src/FormHelpText/test/FormHelpTextStylesSpec.tsx
@@ -12,5 +12,8 @@ describe('FormHelpText styles', () => {
     const dom = getDOMNode(instanceRef.current);
     assert.equal(getStyle(dom, 'display'), 'block', 'FormHelpText display');
     assert.equal(getStyle(dom, 'color'), toRGB('#8e8e93'), 'FormHelpText color');
+
+    expect(dom).to.have.style('font-size', '12px');
+    expect(dom).to.have.style('line-height', '20px');
   });
 });
