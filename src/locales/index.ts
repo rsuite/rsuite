1026c36755621fff0b04455e8a464bec3bc3662b
Michael <1381694+mvanleest@users.noreply.github.com>
2023-01-26 00:46:05 +0100

    feat: added Dutch locale (nl_NL)

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -10,6 +10,7 @@ export { default as fiFI } from './fi_FI';
 export { default as itIT } from './it_IT';
 export { default as kkKZ } from './kk_KZ';
 export { default as koKR } from './ko_KR';
+export { default as nlNL } from './nl_NL';
 export { default as ptBR } from './pt_BR';
 export { default as ruRU } from './ru_RU';
 export { default as svSE } from './sv_SE';
---
+++ src/locales/nl_NL.ts
@@ -0,0 +1,78 @@
+import nl from 'date-fns/locale/nl';
+
+const Calendar = {
+  sunday: 'Zo',
+  monday: 'Ma',
+  tuesday: 'Di',
+  wednesday: 'Wo',
+  thursday: 'Do',
+  friday: 'Vr',
+  saturday: 'Za',
+  ok: 'Oke',
+  today: 'Vandaag',
+  yesterday: 'Gisteren',
+  hours: 'Uren',
+  minutes: 'Mimnuten',
+  seconds: 'Seconden',
+  formattedMonthPattern: 'MMM, yyyy',
+  formattedDayPattern: 'dd MMM, yyyy',
+  dateLocale: nl as any
+};
+
+export default {
+  common: {
+    loading: 'Laden...',
+    emptyMessage: 'Geen data gevonden'
+  },
+  Plaintext: {
+    unfilled: 'Ongevuld',
+    notSelected: 'Niet geselecteerd',
+    notUploaded: 'Niet geupload'
+  },
+  Pagination: {
+    more: 'Meer',
+    prev: 'Vorige',
+    next: 'Volgende',
+    first: 'Eerste',
+    last: 'Laatste',
+    limit: '{0} / pagina',
+    total: 'Totaal Rijen: {0}',
+    skip: 'Ga naar{0}'
+  },
+  Calendar,
+  DatePicker: {
+    ...Calendar
+  },
+  DateRangePicker: {
+    ...Calendar,
+    last7Days: 'Laatste 7 Dagen'
+  },
+  Picker: {
+    noResultsText: 'Geen resultaten gevonden',
+    placeholder: 'Selecteren',
+    searchPlaceholder: 'Zoeken',
+    checkAll: 'Alles'
+  },
+  InputPicker: {
+    newItem: 'Nieuw item',
+    createOption: 'Creëer optie "{0}"'
+  },
+  Uploader: {
+    inited: 'Gestart',
+    progress: 'Uploaden',
+    error: 'Error',
+    complete: 'Voltooid',
+    emptyFile: 'Leeg',
+    upload: 'Upload'
+  },
+  CloseButton: {
+    closeLabel: 'Sluiten'
+  },
+  Breadcrumb: {
+    expandText: 'Toon pad'
+  },
+  Toggle: {
+    on: 'Open',
+    off: 'Sluit'
+  }
+};
