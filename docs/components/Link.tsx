3e4fada31841b5d4d69c997c553a1890c30f83ee
Doma <leishenghao@126.com>
2023-03-15 14:39:57 +0800

    build(docs): Leverage Next.js built-in features (#3109)
    * build(docs): serve design materials using nextjs rewrites
    
    * build(docs): use nextjs internationalized routing
    
    * build(docs): simplify build scripts
    
    * fix(docs): fix missing button style on homepage
    
    * build(docs): disable locale detection
    
    * build(docs): remove unused lines from Dockerfile

--- docs/components/Link.tsx
+++ docs/components/Link.tsx
@@ -1,30 +1,21 @@
 import React from 'react';
 import NextLink, { LinkProps } from 'next/link';
-import AppContext from './AppContext';
 
 interface MyLinkProps extends LinkProps {
   className?: string;
   children?: React.ReactNode;
 }
 
-const paths = {
-  en: '',
-  zh: '/zh'
-};
-
-export const languageToPath = language => paths[language] || '';
-
 export default React.forwardRef(function Link(
   props: MyLinkProps,
   ref: React.RefObject<HTMLAnchorElement>
 ) {
   const { as, href, prefetch, scroll, passHref, replace, shallow, className, ...rest } = props;
-  const { language } = React.useContext(AppContext);
   return (
     <NextLink
       href={href}
       prefetch={prefetch}
-      as={as ? as : `${languageToPath(language)}${href}`}
+      as={as}
       scroll={scroll}
       passHref={passHref}
       replace={replace}
