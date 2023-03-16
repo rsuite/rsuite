0c385e56fe770e9af311414cdab53decb6fc2102
simonguo <simonguo.2009@gmail.com>
2023-03-16 18:57:26 +0800

    fix(Table): fix missing `ref` in type definition

--- src/Table/index.tsx
+++ src/Table/index.tsx
@@ -1,5 +1,16 @@
 import Table from './Table';
-export type { TableProps, ColumnProps, ColumnGroupProps, TableLocaleType } from 'rsuite-table';
-export type { TableInstance, CellProps } from './Table';
+export type {
+  TableProps,
+  ColumnProps,
+  ColumnGroupProps,
+  HeaderCellProps,
+  CellProps,
+  SortType,
+  RowDataType,
+  RowKeyType,
+  TableLocaleType,
+  TableSizeChangeEventName,
+  TableInstance
+} from 'rsuite-table';
 
 export default Table;
