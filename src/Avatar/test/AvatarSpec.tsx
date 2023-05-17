a9a85bbbc5b6e0072306788323d99fd911e82a20
Doma <leishenghao@126.com>
2023-05-17 15:25:05 +0800

    test(Avatar): drop usage of getDOMNode util

--- src/Avatar/test/AvatarSpec.tsx
+++ src/Avatar/test/AvatarSpec.tsx
@@ -1,69 +1,65 @@
 import React from 'react';
-import { getDOMNode } from '@test/testUtils';
 import { testStandardProps } from '@test/commonCases';
 import Avatar from '../Avatar';
+import { render, screen } from '@testing-library/react';
 
 describe('Avatar', () => {
   testStandardProps(<Avatar />);
 
   it('Should render avatar', () => {
     const content = 'RS';
-    const instance = getDOMNode(<Avatar>{content}</Avatar>);
-    assert.equal(instance.textContent, content);
+    render(<Avatar>{content}</Avatar>);
+
+    expect(screen.getByText(content)).to.exist;
   });
 
   it('Change background', () => {
     const background = 'rgb(123, 31, 162)';
-    const instance = getDOMNode(<Avatar style={{ background }}>RS</Avatar>);
-    assert.equal(instance.style.backgroundColor, background);
+    const { container } = render(<Avatar style={{ background }}>RS</Avatar>);
+    expect(container.firstChild).to.have.style('background-color', background);
   });
 
   it('Should render image avatar', () => {
     const src = 'https://avatars2.githubusercontent.com/u/12592949?s=460&v=4';
     const alt = 'RS';
-    const instance = getDOMNode(
+    render(
       <Avatar src={src} alt={alt}>
         RS
       </Avatar>
     );
-    const avatarImg = instance.getElementsByClassName('rs-avatar-image')[0];
-    assert.notEqual(avatarImg, null);
-    assert.equal(avatarImg.getAttribute('src'), src);
-    assert.equal(avatarImg.getAttribute('alt'), alt);
+    const img = screen.getByRole('img');
+    expect(img).to.have.attr('src', src);
+    expect(img).to.have.attr('alt', alt);
   });
 
   it('Should render circle avatar', () => {
-    const instance = getDOMNode(<Avatar circle>RS</Avatar>);
-    assert.include(instance.className, 'rs-avatar-circle');
+    const { container } = render(<Avatar circle>RS</Avatar>);
+    expect(container.firstChild).to.have.class('rs-avatar-circle');
   });
 
   it('Should render srcSet', () => {
     const srcSet = 'elva-fairy-320w.jpg 320w,elva-fairy-480w.jpg 480w';
-    const instance = getDOMNode(<Avatar srcSet={srcSet} />);
-    assert.equal(
-      (instance.querySelector('img') as HTMLImageElement).getAttribute('srcset'),
-      srcSet
-    );
+    render(<Avatar srcSet={srcSet} />);
+    expect(screen.getByRole('img')).to.have.attr('srcset', srcSet);
   });
 
   it('Should render sizes', () => {
     const srcSet = 'elva-fairy-320w.jpg 320w,elva-fairy-480w.jpg 480w';
     const sizes = '(max-width: 320px) 280px,(max-width: 480px) 440px, 800px';
-    const instance = getDOMNode(<Avatar srcSet={srcSet} sizes={sizes} />);
-    assert.equal((instance.querySelector('img') as HTMLImageElement).getAttribute('sizes'), sizes);
+    render(<Avatar srcSet={srcSet} sizes={sizes} />);
+    expect(screen.getByRole('img')).to.have.attr('sizes', sizes);
   });
 
   it('Should render imgProps', () => {
-    const instance = getDOMNode(<Avatar src="bac.jpg" imgProps={{ id: 'img', title: 'Avatar' }} />);
-    assert.equal((instance.querySelector('img') as HTMLImageElement).getAttribute('id'), 'img');
-    assert.equal(
-      (instance.querySelector('img') as HTMLImageElement).getAttribute('title'),
-      'Avatar'
-    );
+    render(<Avatar src="bac.jpg" imgProps={{ id: 'img', title: 'Avatar' }} />);
+
+    expect(screen.getByRole('img')).to.have.id('img');
+    expect(screen.getByRole('img')).to.have.attr('title', 'Avatar');
   });
 
   it('Should apply size class', () => {
-    const instance = getDOMNode(<Avatar size="lg">RS</Avatar>);
-    assert.include(instance.className, 'rs-avatar-lg');
+    const { container } = render(<Avatar size="lg">RS</Avatar>);
+
+    expect(container.firstChild).to.have.class('rs-avatar-lg');
   });
 });
