c5d11f806f397f94972d8f25961e42d86db058a4
Doma <leishenghao@126.com>
2023-05-17 16:28:16 +0800

    test(ButtonToolbar): drop usage of getDOMNode util

--- src/ButtonToolbar/test/ButtonToolbarStyleSpec.tsx
+++ src/ButtonToolbar/test/ButtonToolbarStyleSpec.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import ButtonToolbar from '../ButtonToolbar';
-import { getDOMNode, getStyle } from '@test/testUtils';
 
 import '../styles/index.less';
 
 describe('ButtonToolbar styles', () => {
   it('Should render the correct vertical align', () => {
-    const instanceRef = React.createRef<HTMLDivElement>();
-    render(<ButtonToolbar ref={instanceRef} />);
-    assert.equal(getStyle(getDOMNode(instanceRef.current), 'line-height'), '0px');
+    const { container } = render(<ButtonToolbar />);
+
+    expect(container.firstChild).to.have.style('line-height', '0px');
   });
 });
