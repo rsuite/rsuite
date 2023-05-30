188a11263055e7de76281cdd194c849322a98a34
leon <hey@leonfong.me>
2023-05-30 11:21:08 +0800

    test(Tag): drop usage of getDOMNode util

--- src/Tag/test/TagSpec.tsx
+++ src/Tag/test/TagSpec.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import ReactTestUtils from 'react-dom/test-utils';
-import { getDOMNode } from '@test/testUtils';
 import { testStandardProps } from '@test/commonCases';
 import Tag from '../Tag';
 import Sinon from 'sinon';
+import { render } from '@testing-library/react';
 
 describe('Tag', () => {
   testStandardProps(<Tag />);
   it('Should output a Tag', () => {
-    const instance = getDOMNode(<Tag />);
-    assert.equal(instance.className, 'rs-tag rs-tag-md rs-tag-default');
+    const { container } = render(<Tag />);
+    expect(container.firstChild).to.have.class('rs-tag');
   });
 
   it('Should call onClose callback', () => {
     const onClose = Sinon.spy();
-    const instance = getDOMNode(
+    const { container } = render(
       <Tag closable onClose={onClose}>
         tag
       </Tag>
     );
-    ReactTestUtils.Simulate.click(instance.querySelector('.rs-tag-icon-close') as HTMLElement);
-
+    ReactTestUtils.Simulate.click(
+      // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+      container.querySelector('.rs-tag-icon-close') as HTMLElement
+    );
     expect(onClose).to.have.been.calledOnce;
   });
 });
