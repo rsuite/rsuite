cdb3c7412e793c1d89958ea6fddabbf91d326575
Doma <leishenghao@126.com>
2023-07-06 10:44:14 +0800

    fix(Uploader): correct type for status argument of onError callback (#3273)

--- src/Uploader/test/Uploader.test.tsx
+++ src/Uploader/test/Uploader.test.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import Uploader from '../Uploader';
+import { expectType } from 'ts-expect';
 
 <Uploader appearance="primary" action="#" />;
 
 <Uploader appearance="primary" color="orange" action="#" />;
 
 <Uploader size="lg" action="#" />;
+
+<Uploader
+  action="#"
+  onError={status => {
+    expectType<'timeout' | 'server_error' | 'xhr_error'>(status.type);
+  }}
+/>;
