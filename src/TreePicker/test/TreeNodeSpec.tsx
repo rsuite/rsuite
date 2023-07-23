bd61491eee65e8223c2b2b5192ea140ed51c3685
Christian Caracach <christiancaracach@gmail.com>
2023-07-23 10:52:19 -0300

    test(TreeNode): removing use of getDOMNode

--- src/TreePicker/test/TreeNodeSpec.tsx
+++ src/TreePicker/test/TreeNodeSpec.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { Simulate } from 'react-dom/test-utils';
 import sinon from 'sinon';
-import { getDOMNode } from '@test/testUtils';
 import TreeNode from '../TreeNode';
+import { render } from '@testing-library/react';
 
 describe('TreePicker - TreeNode', () => {
   it('Should render tree node', () => {
-    const instance = getDOMNode(<TreeNode layer={0} visible nodeData={{}} />);
+    const { container } = render(<TreeNode layer={0} visible nodeData={{}} />);
 
-    assert.include(instance.className, 'rs-tree-node');
-    assert.include(instance.getAttribute('role'), 'treeitem');
+    expect(container.firstChild).to.have.class('rs-tree-node');
+    expect(container.firstChild).to.have.attr('role', 'treeitem');
   });
 
   it('Should call `onDragStart` callback', () => {
     const onDragStartSpy = sinon.spy();
-    const instance = getDOMNode(
+    const { container } = render(
       <TreeNode layer={0} onDragStart={onDragStartSpy} nodeData={1} visible />
     );
-
-    Simulate.dragStart(instance);
+    //eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const div = container.querySelector('div') as HTMLElement;
+    Simulate.dragStart(div); //Not working
 
     assert.isTrue(onDragStartSpy.calledOnce);
     assert.equal(onDragStartSpy.firstCall.firstArg, 1);
@@ -26,11 +27,13 @@ describe('TreePicker - TreeNode', () => {
 
   it('Should call `onDragEnter` callback', () => {
     const onDragEnterSpy = sinon.spy();
-    const instance = getDOMNode(
+    const { container } = render(
       <TreeNode layer={0} onDragEnter={onDragEnterSpy} nodeData={1} visible />
     );
+    //eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const div = container.querySelector('div') as HTMLElement;
 
-    Simulate.dragEnter(instance);
+    Simulate.dragEnter(div);
 
     assert.isTrue(onDragEnterSpy.calledOnce);
     assert.equal(onDragEnterSpy.firstCall.firstArg, 1);
@@ -38,11 +41,12 @@ describe('TreePicker - TreeNode', () => {
 
   it('Should call `onDragOver` callback', () => {
     const onDragOverSpy = sinon.spy();
-    const instance = getDOMNode(
+    const { container } = render(
       <TreeNode layer={0} onDragOver={onDragOverSpy} nodeData={1} visible />
     );
-
-    Simulate.dragOver(instance);
+    //eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const div = container.querySelector('div') as HTMLElement;
+    Simulate.dragOver(div);
 
     assert.isTrue(onDragOverSpy.calledOnce);
     assert.equal(onDragOverSpy.firstCall.firstArg, 1);
@@ -50,11 +54,13 @@ describe('TreePicker - TreeNode', () => {
 
   it('Should call `onDragEnd` callback', () => {
     const onDragEndSpy = sinon.spy();
-    const instance = getDOMNode(
+    const { container } = render(
       <TreeNode layer={0} onDragEnd={onDragEndSpy} nodeData={1} visible />
     );
+    //eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+    const div = container.querySelector('div') as HTMLElement;
 
-    Simulate.dragEnd(instance);
+    Simulate.dragEnd(div);
 
     assert.isTrue(onDragEndSpy.calledOnce);
     assert.equal(onDragEndSpy.firstCall.firstArg, 1);
