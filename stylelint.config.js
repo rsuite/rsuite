4a582138e494d23f0b578fa520275fbcd00c2e30
Doma <leishenghao@126.com>
2023-07-25 17:38:06 +0800

    wip chore(stylelint): adopt stylelint-config-standard-less

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,6 @@
 /** @type {import('stylelint').Config} */
 module.exports = {
-  extends: ['stylelint-config-standard'],
+  extends: ['stylelint-config-standard-less'],
   customSyntax: 'postcss-less',
   rules: {
     'import-notation': 'string',
