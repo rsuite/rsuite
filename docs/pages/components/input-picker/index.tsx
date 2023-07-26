c85327317b5264438ae8e044b669e975310a5d0b
simonguo <simonguo.2009@gmail.com>
2023-07-26 15:26:14 +0800

    docs(InputPicker): fix sample data is incomplete

--- docs/pages/components/input-picker/index.tsx
+++ docs/pages/components/input-picker/index.tsx
@@ -3,6 +3,9 @@ import { InputPicker, Button } from 'rsuite';
 import SpinnerIcon from '@rsuite/icons/legacy/Spinner';
 import DefaultPage from '@/components/Page';
 
+import UserIcon from '@rsuite/icons/legacy/User';
+import GroupIcon from '@rsuite/icons/legacy/Group';
+
 export default function Page() {
-  return <DefaultPage dependencies={{ InputPicker, Button, SpinnerIcon }} />;
+  return <DefaultPage dependencies={{ InputPicker, Button, SpinnerIcon, UserIcon, GroupIcon }} />;
 }
