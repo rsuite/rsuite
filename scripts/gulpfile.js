a80f40f360309d415d1b91eefcbcd74d79f17648
simonguo <simonguo.2009@gmail.com>
2023-11-21 11:48:50 +0800

    feat(CSS): add compatibility for relative color syntax

--- scripts/gulpfile.js
+++ scripts/gulpfile.js
@@ -5,6 +5,7 @@ const path = require('path');
 const less = require('gulp-less');
 const postcss = require('gulp-postcss');
 const postcssCustomProperties = require('postcss-custom-properties');
+const postcssRelativeColorSyntax = require('@csstools/postcss-relative-color-syntax');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
 const babel = require('gulp-babel');
@@ -74,7 +75,15 @@ function buildLess({ lessOptions, outputFileName }) {
       .src(`${styleRoot}/index.less`)
       .pipe(sourcemaps.init())
       .pipe(less(lessOptions))
-      .pipe(postcss([require('autoprefixer'), postcssCustomProperties()]))
+      .pipe(
+        postcss([
+          require('autoprefixer'),
+          postcssCustomProperties(),
+          postcssRelativeColorSyntax({
+            preserve: true
+          })
+        ])
+      )
       .pipe(sourcemaps.write('./'))
       .pipe(rename(outputFileName))
       .pipe(gulp.dest(`${distRoot}`));
