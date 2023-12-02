4e0d7c5ecc8611a8e3a3847e1db6b0d255f26a64
Alex Nepsha <nepsha1986@gmail.com>
2023-12-02 18:48:16 +0300

    refactor(List): changed test for ListStyleSpec

--- src/List/test/ListStyleSpec.tsx
+++ src/List/test/ListStyleSpec.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import List from '../index';
-import { getDOMNode, getStyle } from '@test/testUtils';
 
 import '../styles/index.less';
 
 describe('List styles', () => {
   it('Should render correct toggle styles', () => {
-    const instanceRef = React.createRef<HTMLDivElement>();
-    render(<List ref={instanceRef} />);
-    const dom = getDOMNode(instanceRef.current);
+    render(<List data-testid="list" />);
 
-    assert.equal(getStyle(dom, 'position'), 'relative', 'List position');
+    expect(screen.getByTestId('list')).to.have.style('position', 'relative');
   });
 });
