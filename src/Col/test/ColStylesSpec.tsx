18ded8f45a97281a9ae735d40966ae9f98776b61
Alex Nepsha <nepsha1986@gmail.com>
2023-12-08 20:33:33 +0300

    refactor(Col): remove legacy getDOMNode from tests

--- src/Col/test/ColStylesSpec.tsx
+++ src/Col/test/ColStylesSpec.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Col from '../index';
-import { getDOMNode, getStyle, itChrome } from '@test/testUtils';
+import { itChrome } from '@test/testUtils';
 import '../../Grid/styles/index.less';
 
 describe('Col styles', () => {
   itChrome('Should render the correct styles', () => {
-    const instanceRef = React.createRef<HTMLDivElement>();
-    render(
-      <Col ref={instanceRef} md={1}>
-        Title
-      </Col>
-    );
-    assert.equal(getStyle(getDOMNode(instanceRef.current), 'padding'), '0px 5px');
+    render(<Col md={1}>Title</Col>);
+    expect(screen.getByRole('gridcell')).to.have.style('padding', '0px 5px');
   });
 });
