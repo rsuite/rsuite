c997931e9f8216927cf6d3f0d443b61f21df2cc2
Alex Nepsha <nepsha1986@gmail.com>
2023-12-21 13:09:02 +0300

    refactor(Steps): test fix

--- src/Steps/test/StepItemStylesSpec.tsx
+++ src/Steps/test/StepItemStylesSpec.tsx
@@ -7,6 +7,6 @@ import '../styles/index.less';
 describe('StepItem styles', () => {
   it('Should render the correct styles', () => {
     render(<StepItem data-testid="step_item" />);
-    expect(screen.getByTestId('step_item')).to.have.style('paddingLeft', '40px');
+    expect(screen.getByTestId('step_item')).to.have.style('padding-left', '40px');
   });
 });
