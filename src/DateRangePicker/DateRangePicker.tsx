74f1c7ceb006e6b825115595fd596ee8d66f6199
simonguo <simonguo.2009@gmail.com>
2023-12-23 09:56:09 +0800

    fix: remove data-testid

--- src/DateRangePicker/DateRangePicker.tsx
+++ src/DateRangePicker/DateRangePicker.tsx
@@ -869,9 +869,7 @@ const DateRangePicker: DateRangePicker = React.forwardRef((props: DateRangePicke
 
             <>
               <div className={prefix('daterange-content')}>
-                <div className={prefix('daterange-header')} data-testid="daterange-header">
-                  {getDisplayString(selectedDates)}
-                </div>
+                <div className={prefix('daterange-header')}>{getDisplayString(selectedDates)}</div>
                 <div
                   className={prefix(`daterange-calendar-${showOneCalendar ? 'single' : 'group'}`)}
                 >
