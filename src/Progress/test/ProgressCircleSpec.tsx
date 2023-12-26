b70fab5454053b7d562d0f9e94eff3ddb298ec41
Alex Nepsha <nepsha1986@gmail.com>
2023-12-26 11:57:14 +0300

    refactor(Progress): remove legacy getDOMNode

--- src/Progress/test/ProgressCircleSpec.tsx
+++ src/Progress/test/ProgressCircleSpec.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable testing-library/no-node-access */
 import React from 'react';
-import { getDOMNode, testStandardProps } from '@test/utils';
+import { render, screen } from '@testing-library/react';
+import { testStandardProps } from '@test/utils';
 import ProgressCircle from '../ProgressCircle';
 
 describe('Progress - Circle', () => {
@@ -8,82 +10,91 @@ describe('Progress - Circle', () => {
   });
 
   it('Should render a Circle', () => {
-    const instance = getDOMNode(<ProgressCircle />);
-    assert.ok(instance.className.match(/\brs-progress-circle\b/));
+    render(<ProgressCircle />);
+
+    expect(screen.getByRole('progressbar')).to.have.class('rs-progress-circle');
   });
 
   it('Should have a percentage', () => {
-    const instance = getDOMNode(<ProgressCircle percent={10} />);
-    assert.equal(instance.textContent, '10%');
+    render(<ProgressCircle percent={10} />);
+    expect(screen.getByRole('progressbar')).to.have.text('10%');
   });
 
   it('Should have a width', () => {
-    const instance = getDOMNode(<ProgressCircle trailWidth={0} percent={1} strokeWidth={10} />);
+    render(<ProgressCircle trailWidth={0} percent={1} strokeWidth={10} />);
 
-    assert.equal(
-      (instance.querySelector('.rs-progress-trail') as HTMLElement).getAttribute('stroke-width'),
+    expect(screen.getByRole('progressbar').querySelector('.rs-progress-trail')).to.have.attr(
+      'stroke-width',
       '10'
     );
-    assert.equal(
-      (instance.querySelector('.rs-progress-stroke') as HTMLElement).getAttribute('stroke-width'),
+    expect(screen.getByRole('progressbar').querySelector('.rs-progress-stroke')).to.have.attr(
+      'stroke-width',
       '10'
     );
   });
 
   it('Should have a background color', () => {
-    const instance = getDOMNode(<ProgressCircle strokeColor={'#ff0000'} />);
+    render(<ProgressCircle strokeColor={'#ff0000'} />);
 
-    assert.equal(
-      (instance.querySelector('.rs-progress-stroke') as HTMLElement).style.stroke,
+    expect(screen.getByRole('progressbar').querySelector('.rs-progress-stroke')).to.have.style(
+      'stroke',
       'rgb(255, 0, 0)'
     );
   });
 
   it('Should render info', () => {
-    const instance = getDOMNode(<ProgressCircle />);
-    const instance2 = getDOMNode(<ProgressCircle showInfo={false} />);
+    render(<ProgressCircle />);
 
-    assert.ok(instance.querySelector('.rs-progress-circle-info'));
-    assert.ok(!instance2.querySelector('.rs-progress-circle-info'));
+    expect(screen.getByRole('progressbar').querySelector('.rs-progress-circle-info')).to.exist;
+  });
+
+  it('Should not render info', () => {
+    render(<ProgressCircle showInfo={false} />);
+
+    expect(screen.getByRole('progressbar').querySelector('.rs-progress-circle-info')).to.not.exist;
   });
 
   it('Should render a status', () => {
-    const instance = getDOMNode(<ProgressCircle status="success" />);
-    assert.ok(instance.className.match(/\brs-progress-circle-success\b/));
+    render(<ProgressCircle status="success" />);
+
+    expect(screen.getByRole('progressbar')).to.have.class('rs-progress-circle-success');
   });
 
   it('Should be able to customize the Path type', () => {
-    const instance = getDOMNode(<ProgressCircle strokeLinecap="butt" />);
+    render(<ProgressCircle strokeLinecap="butt" />);
 
-    assert.equal(
-      (instance.querySelector('.rs-progress-stroke') as HTMLElement).getAttribute('stroke-linecap'),
+    expect(screen.getByRole('progressbar').querySelector('.rs-progress-stroke')).to.have.attr(
+      'stroke-linecap',
       'butt'
     );
   });
 
   it('Should render start position by `gapPosition`', () => {
-    const instance1 = getDOMNode(<ProgressCircle gapPosition="top" />);
-    const instance2 = getDOMNode(<ProgressCircle gapPosition="bottom" />);
-    const instance3 = getDOMNode(<ProgressCircle gapPosition="left" />);
-    const instance4 = getDOMNode(<ProgressCircle gapPosition="right" />);
+    render(
+      <>
+        <ProgressCircle gapPosition="top" />
+        <ProgressCircle gapPosition="bottom" />
+        <ProgressCircle gapPosition="left" />
+        <ProgressCircle gapPosition="right" />
+      </>
+    );
 
-    assert.equal(
-      (instance1.querySelector('.rs-progress-trail') as HTMLElement).getAttribute('d'),
+    const progressCircles = screen.getAllByRole('progressbar');
+
+    expect(progressCircles[0].querySelector('.rs-progress-trail')).to.have.attr(
+      'd',
       'M 50,50 m 0,-47 a 47,47 0 1 1 0,94 a 47,47 0 1 1 0,-94'
     );
-
-    assert.equal(
-      (instance2.querySelector('.rs-progress-trail') as HTMLElement).getAttribute('d'),
+    expect(progressCircles[1].querySelector('.rs-progress-trail')).to.have.attr(
+      'd',
       'M 50,50 m 0,47 a 47,47 0 1 1 0,-94 a 47,47 0 1 1 0,94'
     );
-
-    assert.equal(
-      (instance3.querySelector('.rs-progress-trail') as HTMLElement).getAttribute('d'),
+    expect(progressCircles[2].querySelector('.rs-progress-trail')).to.have.attr(
+      'd',
       'M 50,50 m -47,0 a 47,47 0 1 1 94,0 a 47,47 0 1 1 -94,0'
     );
-
-    assert.equal(
-      (instance4.querySelector('.rs-progress-trail') as HTMLElement).getAttribute('d'),
+    expect(progressCircles[3].querySelector('.rs-progress-trail')).to.have.attr(
+      'd',
       'M 50,50 m 47,0 a 47,47 0 1 1 -94,0 a 47,47 0 1 1 94,0'
     );
   });
