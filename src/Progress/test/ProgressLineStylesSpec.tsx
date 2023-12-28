0b0a792d7302d87e8e8e9fc156cc3ac12db9821c
Alex Nepsha <nepsha1986@gmail.com>
2023-12-28 04:01:14 +0300

    refactor(Progress): remove legacy getDOMNode (#3524)
    * refactor(Progress): remove legacy getDOMNode
    
    * refactor(Progress): remove legacy getDOMNode
    
    * refactor(Progress): remove legacy getDOMNode

--- src/Progress/test/ProgressLineStylesSpec.tsx
+++ src/Progress/test/ProgressLineStylesSpec.tsx
@@ -1,28 +1,23 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import ProgressLine from '../ProgressLine';
-import { getDOMNode, getStyle, toRGB, inChrome } from '@test/utils';
+import { toRGB } from '@test/utils';
 
 import '../styles/index.less';
 
 describe('ProgressLine styles', () => {
   it('Should render the correct styles', () => {
-    const instanceRef = React.createRef<HTMLDivElement>();
-    render(<ProgressLine ref={instanceRef} />);
-    const dom = getDOMNode(instanceRef.current);
-    const outerDom = dom.querySelector('.rs-progress-line-outer') as HTMLElement;
-    const innerDom = dom.querySelector('.rs-progress-line-inner') as HTMLElement;
-    const infoDom = dom.querySelector('.rs-progress-info') as HTMLElement;
-    assert.equal(getStyle(dom, 'fontSize'), '16px', 'ProgressLine font-size');
-    assert.equal(getStyle(dom, 'height'), '36px', 'ProgressLine height');
-    inChrome &&
-      assert.equal(getStyle(outerDom, 'borderRadius'), '5px', 'ProgressLine outer border-radius');
-    assert.equal(
-      getStyle(innerDom, 'backgroundColor'),
-      toRGB('#e5e5ea'),
-      'ProgressLine inner border-radius'
-    );
-    inChrome &&
-      assert.equal(getStyle(infoDom, 'paddingLeft'), '12px', 'ProgressLine inner padding-left');
+    render(<ProgressLine />);
+
+    const progressBar = screen.getByRole('progressbar');
+    const lineOuter = progressBar.querySelector('.rs-progress-line-outer') as HTMLElement;
+    const lineInner = progressBar.querySelector('.rs-progress-line-inner') as HTMLElement;
+    const info = progressBar.querySelector('.rs-progress-info') as HTMLElement;
+
+    expect(progressBar).to.have.style('font-size', '16px');
+    expect(progressBar).to.have.style('height', '36px');
+    expect(lineOuter).to.have.style('border-radius', '5px');
+    expect(lineInner).to.have.style('background-color', toRGB('#e5e5ea'));
+    expect(info).to.have.style('padding-left', '12px');
   });
 });
