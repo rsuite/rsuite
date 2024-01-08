365a9bc2fe99c5fe5dd9710e21978487b0fbb02e
simonguo <simonguo.2009@gmail.com>
2024-01-08 14:54:44 +0800

    feat(cra): supports using CRA to install typescript template

--- tooling/cra-template-typescript/template/src/App.tsx
+++ tooling/cra-template-typescript/template/src/App.tsx
@@ -1,48 +1,45 @@
 import React from 'react';
 import { Button, CustomProvider, Container, Stack, Toggle } from 'rsuite';
-import en from 'rsuite/locales/en_GB';
+import { Icon } from '@rsuite/icons';
 import { FaMoon, FaSun } from 'react-icons/fa';
-import logo from './logo.svg';
-
+import Logo from './Logo';
 import './App.css';
 
 function App() {
-  const [theme, setTheme] = React.useState<'light' | 'dark'>('light');
+  const [theme, setTheme] = React.useState<'light' | 'dark'>('dark');
 
   const toggleTheme = (checked: boolean) => {
-    setTheme(checked ? 'dark' : 'light');
+    setTheme(checked ? 'light' : 'dark');
   };
 
   return (
-    <CustomProvider theme={theme} locale={en}>
+    <CustomProvider theme={theme}>
       <Container className="App">
-        <Toggle checkedChildren={<FaMoon />} unCheckedChildren={<FaSun />} onChange={toggleTheme} />
-
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Logo />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
 
+          <Toggle
+            checked={theme === 'light'}
+            checkedChildren={<Icon as={FaSun} style={{ fontSize: 16 }} />}
+            unCheckedChildren={<Icon as={FaMoon} style={{ fontSize: 16 }} />}
+            onChange={toggleTheme}
+          />
+
           <Stack spacing={10}>
             <Button
-              appearance="link"
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </Button>
-
-            <Button
-              appearance="link"
-              className="App-link"
+              appearance="primary"
               href="https://rsuitejs.com"
               target="_blank"
               rel="noopener noreferrer"
             >
               Learn React Suite
+            </Button>
+
+            <Button href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+              Learn React
             </Button>
           </Stack>
         </header>
