1749d8f29f3bcfa762ac50ad31fc8b7fb6955a7c
simonguo <simonguo.2009@gmail.com>
2024-01-08 15:14:55 +0800

    feat(cra): update template

--- tooling/cra-template/template/src/App.js
+++ tooling/cra-template/template/src/App.js
@@ -1,38 +1,45 @@
 import React from 'react';
-import { Button, CustomProvider, Container, Stack } from 'rsuite';
-import logo from './logo.svg';
-
+import { Button, CustomProvider, Container, Stack, Toggle } from 'rsuite';
+import { Icon } from '@rsuite/icons';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import Logo from './Logo';
 import './App.css';
 
 function App() {
+  const [theme, setTheme] = React.useState('dark');
+
+  const toggleTheme = checked => {
+    setTheme(checked ? 'light' : 'dark');
+  };
+
   return (
-    <CustomProvider theme="light">
-      <Container className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+    <CustomProvider theme={theme}>
+      <Container className="app">
+        <header className="app-header">
+          <Logo />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
 
+          <Toggle
+            checked={theme === 'light'}
+            checkedChildren={<Icon as={FaSun} style={{ fontSize: 16 }} />}
+            unCheckedChildren={<Icon as={FaMoon} style={{ fontSize: 16 }} />}
+            onChange={toggleTheme}
+          />
+
           <Stack spacing={10}>
             <Button
-              appearance="link"
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </Button>
-
-            <Button
-              appearance="link"
-              className="App-link"
+              appearance="primary"
               href="https://rsuitejs.com"
               target="_blank"
               rel="noopener noreferrer"
             >
               Learn React Suite
+            </Button>
+
+            <Button href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+              Learn React
             </Button>
           </Stack>
         </header>
