bbe3609d4492e9186e32309f6c6ee6101d5eb83c
Simon Guo <simonguo.2009@gmail.com>
2024-01-09 09:46:49 +0800

    feat(cra): supports using CRA to install typescript template (#3540)
    * feat(cra): supports using CRA to install typescript template
    
    * feat(cra): supports using CRA to install typescript template

---
+++ tooling/cra-template-typescript/README.md
@@ -0,0 +1,9 @@
+# @rsuite/cra-template-typescript
+
+This is the official rsuite+typescript template for [Create React App](https://github.com/facebook/create-react-app).
+
+## Usage
+
+```sh
+npx create-react-app my-app --template @rsuite/typescript
+```
---
+++ tooling/cra-template-typescript/template/src/App.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Button, CustomProvider, Container, Stack, Toggle } from 'rsuite';
+import { Icon } from '@rsuite/icons';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import Logo from './Logo';
+import './App.css';
+
+function App() {
+  const [theme, setTheme] = React.useState<'light' | 'dark'>('dark');
+
+  const toggleTheme = (checked: boolean) => {
+    setTheme(checked ? 'light' : 'dark');
+  };
+
+  return (
+    <CustomProvider theme={theme}>
+      <Container className="App">
+        <header className="App-header">
+          <Logo />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+
+          <Toggle
+            checked={theme === 'light'}
+            checkedChildren={<Icon as={FaSun} style={{ fontSize: 16 }} />}
+            unCheckedChildren={<Icon as={FaMoon} style={{ fontSize: 16 }} />}
+            onChange={toggleTheme}
+          />
+
+          <Stack spacing={10}>
+            <Button
+              appearance="primary"
+              href="https://rsuitejs.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React Suite
+            </Button>
+
+            <Button href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+              Learn React
+            </Button>
+          </Stack>
+        </header>
+      </Container>
+    </CustomProvider>
+  );
+}
+
+export default App;
