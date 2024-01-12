5c793c2a48fdfdc02beda3b1ab071e7a36cdcb17
simonguo <simonguo.2009@gmail.com>
2024-01-12 10:14:25 +0800

    refactor: remove dependency on TextMask

--- src/Picker/PickerToggle.tsx
+++ src/Picker/PickerToggle.tsx
@@ -1,11 +1,10 @@
-import React, { useState, useCallback, useEffect, useRef } from 'react';
+import React, { useRef, useMemo } from 'react';
 import ToggleButton, { ToggleButtonProps } from './ToggleButton';
-import { useClassNames, mergeRefs } from '../utils';
+import { useClassNames, mergeRefs, useEventCallback } from '../utils';
 import { RsRefForwardingComponent, TypeAttributes } from '../@types/common';
 import Plaintext from '../Plaintext';
 import useToggleCaret from '../utils/useToggleCaret';
 import { IconProps } from '@rsuite/icons/lib/Icon';
-import TextMask from '../MaskedInput/TextMask';
 import Stack from '../Stack';
 import PickerIndicator from './PickerIndicator';
 import PickerLabel from './PickerLabel';
@@ -13,36 +12,33 @@ import PickerLabel from './PickerLabel';
 type ValueType = string | number;
 
 export interface PickerToggleProps extends ToggleButtonProps {
-  value?: ValueType | ValueType[];
-  inputValue?: ValueType | ValueType[];
-  id?: string;
+  active?: boolean;
   hasValue?: boolean;
   cleanable?: boolean;
   caret?: boolean;
-  active?: boolean;
-  disabled?: boolean;
-  readOnly?: boolean;
-  plaintext?: boolean;
-  tabIndex?: number;
-
-  /** Whether to display an loading indicator on toggle button */
-  loading?: boolean;
-
-  // Renders an input and is editable
-  editable?: boolean;
-  name?: string;
-
-  placement?: TypeAttributes.Placement;
   /**
    * Custom caret component
    * @deprecated Use `caretAs` instead
    */
   caretComponent?: React.FC<IconProps>;
-
-  /** Custom caret component */
+  /**
+   * Custom caret component
+   */
   caretAs?: React.ElementType;
-  onClean?: (event: React.MouseEvent) => void;
+  disabled?: boolean;
+  placement?: TypeAttributes.Placement;
+  readOnly?: boolean;
+  plaintext?: boolean;
+  tabIndex?: number;
+  /**
+   * Whether to display an loading indicator on toggle button
+   */
+  loading?: boolean;
   label?: React.ReactNode;
+  name?: string;
+  id?: string;
+  inputValue?: ValueType | ValueType[];
+  onClean?: (event: React.MouseEvent) => void;
 }
 
 const PickerToggle: RsRefForwardingComponent<typeof ToggleButton, PickerToggleProps> =
@@ -58,15 +54,12 @@ const PickerToggle: RsRefForwardingComponent<typeof ToggleButton, PickerTogglePr
       readOnly,
       plaintext,
       hasValue,
-      editable,
       loading = false,
-      cleanable: cleanableProp,
-      tabIndex: tabIndexProp = editable ? -1 : 0,
+      cleanable,
+      tabIndex = 0,
       id,
-      value,
       inputValue: inputValueProp,
       onClean,
-
       placement = 'bottomStart',
       caretComponent,
       caretAs = caretComponent,
@@ -75,62 +68,26 @@ const PickerToggle: RsRefForwardingComponent<typeof ToggleButton, PickerTogglePr
       ...rest
     } = props;
 
-    const inputRef = useRef<HTMLInputElement>(null);
-    const comboboxRef = useRef<HTMLDivElement>(null);
+    const combobox = useRef<HTMLDivElement>(null);
     const { withClassPrefix, merge, prefix } = useClassNames(classPrefix);
-    const getInputValue = useCallback(
-      () =>
-        typeof inputValueProp === 'undefined'
-          ? Array.isArray(value)
-            ? value.join(',')
-            : value
-          : inputValueProp,
-      [inputValueProp, value]
-    );
-    const [inputValue, setInputValue] = useState(getInputValue);
 
-    useEffect(() => {
-      if (comboboxRef.current) {
-        const value = getInputValue();
-        setInputValue(value);
+    const inputValue = useMemo(() => {
+      if (typeof inputValueProp === 'number' || typeof inputValueProp === 'string') {
+        return inputValueProp;
+      } else if (Array.isArray(inputValueProp)) {
+        return inputValueProp.join(',');
       }
-    }, [getInputValue]);
+
+      return '';
+    }, [inputValueProp]);
 
     const classes = merge(className, withClassPrefix({ active }));
 
-    const handleInputBlur = () => {
-      setInputValue(getInputValue());
-    };
-
-    const handleClean = useCallback(
-      (event: React.MouseEvent<HTMLElement>) => {
-        event.stopPropagation();
-        onClean?.(event);
-
-        // When the value is cleared, the current component is still in focus.
-        editable ? inputRef.current?.focus() : comboboxRef.current?.focus();
-      },
-      [editable, onClean]
-    );
-
-    const handleInputChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
-      const value = event.target?.value;
-      setInputValue(value);
-    }, []);
-
-    const renderInput = useCallback(
-      (ref, props) => (
-        <input
-          type="text"
-          autoComplete="off"
-          ref={mergeRefs(inputRef, ref)}
-          name={name}
-          style={{ pointerEvents: editable ? undefined : 'none' }}
-          {...props}
-        />
-      ),
-      [editable, name]
-    );
+    const handleClean = useEventCallback((event: React.MouseEvent<HTMLElement>) => {
+      event.stopPropagation();
+      onClean?.(event);
+      combobox.current?.focus();
+    });
 
     const ToggleCaret = useToggleCaret(placement);
     const Caret = caretAs ?? ToggleCaret;
@@ -143,12 +100,7 @@ const PickerToggle: RsRefForwardingComponent<typeof ToggleButton, PickerTogglePr
       );
     }
 
-    const showCleanButton = cleanableProp && hasValue && !readOnly;
-
-    // When the component is read-only or disabled, the input is not interactive.
-    const inputFocused = readOnly || disabled ? false : editable;
-
-    const tabIndex = disabled ? undefined : tabIndexProp;
+    const showCleanButton = cleanable && hasValue && !readOnly;
 
     return (
       <Component
@@ -158,9 +110,9 @@ const PickerToggle: RsRefForwardingComponent<typeof ToggleButton, PickerTogglePr
         aria-disabled={disabled}
         aria-owns={id ? `${id}-listbox` : undefined}
         {...rest}
-        ref={mergeRefs(comboboxRef, ref)}
+        ref={mergeRefs(combobox, ref)}
         disabled={disabled}
-        tabIndex={tabIndex}
+        tabIndex={disabled ? undefined : tabIndex}
         className={classes}
       >
         <Stack>
@@ -176,21 +128,18 @@ const PickerToggle: RsRefForwardingComponent<typeof ToggleButton, PickerTogglePr
             </Stack.Item>
           )}
           <Stack.Item grow={1} style={{ overflow: 'hidden' }}>
-            <TextMask
-              aria-hidden={!inputFocused}
+            <input
+              readOnly
+              aria-hidden={true}
               aria-controls={id ? `${id}-listbox` : undefined}
               aria-labelledby={label ? `${id}-label` : undefined}
-              mask={[]}
-              value={Array.isArray(inputValue) ? inputValue.toString() : inputValue}
-              onBlur={handleInputBlur}
-              onChange={handleInputChange}
-              id={id}
-              readOnly={!inputFocused}
-              disabled={disabled}
-              tabIndex={editable ? 0 : -1}
-              className={prefix('textbox', { 'read-only': !inputFocused })}
-              render={renderInput}
+              tabIndex={-1}
               data-testid="picker-toggle-input"
+              name={name}
+              value={inputValue}
+              id={id}
+              className={prefix('textbox', 'read-only')}
+              style={{ pointerEvents: 'none' }}
             />
             {children ? (
               <span
@@ -201,7 +150,6 @@ const PickerToggle: RsRefForwardingComponent<typeof ToggleButton, PickerTogglePr
               </span>
             ) : null}
           </Stack.Item>
-
           <Stack.Item className={prefix`indicator`}>
             <PickerIndicator
               as={React.Fragment}
