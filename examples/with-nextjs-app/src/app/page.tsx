e1d574b4d7d368733fe9b47eb84fa2dfbc1e72e4
simonguo <simonguo.2009@gmail.com>
2024-05-30 18:09:42 +0800

    docs(with-nextjs-app): use FlexboxGrid

--- examples/with-nextjs-app/src/app/page.tsx
+++ examples/with-nextjs-app/src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
-import styles from './page.module.css';
+import FlexboxGrid from 'rsuite/FlexboxGrid';
+import FlexboxGridItem from 'rsuite/FlexboxGridItem';
 import Navbar from '../components/Navbar';
+
+import styles from './page.module.css';
 
 export default function Home() {
   return (
@@ -26,55 +29,63 @@ export default function Home() {
         <Image src="/rsuite.svg" alt="React Suite Logo" width={60} height={60} priority />
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <FlexboxGrid className={styles.grid}>
+        <FlexboxGridItem colspan={6}>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Docs <span>-&gt;</span>
+            </h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+        </FlexboxGridItem>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+        <FlexboxGridItem colspan={6}>
+          <a
+            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Learn <span>-&gt;</span>
+            </h2>
+            <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+          </a>
+        </FlexboxGridItem>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+        <FlexboxGridItem colspan={6}>
+          <a
+            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Templates <span>-&gt;</span>
+            </h2>
+            <p>Explore starter templates for Next.js.</p>
+          </a>
+        </FlexboxGridItem>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
-        </a>
-      </div>
+        <FlexboxGridItem colspan={6}>
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Deploy <span>-&gt;</span>
+            </h2>
+            <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
+          </a>
+        </FlexboxGridItem>
+      </FlexboxGrid>
     </main>
   );
 }
