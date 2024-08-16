6396b3e6d3692f76850409a5d3d1bd60aea40788
Du <53305259+myNameIsDu@users.noreply.github.com>
2024-08-16 09:45:14 +0800

    fix: use useLayoutEffect instead of useEffect in useEventCallback (#3921)

--- src/internals/hooks/useEventCallback.ts
+++ src/internals/hooks/useEventCallback.ts
@@ -1,4 +1,5 @@
-import { useEffect, useCallback, useRef } from 'react';
+import { useCallback, useRef } from 'react';
+import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
 
 /**
  * https://github.com/facebook/react/issues/14099#issuecomment-440013892
@@ -6,7 +7,12 @@ import { useEffect, useCallback, useRef } from 'react';
  */
 export function useEventCallback(fn) {
   const ref = useRef(fn);
-  useEffect(() => {
+  /**
+   * use useLayoutEffect instead of useEffect.
+   * useLayoutEffect is earlier than useEffect, sometimes we use setState and then use callback immediately,
+   * However the state in callback is not the latest, because useEffect is not triggered.
+   */
+  useIsomorphicLayoutEffect(() => {
     ref.current = fn;
   });
   return useCallback((...args) => {
