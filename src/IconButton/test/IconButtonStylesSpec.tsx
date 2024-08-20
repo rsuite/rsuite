648277ecd6c209451642682abba377604c0c155e
Christian Caracach <christiancaracach@gmail.com>
2024-08-20 17:58:44 -0300

    test(IconButton): requested changes

--- src/IconButton/test/IconButtonStylesSpec.tsx
+++ src/IconButton/test/IconButtonStylesSpec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import IconButton from '../index';
 import { getStyle, inChrome } from '@test/utils';
 
@@ -9,14 +9,18 @@ describe('IconButton styles', () => {
   it('Should render the correct width and height', () => {
     const instanceRef = React.createRef<HTMLButtonElement>();
     render(<IconButton ref={instanceRef} />);
-    const { container } = render(<IconButton ref={instanceRef} />);
-    const dom = container.firstChild as HTMLElement;
-    expect(getStyle(dom, 'width')).to.equal(getStyle(dom, 'height'));
+    const iconButton = screen.getByRole('button');
+
+    expect(getStyle(iconButton as Element, 'width')).to.equal(
+      getStyle(iconButton as Element, 'height')
+    );
   });
 
   it('Should render the correct border-radius', () => {
     const instanceRef = React.createRef<HTMLButtonElement>();
-    const { container } = render(<IconButton circle ref={instanceRef} />);
-    inChrome && expect(getStyle(container.firstChild as Element, 'borderRadius')).to.equal('50%');
+    render(<IconButton circle ref={instanceRef} />);
+    const iconButton = screen.getByRole('button');
+
+    inChrome && expect(getStyle(iconButton as Element, 'borderRadius')).to.equal('50%');
   });
 });
