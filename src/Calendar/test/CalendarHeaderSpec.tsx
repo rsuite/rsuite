3c0a53fb54bbae108f584d421383ec159c42b9c9
Simon Guo <simonguo.2009@gmail.com>
2024-09-04 23:13:01 +0800

    test(Calendar): refactor using fireEvent instead of Simulate (#3953)
    * test(Calendar): refactor using fireEvent instead of Simulate
    
    * test: fix error tests

--- src/Calendar/test/CalendarHeaderSpec.tsx
+++ src/Calendar/test/CalendarHeaderSpec.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import ReactTestUtils from 'react-dom/test-utils';
-
+import sinon from 'sinon';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { testStandardProps } from '@test/utils';
 import Header from '../CalendarHeader';
 import CalendarContext from '../CalendarContext';
-import Sinon from 'sinon';
-import { testStandardProps } from '@test/utils';
 
 describe('Calendar-Header', () => {
   testStandardProps(<Header />);
@@ -17,48 +15,47 @@ describe('Calendar-Header', () => {
   });
 
   it('Should call `onMoveForward` callback', () => {
-    const onMoveForward = Sinon.spy();
+    const onMoveForward = sinon.spy();
 
     render(<Header showMonth onMoveForward={onMoveForward} />);
 
-    ReactTestUtils.Simulate.click(screen.getByRole('button', { name: 'Next month' }));
+    fireEvent.click(screen.getByRole('button', { name: 'Next month' }));
     expect(onMoveForward).to.have.been.calledOnce;
   });
 
   it('Should call `onMoveBackward` callback', () => {
-    const onMoveBackward = Sinon.spy();
+    const onMoveBackward = sinon.spy();
 
     render(<Header showMonth onMoveBackward={onMoveBackward} />);
 
-    ReactTestUtils.Simulate.click(screen.getByRole('button', { name: 'Previous month' }));
+    fireEvent.click(screen.getByRole('button', { name: 'Previous month' }));
+
     expect(onMoveBackward).to.have.been.calledOnce;
   });
 
   it('Should call `onToggleMonthDropdown` callback', () => {
-    const onToggleMonthDropdown = Sinon.spy();
+    const onToggleMonthDropdown = sinon.spy();
 
     render(<Header showMonth onToggleMonthDropdown={onToggleMonthDropdown} />);
 
-    ReactTestUtils.Simulate.click(screen.getByRole('button', { name: 'Select month' }));
+    fireEvent.click(screen.getByRole('button', { name: 'Select month' }));
+
     expect(onToggleMonthDropdown).to.have.been.calledOnce;
   });
 
   it('Should call `onToggleTimeDropdown` callback', () => {
-    const onToggleTimeDropdown = Sinon.spy();
-    const ref = React.createRef<HTMLDivElement>();
+    const onToggleTimeDropdown = sinon.spy();
 
     render(
       <CalendarContext.Provider
         value={{ date: new Date(), format: 'HH:mm:ss', locale: {}, isoWeek: false, weekStart: 0 }}
       >
-        <Header showTime onToggleTimeDropdown={onToggleTimeDropdown} ref={ref} />
+        <Header showTime onToggleTimeDropdown={onToggleTimeDropdown} />
       </CalendarContext.Provider>
     );
 
-    ReactTestUtils.Simulate.click(
-      // eslint-disable-next-line testing-library/no-node-access
-      (ref.current as HTMLDivElement).querySelector('.rs-calendar-header-title-time') as HTMLElement
-    );
+    fireEvent.click(screen.getByRole('button', { name: 'Select time' }));
+
     expect(onToggleTimeDropdown).to.have.been.calledOnce;
   });
 });
