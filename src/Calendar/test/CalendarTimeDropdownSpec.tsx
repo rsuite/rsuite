0f44b9f6ce0853633052c66bc064023e20e387d5
simonguo <simonguo.2009@gmail.com>
2024-09-04 22:01:31 +0800

    test: fix error tests

--- src/Calendar/test/CalendarTimeDropdownSpec.tsx
+++ src/Calendar/test/CalendarTimeDropdownSpec.tsx
@@ -7,12 +7,12 @@ import CalendarContext from '../CalendarContext';
 import en_US from '../../locales/en_US';
 
 describe('Calendar - TimeDropdown', () => {
-  testStandardProps(<TimeDropdown />);
+  testStandardProps(<TimeDropdown show />);
 
   it('Should render a div with `time-dropdown` class', () => {
     render(<TimeDropdown />);
 
-    expect(screen.getByRole('group')).to.have.class('rs-calendar-time-dropdown');
+    expect(screen.getByRole('group', { hidden: true })).to.have.class('rs-calendar-time-dropdown');
   });
 
   it('Should render hours, minutes and seconds', () => {
@@ -93,7 +93,7 @@ describe('Calendar - TimeDropdown', () => {
       </CalendarContext.Provider>
     );
 
-    fireEvent.click(screen.getByRole('option', { name: '1 hours' }));
+    fireEvent.click(screen.getByRole('option', { name: '1 hours', hidden: true }));
 
     expect(onChangeTime).to.have.been.calledOnce;
   });
@@ -117,7 +117,7 @@ describe('Calendar - TimeDropdown', () => {
       </CalendarContext.Provider>
     );
 
-    screen.getAllByRole('option').forEach((option, index) => {
+    screen.getAllByRole('option', { hidden: true }).forEach((option, index) => {
       expect(option).to.have.attribute('aria-disabled', index > 10 ? 'true' : 'false');
     });
   });
@@ -141,6 +141,6 @@ describe('Calendar - TimeDropdown', () => {
       </CalendarContext.Provider>
     );
 
-    expect(screen.getAllByRole('option')).to.have.length(11);
+    expect(screen.getAllByRole('option', { hidden: true })).to.have.length(11);
   });
 });
