a56dedea06200f61061b2084aeb82bcf64c3d5ef
simonguo <simonguo.2009@gmail.com>
2024-10-22 14:32:09 +0800

    fix: provided requires using 'export type'

--- src/CustomProvider/index.tsx
+++ src/CustomProvider/index.tsx
@@ -3,6 +3,6 @@ import FormattedDate from './FormattedDate';
 
 export { CustomContext, FormattedDate };
 export { useCustom } from './useCustom';
-export { CustomProviderProps } from './CustomProvider';
+export type { CustomProviderProps } from './CustomProvider';
 
 export default CustomProvider;
