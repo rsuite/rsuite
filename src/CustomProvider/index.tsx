86ddb72b23aef22f1dec773c82c478691d5116e9
simonguo <simonguo.2009@gmail.com>
2024-10-27 11:30:40 +0800

    fix: export type ReactSuiteComponents

--- src/CustomProvider/index.tsx
+++ src/CustomProvider/index.tsx
@@ -4,5 +4,6 @@ import FormattedDate from './FormattedDate';
 export { CustomContext, FormattedDate };
 export { useCustom } from './useCustom';
 export type { CustomProviderProps } from './CustomProvider';
+export type { ReactSuiteComponents } from './types';
 
 export default CustomProvider;
