3d5aee9a1e3988a8f086756bb5db5f943165aa92
simonguo <simonguo.2009@gmail.com>
2024-11-28 10:59:00 +0800

    docs: add support for full screen on FakeBrowser

--- docs/components/FakeBrowser/FakeBrowser.tsx
+++ docs/components/FakeBrowser/FakeBrowser.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import classNames from 'classnames';
+import { TiArrowUnsorted } from 'react-icons/ti';
 
 interface FakeBrowserProps extends React.HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
@@ -9,17 +10,34 @@ interface FakeBrowserProps extends React.HTMLAttributes<HTMLDivElement> {
 
 function FakeBrowser(props: FakeBrowserProps) {
   const { children, width, height, style, className, ...rest } = props;
+  const browserRef = useRef<HTMLDivElement>(null);
+
+  const toggleFullscreen = () => {
+    const browser = browserRef.current;
+    if (!browser) {
+      return;
+    }
+
+    if (document.fullscreenElement) {
+      document.exitFullscreen();
+    } else {
+      browser.requestFullscreen();
+    }
+  };
 
   return (
     <div
       className={classNames('fake-browser', className)}
       style={{ width, height, ...style }}
+      ref={browserRef}
       {...rest}
     >
       <div className="fake-browser-header">
         <button className="fake-browser-header-button red" />
         <button className="fake-browser-header-button yellow" />
-        <button className="fake-browser-header-button green" />
+        <button className="fake-browser-header-button green" onClick={toggleFullscreen}>
+          <TiArrowUnsorted className="fake-browser-header-button-icon" />
+        </button>
       </div>
 
       <div className="fake-browser-content">{children}</div>
