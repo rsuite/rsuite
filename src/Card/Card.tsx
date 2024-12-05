fd522a6cb4b506a072ac4ba2f6698013b8ba52b1
simonguo <simonguo.2009@gmail.com>
2024-12-05 10:53:09 +0800

    fix: add propTypes for Card

--- src/Card/Card.tsx
+++ src/Card/Card.tsx
@@ -5,6 +5,7 @@ import CardBody from './CardBody';
 import CardFooter from './CardFooter';
 import { useCustom } from '../CustomProvider';
 import { useClassNames } from '@/internals/hooks';
+import { oneOf } from '@/internals/propTypes';
 import type { WithAsProps, RsRefForwardingComponent } from '@/internals/types';
 
 export interface CardProps extends WithAsProps {
@@ -81,7 +82,10 @@ Card.Footer = CardFooter;
 
 Card.propTypes = {
   bordered: PropTypes.bool,
-  shaded: PropTypes.oneOfType([PropTypes.bool, PropTypes.oneOf(['hover'])])
+  shaded: PropTypes.oneOfType([PropTypes.bool, PropTypes.oneOf(['hover'])]),
+  width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+  direction: oneOf(['row', 'column']),
+  size: oneOf(['lg', 'md', 'sm'])
 };
 
 export default Card;
