19950e429163c2a9a1c4d4d54203d3c630870767
simonguo <simonguo.2009@gmail.com>
2024-12-20 15:07:55 +0800

    fix: fix the upstream dependency conflict

--- test/utils/renderHook.ts
+++ test/utils/renderHook.ts
@@ -2,7 +2,6 @@ import React from 'react';
 
 // https://github.com/testing-library/react-hooks-testing-library#a-note-about-react-18-support
 import * as testLibrary from '@testing-library/react';
-import * as testLibrary17 from '@testing-library/react-hooks/dom';
 
 const majorVersion = parseInt(React.version);
 
@@ -10,4 +9,11 @@ const majorVersion = parseInt(React.version);
  * @type {typeof import('@testing-library/react-hooks').renderHook}
  */
 export const renderHook =
-  majorVersion >= 18 ? testLibrary['renderHook'] : (testLibrary17['renderHook'] as any);
+  majorVersion >= 18
+    ? testLibrary['renderHook']
+    : ((async () => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const { renderHook: renderHook17 } = await import('@testing-library/react-hooks/dom');
+        return renderHook17;
+      })() as any);
