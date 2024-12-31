fa249cde01f45983fc054667937df8306f9df53c
simonguo <simonguo.2009@gmail.com>
2024-12-31 17:09:15 +0800

    test: add tests for NavbarDrawer

--- src/Navbar/test/NavbarDrawerSpec.tsx
+++ src/Navbar/test/NavbarDrawerSpec.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import sinon from 'sinon';
 import Navbar from '../Navbar';
 import NavbarDrawer from '../NavbarDrawer';
 import NavbarToggle from '../NavbarToggle';
+import Drawer from '../../Drawer';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 
 describe('NavbarDrawer', () => {
-  it('Should render a drawer when NavbarToggle is clicked', () => {
+  it('Should not render a drawer by default', () => {
+    const { container } = render(<NavbarDrawer />);
+
+    expect(container.firstChild).to.not.exist;
+  });
+
+  it('Should render a drawer when NavbarToggle is clicked', async () => {
+    const onDrawerOpenChange = sinon.spy();
     render(
-      <Navbar>
+      <Navbar onDrawerOpenChange={onDrawerOpenChange}>
         <NavbarToggle />
-        <NavbarDrawer />
+        <NavbarDrawer>
+          <Drawer.Header>
+            <Drawer.Title>Drawer Title</Drawer.Title>
+          </Drawer.Header>
+        </NavbarDrawer>
       </Navbar>
     );
 
@@ -18,5 +31,14 @@ describe('NavbarDrawer', () => {
     fireEvent.click(screen.getByRole('button'));
 
     expect(screen.getByRole('dialog')).to.be.visible;
+    expect(onDrawerOpenChange).to.be.calledWith(true);
+
+    fireEvent.click(screen.getByRole('button', { name: 'Close' }));
+
+    await waitFor(() => {
+      expect(screen.queryByRole('dialog')).to.not.exist;
+    });
+
+    expect(onDrawerOpenChange).to.be.calledWith(false);
   });
 });
