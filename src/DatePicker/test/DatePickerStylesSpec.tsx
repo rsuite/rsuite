43e755c0cafe0d2f1b471dd18f3e3e4e1cfc671c
simonguo <simonguo.2009@gmail.com>
2025-03-19 16:58:41 +0800

    fix(DatePicker): adjust label padding based on size

--- src/DatePicker/test/DatePickerStylesSpec.tsx
+++ src/DatePicker/test/DatePickerStylesSpec.tsx
@@ -22,4 +22,18 @@ describe('DatePicker styles', () => {
     render(<DatePicker open />);
     expect(screen.getByRole('dialog')).to.have.style('padding', '0px');
   });
+
+  it('Should update the label padding based on size', () => {
+    const { rerender } = render(<DatePicker label="custom-label" />);
+    expect(screen.getByText('custom-label')).to.have.style('padding', '0px 11px');
+
+    rerender(<DatePicker label="custom-label" size="lg" />);
+    expect(screen.getByText('custom-label')).to.have.style('padding', '0px 15px');
+
+    rerender(<DatePicker label="custom-label" size="sm" />);
+    expect(screen.getByText('custom-label')).to.have.style('padding', '0px 9px');
+
+    rerender(<DatePicker label="custom-label" size="xs" />);
+    expect(screen.getByText('custom-label')).to.have.style('padding', '0px 7px');
+  });
 });
