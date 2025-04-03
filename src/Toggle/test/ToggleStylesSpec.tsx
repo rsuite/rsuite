fa16a095faa2b43cd7d1813dd5bf6cf445f611cd
simonguo <simonguo.2009@gmail.com>
2025-04-03 14:07:55 +0800

    test: fix incorrect tests

--- src/Toggle/test/ToggleStylesSpec.tsx
+++ src/Toggle/test/ToggleStylesSpec.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Toggle from '../';
 import { render } from '@testing-library/react';
 import { toRGB } from '@test/utils';
-
 import '../styles/index.less';
 
 describe('Toggle styles', () => {
@@ -11,7 +10,7 @@ describe('Toggle styles', () => {
 
     expect(container.querySelector('.rs-toggle-track')).to.have.style(
       'background-color',
-      toRGB('#b6b7b8')
+      toRGB('#939393')
     );
   });
 
