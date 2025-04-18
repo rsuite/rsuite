14eb5b6d5b2ea31af4137710c28ca0fa250a3faa
simonguo <simonguo.2009@gmail.com>
2025-04-18 12:37:16 +0800

    feat(Navbar.Toggle): add a new Burger component, applied to Navbar.Toggle

---
+++ src/internals/Burger/Burger.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import Box, { BoxProps } from '@/internals/Box';
+import { forwardRef, mergeStyles, getCssValue, getColorStyle } from '@/internals/utils';
+import { useStyles } from '../hooks';
+import type { Color } from '@/internals/types';
+
+export interface BurgerProps extends BoxProps, React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /** Color of the Box */
+  color?: Color | React.CSSProperties['color'];
+
+  /** If true, burger is in open (X) state */
+  open?: boolean;
+
+  /** Thickness of the burger lines (px) */
+  lineThickness?: number;
+}
+
+/**
+ * Burger (hamburger menu) button for toggling navigation menus.
+ */
+const Burger = forwardRef<'button', BurgerProps>((props, ref) => {
+  const {
+    as = 'button',
+    classPrefix = 'burger',
+    className,
+    color,
+    open = true,
+    lineThickness,
+    style,
+    ...rest
+  } = props;
+  const { withPrefix, merge, cssVar, prefix } = useStyles(classPrefix);
+  const classes = merge(className, withPrefix());
+  const mergedStyle = mergeStyles(
+    style,
+    cssVar('thickness', getCssValue(lineThickness)),
+    getColorStyle(color, 'burger')
+  );
+
+  return (
+    <Box
+      as={as}
+      ref={ref}
+      className={classes}
+      aria-pressed={open}
+      data-opened={open}
+      style={mergedStyle}
+      {...rest}
+    >
+      <div className={prefix('line')} />
+    </Box>
+  );
+});
+
+Burger.displayName = 'Burger';
+
+export default Burger;
