0a7465eee35ada554fc8ae74b16507315afd56e0
simonguo <simonguo.2009@gmail.com>
2025-04-18 18:35:42 +0800

    fix: no exported member named 'CustomProviderProps'

--- src/CustomProvider/index.tsx
+++ src/CustomProvider/index.tsx
@@ -1,5 +1,8 @@
 import CustomProvider from './CustomProvider';
 
+// export types
+export type { CustomProviderProps } from '@/internals/Provider/CustomContext';
+
 // export components
 export { CustomProvider };
 export default CustomProvider;
