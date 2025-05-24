0aded45aaabf2af174641f6b45753f7416b59793
Simon Guo <simonguo.2009@gmail.com>
2025-05-24 14:46:50 +0800

    docs(Stat): update ring progress documentation and related examples (#4297)

--- docs/pages/components/stat/index.tsx
+++ docs/pages/components/stat/index.tsx
@@ -5,6 +5,7 @@ import {
   HStack,
   VStack,
   Progress,
+  ProgressCircle,
   SelectPicker,
   useBreakpointValue
 } from 'rsuite';
@@ -27,6 +28,7 @@ export default function Page() {
         HStack,
         VStack,
         Progress,
+        ProgressCircle,
         Stat,
         StatGroup,
         SelectPicker,
