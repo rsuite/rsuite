4c5d2709d9a67613509168274171bd6ad7d43617
Simon Guo <simonguo.2009@gmail.com>
2025-12-19 14:14:45 +0800

    chore(examples): refresh with-typescript (#4488)

--- examples/with-typescript/src/index.tsx
+++ examples/with-typescript/src/index.tsx
@@ -1,12 +1,20 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { CustomProvider } from 'rsuite';
 
 import { Hello } from './components/Hello';
 
-import 'rsuite/styles/index.less';
+import 'rsuite/dist/rsuite-no-reset.min.css';
 
+const container = document.getElementById('example');
 
-ReactDOM.render(
-  <Hello compiler="TypeScript" framework="React" />,
-  document.getElementById('example')
-);
+if (container) {
+  const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  const root = createRoot(container);
+
+  root.render(
+    <CustomProvider theme={mediaQuery.matches ? 'dark' : 'light'}>
+      <Hello compiler="TypeScript" framework="React" />
+    </CustomProvider>
+  );
+}
