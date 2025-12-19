c700908a5fa73edddb33745078770793307df6a4
simonguo <simonguo.2009@gmail.com>
2025-12-19 11:23:38 +0800

    chore(examples): refresh with-styled-components example

--- examples/with-styled-components/src/index.js
+++ examples/with-styled-components/src/index.js
@@ -1,5 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { CustomProvider } from 'rsuite';
+import 'rsuite/dist/rsuite-no-reset.min.css';
+import './globals.css';
+
+function Root() {
+  const [theme, setTheme] = React.useState('light');
+
+  React.useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    setTheme(mediaQuery.matches ? 'dark' : 'light');
+
+    const handleChange = e => {
+      setTheme(e.matches ? 'dark' : 'light');
+    };
+
+    mediaQuery.addEventListener('change', handleChange);
+    return () => mediaQuery.removeEventListener('change', handleChange);
+  }, []);
+
+  return (
+    <CustomProvider theme={theme}>
+      <App />
+    </CustomProvider>
+  );
+}
+
+const container = document.getElementById('root');
+createRoot(container).render(<Root />);
